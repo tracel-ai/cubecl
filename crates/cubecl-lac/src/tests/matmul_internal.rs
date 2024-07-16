ebd0b8b9844c62e9011b1591866e8b5cd2bc0b2f
louisfd <louisfd94@gmail.com>
2024-07-16 13:18:53 -0400

    test in shared memo

--- crates/cubecl-lac/src/tests/matmul_internal.rs
+++ crates/cubecl-lac/src/tests/matmul_internal.rs
@@ -150,6 +150,13 @@ macro_rules! testgen_matmul_internal {
         }
 
         #[test]
+        pub fn compute_loop_cmma_offseted_warp_in_shared_memory_test() {
+            cmma_compute_loop_tests::compute_loop_cmma_offseted_warp_in_shared_memory_test::<TestRuntime>(
+                &Default::default(),
+            )
+        }
+
+        #[test]
         pub fn cmma_compute_loop_mimic_offseted_warp_test() {
             cmma_compute_loop_tests::compute_loop_mimic_offseted_warp_test::<TestRuntime>(
                 &Default::default(),
