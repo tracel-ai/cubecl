56164c2475edc9c01581c4a818220a482337e658
cBournhonesque <charlesbour@gmail.com>
2024-08-12 16:03:47 -0400

    support destructuring tuples in cube macro

--- crates/cubecl-core/tests/frontend/tuple.rs
+++ crates/cubecl-core/tests/frontend/tuple.rs
@@ -8,6 +8,13 @@ pub fn tuple_const() -> (UInt, UInt) {
     (x, y)
 }
 
+#[cube]
+pub fn tuple_destructuring() -> (UInt, UInt) {
+    let x = (UInt::new(0), UInt::new(1));
+    let (a, b) = x;
+    (a + UInt::new(1), b)
+}
+
 mod tests {
     use super::*;
     use cubecl_core::{
@@ -37,6 +44,33 @@ mod tests {
 
         cpa!(scope, x = zero);
         cpa!(scope, y = one);
+
+        scope.operations
+    }
+
+    #[test]
+    fn cube_tuple_destructuring() {
+        let mut context = CubeContext::root();
+
+        tuple_destructuring::__expand(&mut context);
+        let scope = context.into_scope();
+
+        assert_eq!(scope.operations, inline_macro_ref_tuple_destructuring());
+    }
+
+    fn inline_macro_ref_tuple_destructuring() -> Vec<Operation> {
+        let context = CubeContext::root();
+
+        let mut scope = context.into_scope();
+        let a = scope.create_local(Item::new(Elem::UInt));
+        let b = scope.create_local(Item::new(Elem::UInt));
+
+        let zero: Variable = 0u32.into();
+        let one: Variable = 1u32.into();
+
+        cpa!(scope, a = zero);
+        cpa!(scope, b = one);
+        cpa!(scope, a = a + 1u32);
 
         scope.operations
     }
