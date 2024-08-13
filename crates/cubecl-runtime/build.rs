11346bee4cb33ec97528923cb2d1f764a03acdc6
Arthur Brussee <arthur.brussee@gmail.com>
2024-08-13 16:19:15 +0100

    Also depend on std

--- crates/cubecl-runtime/build.rs
+++ crates/cubecl-runtime/build.rs
@@ -3,6 +3,6 @@ use cfg_aliases::cfg_aliases;
 fn main() {
     // Setup cfg aliases
     cfg_aliases! {
-        autotune_persistent_cache: { any(target_os = "windows", target_os = "linux", target_os = "macos") },
+        autotune_persistent_cache: { all(feature = "std", any(target_os = "windows", target_os = "linux", target_os = "macos")) },
     }
 }
