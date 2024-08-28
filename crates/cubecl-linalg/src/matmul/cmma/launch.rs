5831bd1f824575541c5255f3b65bab5ba2089057
louisfd <louisfd94@gmail.com>
2024-08-28 16:09:41 -0400

    sm max in bytes

--- crates/cubecl-linalg/src/matmul/cmma/launch.rs
+++ crates/cubecl-linalg/src/matmul/cmma/launch.rs
@@ -31,13 +31,13 @@ pub fn matmul_cmma<R: Runtime, F: Float>(
 #[derive(Debug)]
 pub enum UnavailabilityReason {
     HighlyPermutatedInput,
-    ShapeMemoryLimitBusted,
+    SharedMemoryLimitBusted,
     InvalidConfig(String),
     CmmaInstructionsUnsupported,
 }
 
 /// Checks if the matmul cmma can be used.
-pub fn check_cmma_availability<R: Runtime>(
+pub fn check_cmma_availability<R: Runtime, F: Float>(
     client: &ComputeClient<R::Server, R::Channel>,
     config: Option<&CmmaLaunchConfig>,
 ) -> Result<(), UnavailabilityReason> {
@@ -66,16 +66,15 @@ pub fn check_cmma_availability<R: Runtime>(
             ));
         }
 
-        if b_k * max(b_m, b_n) > <R::Compiler as Compiler>::max_shared_memory_size() {
-            return Err(UnavailabilityReason::ShapeMemoryLimitBusted);
-        }
-
-        if b_m * b_n > <R::Compiler as Compiler>::max_shared_memory_size() {
-            return Err(UnavailabilityReason::ShapeMemoryLimitBusted);
+        if F::as_elem().size() * (b_m * b_n + b_m * b_k + b_n * b_k)
+            > <R::Compiler as Compiler>::max_shared_memory_size()
+        {
+            return Err(UnavailabilityReason::SharedMemoryLimitBusted);
         }
 
         let n_tiles = b_m * b_n / (CMMA_TILE_SIZE * CMMA_TILE_SIZE);
         let lane_dim = config.cube_dim_x * config.cube_dim_y / CMMA_COOP_DIM;
+
         if n_tiles % lane_dim != 0 {
             return Err(UnavailabilityReason::InvalidConfig(
                 "Must have an exact number of tiles per coop group.".to_string(),
