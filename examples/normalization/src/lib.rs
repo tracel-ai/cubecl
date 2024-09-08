469f73b43ecc276da6cae9698f2bf8b8f3224662
Rian Goossens <riangoossens@gmail.com>
2024-09-08 19:23:17 +0200

    updated norm example, showing a bug in magnitude for wgpu

--- examples/normalization/src/lib.rs
+++ examples/normalization/src/lib.rs
@@ -1,36 +1,44 @@
 use cubecl::prelude::*;
 
 #[cube(launch_unchecked)]
-fn norm_test<F: Float>(input: &Array<F>, output: &mut Array<F>) {
+fn norm_test<F: Float>(input: &Array<F>, output_a: &mut Array<F>, output_b: &mut Array<F>) {
     if ABSOLUTE_POS < input.len() {
-        let original_magnitude = F::magnitude(input[ABSOLUTE_POS]);
-        let normalized = F::normalize(input[ABSOLUTE_POS]);
-        let new_magnitude = F::magnitude(normalized);
-        output[ABSOLUTE_POS] = original_magnitude + new_magnitude;
+        output_a[ABSOLUTE_POS] = F::normalize(input[ABSOLUTE_POS]);
+        output_b[ABSOLUTE_POS] = input[ABSOLUTE_POS] / F::magnitude(input[ABSOLUTE_POS]);
     }
 }
 
 pub fn launch<R: Runtime>(device: &R::Device) {
     let client = R::client(device);
     let input = &[-1., 0., 1., 5.];
-    let output_handle = client.empty(2 * core::mem::size_of::<f32>());
     let input_handle = client.create(f32::as_bytes(input));
+    let output_a_handle = client.empty(input.len() * core::mem::size_of::<f32>());
+    let output_b_handle = client.empty(input.len() * core::mem::size_of::<f32>());
 
     unsafe {
         norm_test::launch_unchecked::<F32, R>(
             &client,
             CubeCount::Static(1, 1, 1),
             CubeDim::new(input.len() as u32, 1, 1),
-            ArrayArg::from_raw_parts(&input_handle, input.len(), 2),
-            ArrayArg::from_raw_parts(&output_handle, input.len(), 1),
+            ArrayArg::from_raw_parts(&input_handle, input.len(), 4),
+            ArrayArg::from_raw_parts(&output_a_handle, input.len(), 4),
+            ArrayArg::from_raw_parts(&output_b_handle, input.len(), 4),
         )
     };
 
-    let bytes = client.read(output_handle.binding());
+    let bytes = client.read(output_a_handle.binding());
     let output = f32::from_bytes(&bytes);
 
     println!(
         "Executed normalize with runtime {:?} => {output:?}",
+        R::name()
+    );
+
+    let bytes = client.read(output_b_handle.binding());
+    let output = f32::from_bytes(&bytes);
+
+    println!(
+        "Executed normalize using magnitude with runtime {:?} => {output:?}",
         R::name()
     );
 }
