44804d2a55899c44b823f84abe40396c41dccabe
Arthur Brussee <arthur.brussee@gmail.com>
2024-09-18 18:38:10 -0400

    Fix f postfix

--- crates/cubecl-wgpu/src/compiler/wgsl/base.rs
+++ crates/cubecl-wgpu/src/compiler/wgsl/base.rs
@@ -244,7 +244,7 @@ impl Display for Item {
 fn format_number(num: f64) -> String {
     let formatted = format!("{:.34}", num);
     let trimmed = formatted.trim_end_matches('0').trim_end_matches('.');
-    trimmed.to_string()
+    trimmed.to_string() + "f"
 }
 
 impl Display for Variable {
