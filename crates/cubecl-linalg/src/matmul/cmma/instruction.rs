a883f4208e51fc31e3c10d2a28a5e299661f7237
louisfd <louisfd94@gmail.com>
2024-09-30 13:14:21 -0400

    fix

--- crates/cubecl-linalg/src/matmul/cmma/instruction.rs
+++ crates/cubecl-linalg/src/matmul/cmma/instruction.rs
@@ -89,7 +89,7 @@ where
     }
 
     fn read_output<C: CubePrimitive>(out: &cmma::Matrix<O>, slice: &mut SliceMut<'_, C>) {
-        cmma::store(slice, out, 16, cmma::MatrixLayout::RowMajor);
+        cmma::store(slice, out, Self::M, cmma::MatrixLayout::RowMajor);
     }
 }
 
@@ -153,7 +153,7 @@ where
     }
 
     fn read_output<C: CubePrimitive>(out: &cmma::Matrix<O>, slice: &mut SliceMut<'_, C>) {
-        cmma::store(slice, out, 16, cmma::MatrixLayout::RowMajor);
+        cmma::store(slice, out, Self::M, cmma::MatrixLayout::RowMajor);
     }
 }
 
