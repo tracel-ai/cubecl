e5b3e01d510cbf0c7cb5135ef00592d4b08e0612
louisfd <louisfd94@gmail.com>
2024-09-30 13:06:29 -0400

    copy pasted a bit

--- crates/cubecl-linalg/src/matmul/cmma/instruction.rs
+++ crates/cubecl-linalg/src/matmul/cmma/instruction.rs
@@ -7,7 +7,17 @@ use half::{bf16, f16};
 use crate::matmul::matrix_layout::{as_cmma_layout, MatrixLayout};
 use crate::matmul::MatmulInstruction;
 
-pub struct CmmaInstruction<I: Numeric, O: Numeric> {
+pub struct CmmaInstruction16_16_16<I: Numeric, O: Numeric> {
+    _input: PhantomData<I>,
+    _output: PhantomData<O>,
+}
+
+pub struct CmmaInstruction32_8_16<I: Numeric, O: Numeric> {
+    _input: PhantomData<I>,
+    _output: PhantomData<O>,
+}
+
+pub struct CmmaInstruction8_32_16<I: Numeric, O: Numeric> {
     _input: PhantomData<I>,
     _output: PhantomData<O>,
 }
@@ -20,7 +30,7 @@ impl CmmaValid<bf16, f32> for (bf16, f32) {}
 pub struct CmmaInstructionConfig {}
 
 #[cube]
-impl<I: Numeric, O: Numeric> MatmulInstruction<I, O> for CmmaInstruction<I, O>
+impl<I: Numeric, O: Numeric> MatmulInstruction<I, O> for CmmaInstruction16_16_16<I, O>
 where
     (I, O): CmmaValid<I, O>,
 {
@@ -30,6 +40,134 @@ where
     type Out = cmma::Matrix<O>;
     const M: u32 = 16;
     const N: u32 = 16;
+    const K: u32 = 16;
+
+    fn execute(lhs: &Self::Lhs, rhs: &Self::Rhs, out: &mut Self::Out) {
+        cmma::execute::<I, I, O, O>(lhs, rhs, out, out);
+    }
+
+    fn init_lhs(#[comptime] layout: MatrixLayout) -> Self::Lhs {
+        cmma::Matrix::<I>::new(
+            cmma::MatrixIdent::A,
+            Self::M,
+            Self::N,
+            Self::K,
+            as_cmma_layout(layout),
+        )
+    }
+
+    fn init_rhs(#[comptime] layout: MatrixLayout) -> Self::Rhs {
+        cmma::Matrix::<I>::new(
+            cmma::MatrixIdent::B,
+            Self::M,
+            Self::N,
+            Self::K,
+            as_cmma_layout(layout),
+        )
+    }
+
+    fn fill_lhs<C: CubePrimitive>(slice: &Slice<'_, C>, lhs: &mut Self::Lhs) {
+        cmma::load(&lhs, slice, Self::K);
+    }
+
+    fn fill_rhs<C: CubePrimitive>(slice: &Slice<'_, C>, rhs: &mut Self::Rhs) {
+        cmma::load(&rhs, slice, Self::N);
+    }
+
+    fn init_output() -> cmma::Matrix<O> {
+        let out = cmma::Matrix::<O>::new(
+            cmma::MatrixIdent::Accumulator,
+            Self::M,
+            Self::N,
+            Self::K,
+            cmma::MatrixLayout::Undefined,
+        );
+
+        cmma::fill(&out, O::from_int(0));
+
+        out
+    }
+
+    fn read_output<C: CubePrimitive>(out: &cmma::Matrix<O>, slice: &mut SliceMut<'_, C>) {
+        cmma::store(slice, out, 16, cmma::MatrixLayout::RowMajor);
+    }
+}
+
+#[cube]
+impl<I: Numeric, O: Numeric> MatmulInstruction<I, O> for CmmaInstruction32_8_16<I, O>
+where
+    (I, O): CmmaValid<I, O>,
+{
+    type Config = CmmaInstructionConfig;
+    type Lhs = cmma::Matrix<I>;
+    type Rhs = cmma::Matrix<I>;
+    type Out = cmma::Matrix<O>;
+    const M: u32 = 32;
+    const N: u32 = 8;
+    const K: u32 = 16;
+
+    fn execute(lhs: &Self::Lhs, rhs: &Self::Rhs, out: &mut Self::Out) {
+        cmma::execute::<I, I, O, O>(lhs, rhs, out, out);
+    }
+
+    fn init_lhs(#[comptime] layout: MatrixLayout) -> Self::Lhs {
+        cmma::Matrix::<I>::new(
+            cmma::MatrixIdent::A,
+            Self::M,
+            Self::N,
+            Self::K,
+            as_cmma_layout(layout),
+        )
+    }
+
+    fn init_rhs(#[comptime] layout: MatrixLayout) -> Self::Rhs {
+        cmma::Matrix::<I>::new(
+            cmma::MatrixIdent::B,
+            Self::M,
+            Self::N,
+            Self::K,
+            as_cmma_layout(layout),
+        )
+    }
+
+    fn fill_lhs<C: CubePrimitive>(slice: &Slice<'_, C>, lhs: &mut Self::Lhs) {
+        cmma::load(&lhs, slice, Self::K);
+    }
+
+    fn fill_rhs<C: CubePrimitive>(slice: &Slice<'_, C>, rhs: &mut Self::Rhs) {
+        cmma::load(&rhs, slice, Self::N);
+    }
+
+    fn init_output() -> cmma::Matrix<O> {
+        let out = cmma::Matrix::<O>::new(
+            cmma::MatrixIdent::Accumulator,
+            Self::M,
+            Self::N,
+            Self::K,
+            cmma::MatrixLayout::Undefined,
+        );
+
+        cmma::fill(&out, O::from_int(0));
+
+        out
+    }
+
+    fn read_output<C: CubePrimitive>(out: &cmma::Matrix<O>, slice: &mut SliceMut<'_, C>) {
+        cmma::store(slice, out, 16, cmma::MatrixLayout::RowMajor);
+    }
+}
+
+#[cube]
+impl<I: Numeric, O: Numeric> MatmulInstruction<I, O> for CmmaInstruction8_32_16<I, O>
+where
+    (I, O): CmmaValid<I, O>,
+{
+    type Config = CmmaInstructionConfig;
+    type Lhs = cmma::Matrix<I>;
+    type Rhs = cmma::Matrix<I>;
+    type Out = cmma::Matrix<O>;
+    const M: u32 = 8;
+    const N: u32 = 32;
     const K: u32 = 16;
 
     fn execute(lhs: &Self::Lhs, rhs: &Self::Rhs, out: &mut Self::Out) {
--- crates/cubecl-linalg/src/tests/matmul/matmul_instruction.rs
+++ crates/cubecl-linalg/src/tests/matmul/matmul_instruction.rs
@@ -2,14 +2,16 @@
 #[macro_export]
 macro_rules! testgen_matmul_instruction_cmma_f16 {
     () => {
-        use cubecl_linalg::matmul::cmma::CmmaInstruction;
+        use cubecl_linalg::matmul::cmma::{
+            CmmaInstruction16_16_16, CmmaInstruction32_8_16, CmmaInstruction8_32_16,
+        };
         use cubecl_linalg::matmul::tests;
         use half::{bf16, f16};
 
         #[test]
         pub fn test_matmul_instruction_f16_in_f16_out() {
             tests::matmul_instruction::test_matmul_instruction::<
-                CmmaInstruction<f16, f16>,
+                CmmaInstruction16_16_16<f16, f16>,
                 f16,
                 f16,
                 TestRuntime,
@@ -19,7 +21,7 @@ macro_rules! testgen_matmul_instruction_cmma_f16 {
         #[test]
         pub fn test_matmul_instruction_f16_in_f32_out() {
             tests::matmul_instruction::test_matmul_instruction::<
-                CmmaInstruction<f16, f32>,
+                CmmaInstruction16_16_16<f16, f32>,
                 f16,
                 f32,
                 TestRuntime,
@@ -29,9 +31,29 @@ macro_rules! testgen_matmul_instruction_cmma_f16 {
         #[test]
         pub fn test_matmul_instruction_bf16_in_f32_out() {
             tests::matmul_instruction::test_matmul_instruction::<
-                CmmaInstruction<bf16, f32>,
+                CmmaInstruction16_16_16<bf16, f32>,
                 bf16,
                 f32,
+                TestRuntime,
+            >(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_instruction_32_8_16() {
+            tests::matmul_instruction::test_matmul_instruction::<
+                CmmaInstruction32_8_16<f16, f16>,
+                f16,
+                f16,
+                TestRuntime,
+            >(&Default::default())
+        }
+
+        #[test]
+        pub fn test_matmul_instruction_8_32_16() {
+            tests::matmul_instruction::test_matmul_instruction::<
+                CmmaInstruction8_32_16<f16, f16>,
+                f16,
+                f16,
                 TestRuntime,
             >(&Default::default())
         }
