dcafc42ee5f519081540987ac2f49797a954b0db
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2024-10-04 08:25:52 -0400

    Feat: slice as aligned (#154)

--- crates/cubecl-core/src/runtime_tests/slice.rs
+++ crates/cubecl-core/src/runtime_tests/slice.rs
@@ -10,14 +10,6 @@ pub fn slice_select(input: &Array<f32>, output: &mut Array<f32>) {
 }
 
 #[cube(launch)]
-pub fn slice_assign(input: &Array<f32>, output: &mut Array<f32>) {
-    if UNIT_POS == 0 {
-        let slice_1 = &mut output.slice_mut(2, 3);
-        slice_1[0] = input[0];
-    }
-}
-
-#[cube(launch)]
 pub fn slice_len(input: &Array<f32>, output: &mut Array<u32>) {
     if UNIT_POS == 0 {
         let slice = input.slice(2, 4);
@@ -36,6 +28,23 @@ pub fn slice_for(input: &Array<f32>, output: &mut Array<f32>) {
         }
 
         output[0] = sum;
+    }
+}
+
+#[cube(launch)]
+pub fn slice_mut_assign(input: &Array<f32>, output: &mut Array<f32>) {
+    if UNIT_POS == 0 {
+        let slice_1 = &mut output.slice_mut(2, 3);
+        slice_1[0] = input[0];
+    }
+}
+
+#[cube(launch)]
+pub fn slice_mut_len(output: &mut Array<u32>) {
+    if UNIT_POS == 0 {
+        let slice = output.slice_mut(0, 2).as_aligned();
+        let _tmp = slice[0]; // It must be used at least once, otherwise wgpu isn't happy.
+        output[0] = slice.len();
     }
 }
 
@@ -79,26 +88,6 @@ pub fn test_slice_len<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) 
     assert_eq!(actual, &[2]);
 }
 
-pub fn test_slice_assign<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
-    let input = client.create(f32::as_bytes(&[15.0]));
-    let output = client.create(f32::as_bytes(&[0.0, 1.0, 2.0, 3.0, 4.0]));
-
-    unsafe {
-        slice_assign::launch::<R>(
-            &client,
-            CubeCount::Static(1, 1, 1),
-            CubeDim::new(1, 1, 1),
-            ArrayArg::from_raw_parts(&input, 5, 1),
-            ArrayArg::from_raw_parts(&output, 1, 1),
-        )
-    };
-
-    let actual = client.read(output.binding());
-    let actual = f32::from_bytes(&actual);
-
-    assert_eq!(actual, &[0.0, 1.0, 15.0, 3.0, 4.0]);
-}
-
 pub fn test_slice_for<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
     let input = client.create(f32::as_bytes(&[0.0, 1.0, 2.0, 3.0, 4.0]));
     let output = client.create(f32::as_bytes(&[0.0]));
@@ -119,6 +108,44 @@ pub fn test_slice_for<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) 
     assert_eq!(actual[0], 5.0);
 }
 
+pub fn test_slice_mut_assign<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
+    let input = client.create(f32::as_bytes(&[15.0]));
+    let output = client.create(f32::as_bytes(&[0.0, 1.0, 2.0, 3.0, 4.0]));
+
+    unsafe {
+        slice_mut_assign::launch::<R>(
+            &client,
+            CubeCount::Static(1, 1, 1),
+            CubeDim::new(1, 1, 1),
+            ArrayArg::from_raw_parts(&input, 5, 1),
+            ArrayArg::from_raw_parts(&output, 1, 1),
+        )
+    };
+
+    let actual = client.read(output.binding());
+    let actual = f32::from_bytes(&actual);
+
+    assert_eq!(actual, &[0.0, 1.0, 15.0, 3.0, 4.0]);
+}
+
+pub fn test_slice_mut_len<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
+    let output = client.empty(core::mem::size_of::<u32>() * 4);
+
+    unsafe {
+        slice_mut_len::launch::<R>(
+            &client,
+            CubeCount::Static(1, 1, 1),
+            CubeDim::new(1, 1, 1),
+            ArrayArg::from_raw_parts(&output, 4, 1),
+        )
+    };
+
+    let actual = client.read(output.binding());
+    let actual = u32::from_bytes(&actual);
+
+    assert_eq!(actual[0], 2);
+}
+
 #[allow(missing_docs)]
 #[macro_export]
 macro_rules! testgen_slice {
@@ -132,12 +159,6 @@ macro_rules! testgen_slice {
         }
 
         #[test]
-        fn test_slice_assign() {
-            let client = TestRuntime::client(&Default::default());
-            cubecl_core::runtime_tests::slice::test_slice_assign::<TestRuntime>(client);
-        }
-
-        #[test]
         fn test_slice_len() {
             let client = TestRuntime::client(&Default::default());
             cubecl_core::runtime_tests::slice::test_slice_len::<TestRuntime>(client);
@@ -147,6 +168,18 @@ macro_rules! testgen_slice {
         fn test_slice_for() {
             let client = TestRuntime::client(&Default::default());
             cubecl_core::runtime_tests::slice::test_slice_for::<TestRuntime>(client);
+        }
+
+        #[test]
+        fn test_slice_mut_assign() {
+            let client = TestRuntime::client(&Default::default());
+            cubecl_core::runtime_tests::slice::test_slice_mut_assign::<TestRuntime>(client);
+        }
+
+        #[test]
+        fn test_slice_mut_len() {
+            let client = TestRuntime::client(&Default::default());
+            cubecl_core::runtime_tests::slice::test_slice_mut_len::<TestRuntime>(client);
         }
     };
 }
