d3f7dd86854343ea8181b8be01cc099b8be127a7
nathaniel <nathaniel.simard.42@gmail.com>
2024-10-15 17:52:24 -0400

    Future executor for no-std

--- crates/cubecl-common/src/future.rs
+++ crates/cubecl-common/src/future.rs
@@ -1,22 +1,32 @@
 use alloc::boxed::Box;
-use core::{any, future::Future};
+use core::future::Future;
 
 /// Block until the [future](Future) is completed and returns the result.
 pub fn block_on<O>(fut: impl Future<Output = O>) -> O {
-    futures::executor::block_on(fut)
+    #[cfg(not(feature = "std"))]
+    {
+        embassy_futures::block_on(fut)
+    }
+
+    #[cfg(feature = "std")]
+    {
+        futures::executor::block_on(fut)
+    }
 }
 
 /// Tries to catch panics within the future.
 pub async fn catch_unwind<O>(
     future: impl Future<Output = O>,
-) -> Result<O, Box<dyn any::Any + Send>> {
-    #[cfg(not(target_family = "wasm"))]
+) -> Result<O, Box<dyn core::any::Any + core::marker::Send>> {
+    #[cfg(all(not(target_family = "wasm"), feature = "std"))]
     {
         use core::panic::AssertUnwindSafe;
         use futures::FutureExt;
-
         AssertUnwindSafe(future).catch_unwind().await
     }
-    #[cfg(target_family = "wasm")]
-    Ok(future.await)
+
+    #[cfg(any(target_family = "wasm", not(feature = "std")))]
+    { 
+        Ok(future.await)
+    }
 }
