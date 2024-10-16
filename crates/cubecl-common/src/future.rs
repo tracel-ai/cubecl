ed136e2385b17e36680589f8a6245926f430f59f
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2024-10-16 15:13:29 -0400

    Fix no-std + wasm infinite loop (#176)

--- crates/cubecl-common/src/future.rs
+++ crates/cubecl-common/src/future.rs
@@ -3,12 +3,17 @@ use core::future::Future;
 
 /// Block until the [future](Future) is completed and returns the result.
 pub fn block_on<O>(fut: impl Future<Output = O>) -> O {
-    #[cfg(not(feature = "std"))]
+    #[cfg(target_family = "wasm")]
+    {
+        super::reader::read_sync(fut)
+    }
+
+    #[cfg(all(not(target_family = "wasm"), not(feature = "std")))]
     {
         embassy_futures::block_on(fut)
     }
 
-    #[cfg(feature = "std")]
+    #[cfg(all(not(target_family = "wasm"), feature = "std"))]
     {
         futures_lite::future::block_on(fut)
     }
--- crates/cubecl-common/src/reader.rs
+++ crates/cubecl-common/src/reader.rs
@@ -1,20 +1,4 @@
-#[cfg(target_has_atomic = "ptr")]
-use alloc::{sync::Arc, task::Wake};
-
-#[cfg(not(target_has_atomic = "ptr"))]
-use portable_atomic_util::{task::Wake, Arc};
-
-use core::{
-    future::Future,
-    task::{Context, Poll, Waker},
-};
-
-struct DummyWaker;
-
-impl Wake for DummyWaker {
-    fn wake(self: Arc<Self>) {}
-    fn wake_by_ref(self: &Arc<Self>) {}
-}
+use core::future::Future;
 
 /// Read a future synchronously.
 ///
@@ -30,21 +14,17 @@ pub fn read_sync<F: Future<Output = T>, T>(f: F) -> T {
 /// On WASM futures cannot block, so this only succeeds if the future returns immediately.
 /// otherwise this returns None.
 pub fn try_read_sync<F: Future<Output = T>, T>(f: F) -> Option<T> {
-    // Create a dummy context.
-    let waker = Waker::from(Arc::new(DummyWaker));
-    let mut context = Context::from_waker(&waker);
+    #[cfg(target_family = "wasm")]
+    {
+        use core::task::Poll;
 
-    // Pin & poll the future. Some backends don't do async readbacks, and instead immediately get
-    // the data. This let's us detect when a future is synchronous and doesn't require any waiting.
-    let mut pinned = core::pin::pin!(f);
-
-    match pinned.as_mut().poll(&mut context) {
-        Poll::Ready(output) => Some(output),
-        // On platforms that support it, now just block on the future and drive it to completion.
-        #[cfg(all(not(target_family = "wasm"), feature = "std"))]
-        Poll::Pending => Some(super::future::block_on(pinned)),
-        // Otherwise, just bail and return None - this futures will have to be read back asynchronously.
-        #[cfg(any(target_family = "wasm", not(feature = "std")))]
-        Poll::Pending => None,
+        match embassy_futures::poll_once(f) {
+            Poll::Ready(output) => Some(output),
+            _ => None,
+        }
+    }
+    #[cfg(not(target_family = "wasm"))]
+    {
+        Some(super::future::block_on(f))
     }
 }
