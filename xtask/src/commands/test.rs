9980da1ae134ec46cff8d806a70c3adb285e5dff
syl20bnr <sylvain.benner@tracel.ai>
2024-10-18 10:51:21 -0400

    Exclude cubecl-hip from xtask commands

--- xtask/src/commands/test.rs
+++ xtask/src/commands/test.rs
@@ -10,7 +10,8 @@ pub struct CubeCLTestCmdArgs {
 pub(crate) fn handle_command(mut args: CubeCLTestCmdArgs) -> anyhow::Result<()> {
     if args.ci {
         // Exclude crates that are not supported on CI
-        args.exclude.extend(vec!["cubecl-cuda".to_string()]);
+        args.exclude
+            .extend(vec!["cubecl-cuda".to_string(), "cubecl-hip".to_string()]);
     }
     base_commands::test::handle_command(args.try_into().unwrap())?;
     Ok(())
