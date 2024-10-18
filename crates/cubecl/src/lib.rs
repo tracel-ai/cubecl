609183cccb26e2ccaa834f2230b5b8f38f9ea507
nathaniel <nathaniel.simard.42@gmail.com>
2024-10-18 14:51:45 -0400

    Export HIP

--- crates/cubecl/src/lib.rs
+++ crates/cubecl/src/lib.rs
@@ -9,5 +9,8 @@ pub use cubecl_wgpu_spirv as wgpu_spirv;
 #[cfg(feature = "cuda")]
 pub use cubecl_cuda as cuda;
 
+#[cfg(feature = "hip")]
+pub use cubecl_hip as hip;
+
 #[cfg(feature = "linalg")]
 pub use cubecl_linalg as linalg;
