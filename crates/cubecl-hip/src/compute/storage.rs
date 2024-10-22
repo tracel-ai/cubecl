6046e60e7d23e8478bfd8b55bb6bb8ae96d674da
ArthurBrussee <arthur.brussee@gmail.com>
2024-10-22 22:07:24 +0100

    Hip hip

--- crates/cubecl-hip/src/compute/storage.rs
+++ crates/cubecl-hip/src/compute/storage.rs
@@ -76,7 +76,7 @@ impl ComputeStorage for HipStorage {
         let offset = handle.offset();
         let size = handle.size();
 
-        let ptr = ptr + offset as usize;
+        let ptr = ptr.wrapping_add(offset as usize);
         let key = ActiveResource::new(ptr);
 
         self.activate_slices
