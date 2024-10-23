5ca69327cffb45d8b426e407c0397bcf9d45fd93
syl20bnr <sylvain.benner@tracel.ai>
2024-10-23 13:16:42 -0400

    Fix includes for HIP fp16 and bfp16

--- crates/cubecl-cpp/src/hip/mod.rs
+++ crates/cubecl-cpp/src/hip/mod.rs
@@ -5,10 +5,10 @@ pub struct Hip;
 
 impl Dialect for Hip {
     fn include_f16(f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("#include <hip_fp16.h>\n")
+        f.write_str("#include <hip/hip_fp16.h>\n")
     }
     fn include_bf16(f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("#include <hip_bfp16.h>\n")
+        f.write_str("#include <hip/hip_bfloat16.h>\n")
     }
     fn include_wmma(f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str("#include <mma.h>\n")
