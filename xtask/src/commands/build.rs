29b7af234e49130eaba3c384a1cf685df099d210
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2024-10-30 15:21:29 +0100

    Ensure SPIR-V compiler is built for `cubecl-wgpu`

--- xtask/src/commands/build.rs
+++ xtask/src/commands/build.rs
@@ -14,5 +14,14 @@ pub(crate) fn handle_command(mut args: CubeCLBuildCmdArgs) -> anyhow::Result<()>
             .extend(vec!["cubecl-cuda".to_string(), "cubecl-hip".to_string()]);
     }
     base_commands::build::handle_command(args.try_into().unwrap())?;
+    // Specific additional commands to test specific features
+    // burn-wgpu with SPIR-V
+    helpers::custom_crates_build(
+        vec!["cubecl-wgpu"],
+        vec!["--features", "spirv"],
+        None,
+        None,
+        "std with all features",
+    )?;
     Ok(())
 }
