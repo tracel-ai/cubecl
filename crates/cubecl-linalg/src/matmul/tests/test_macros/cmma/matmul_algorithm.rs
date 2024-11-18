f5151d7764c08c64cb66d7dc79b55d284f720d91
louisfd <louisfd94@gmail.com>
2024-11-18 12:13:24 -0500

    add large test

--- crates/cubecl-linalg/src/matmul/tests/test_macros/cmma/matmul_algorithm.rs
+++ crates/cubecl-linalg/src/matmul/tests/test_macros/cmma/matmul_algorithm.rs
@@ -20,6 +20,62 @@ macro_rules! matmul_test_define {
         $plane_dim:expr
     ) => {
         #[test]
+        pub fn bo3x4_gpc300x300x300_s4x4x2_t16x16x16_cc_ln4() {
+            let problem = MatmulProblem {
+                m: 300,
+                n: 300,
+                k: 300,
+                batches: vec![3, 4],
+                lhs_layout: MatrixLayout::ColMajor,
+                rhs_layout: MatrixLayout::ColMajor,
+                lhs_line_size: 4,
+                rhs_line_size: 4,
+                out_line_size: 4,
+            };
+
+            struct Test {}
+            impl matmul::Algorithm<$eg> for Test {
+                const PLANE_DIM: u32 = $plane_dim;
+                type EG = $eg;
+                type ES = $es;
+                type EA = $ea;
+                type StageSize = S4x4x2;
+
+                type TileMatmul = $t_16x16x16<Self::ES, Self::EA>;
+                type StageMatmul = stage::single_buffer::Matmul<
+                    Self::ES,
+                    Self::EG,
+                    Self::EA,
+                    Self::TileMatmul,
+                    Self::StageSize,
+                >;
+                type GlobalMatmul =
+                    global::producer_consumer::Matmul<Self::EG, Self::ES, Self::StageMatmul>;
+                type BatchMatmul =
+                    batch::one_to_one::Matmul<Self::EG, Self::ES, Self::GlobalMatmul>;
+
+                fn cube_dim() -> CubeDim {
+                    CubeDim::new($plane_dim, 8, 1)
+                }
+                fn cube_count(_problem: &MatmulProblem) -> CubeCount {
+                    CubeCount::Static(5, 5, 12)
+                }
+            }
+
+            let advanced_config = AdvancedConfig {
+                lhs_tiling_order: TilingOrderConfig::ColMajor,
+                rhs_tiling_order: TilingOrderConfig::RowMajor,
+                ..Default::default()
+            };
+
+            test_matmul_algorithm::<Test, $eg, $es, TestRuntime>(
+                problem,
+                advanced_config,
+                &<<TestRuntime as Runtime>::Device>::default(),
+            );
+        }
+
+        #[test]
         pub fn bo1_gpc16x32x32_s1x2x2_t16x16x16_rr_ln4() {
             let problem = MatmulProblem {
                 m: 16,
