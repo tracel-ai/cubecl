e7caab5303b15ec0d5d3cd4c020bf5224444f7a7
maxime <t.maxime@pm.me>
2024-11-19 09:46:18 -0500

    Optimize warp operations for different vectorization

--- crates/cubecl-cpp/src/shared/warp.rs
+++ crates/cubecl-cpp/src/shared/warp.rs
@@ -56,55 +56,17 @@ unsigned int leader = __ffs(mask) - 1;
             "
             ),
             WarpInstruction::All { input, out } => {
-                write!(
-                    f,
-                    "
-                        {out} = {input};
-                    "
-                )?;
-                for k in 0..out.item().vectorization {
-                    let __all = D::warp_all_indexed(out, k);
-                    let out_indexed = out.index(k);
-                    write!(
-                        f,
-                        "
-                        {{
-                            for (int offset = 1; offset < warpSizeChecked; offset *=2 ) {{
-                                {out_indexed} = {__all};
-                            }}
-                        }}
-                        "
-                    )?;
-                }
-                Ok(())
+                reduce_quantifier(f, input, out, D::warp_all_indexed)
             }
             WarpInstruction::Any { input, out } => {
-                write!(
-                    f,
-                    "
-                        {out} = {input};
-                    "
-                )?;
-                for k in 0..out.item().vectorization {
-                    let __any = D::warp_any_indexed(out, k);
-                    let out_indexed = out.index(k);
-                    write!(
-                        f,
-                        "
-                        {{
-                            for (int offset = 1; offset < warpSizeChecked; offset *=2 ) {{
-                                {out_indexed} = {__any};
-                            }}
-                        }}
-                        "
-                    )?;
-                }
-                Ok(())
+                reduce_quantifier(f, input, out, D::warp_any_indexed)
             }
             WarpInstruction::Broadcast { input, id, out } => {
-                for k in 0..out.item().vectorization {
-                    let __shfl = D::warp_shuffle_indexed(input, k, id);
-                    let out_indexed = out.index(k);
+                let input_optimized = input.optimized();
+                let out_optimized = out.optimized();
+                for k in 0..out_optimized.item().vectorization {
+                    let __shfl = D::warp_shuffle_indexed(&input_optimized, k, id);
+                    let out_indexed = out_optimized.index(k);
                     write!(
                         f,
                         "
@@ -134,9 +96,12 @@ fn reduce_operator<D: Dialect>(
         {out} = {input};
         "
     )?;
-    for k in 0..out.item().vectorization {
-        let __shfl_xor = D::warp_shuffle_xor_indexed(out, k);
-        let out_indexed = out.index(k);
+
+    let optimized = out.optimized();
+
+    for k in 0..optimized.item().vectorization {
+        let __shfl_xor = D::warp_shuffle_xor_indexed(&optimized, k);
+        let out_indexed = optimized.index(k);
         write!(
             f,
             "
@@ -163,20 +128,54 @@ fn reduce_comparison<D: Dialect>(
         {out} = {input};
         "
     )?;
-    let instruction = match out.elem() {
+
+    let optimized = out.optimized();
+
+    let instruction = match optimized.elem() {
         Elem::F16 | Elem::BF16 => format!("__h{cmp}"),
         Elem::F162 | Elem::BF162 => format!("__h{cmp}2"),
         _ => cmp.to_string(),
     };
-    for k in 0..out.item().vectorization {
-        let __shfl_down = D::warp_shuffle_down_indexed(out, k);
-        let out_indexed = out.index(k);
+
+    for k in 0..optimized.item().vectorization {
+        let __shfl_down = D::warp_shuffle_down_indexed(&optimized, k);
+        let out_indexed = optimized.index(k);
         write!(
             f,
             "
             {{
                 for (int offset = warpSizeChecked / 2; offset > 0; offset /= 2) {{
                     {out_indexed} = {instruction}({out_indexed}, {__shfl_down});
+                }}
+            }}
+            "
+        )?;
+    }
+    Ok(())
+}
+
+fn reduce_quantifier<D: Dialect, Q: Fn(&Variable<D>, usize) -> String>(
+    f: &mut core::fmt::Formatter<'_>,
+    input: &Variable<D>,
+    out: &Variable<D>,
+    quantifier: Q,
+) -> core::fmt::Result {
+    write!(
+        f,
+        "
+            {out} = {input};
+        "
+    )?;
+    let optimized = out.optimized();
+    for k in 0..optimized.item().vectorization {
+        let __all = quantifier(&optimized, k);
+        let out_indexed = optimized.index(k);
+        write!(
+            f,
+            "
+            {{
+                for (int offset = 1; offset < warpSizeChecked; offset *=2 ) {{
+                    {out_indexed} = {__all};
                 }}
             }}
             "
