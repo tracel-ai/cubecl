e8dc0f0af4cfaedc813966f5e4b32e2daefab940
maxime <t.maxime@pm.me>
2024-11-29 11:49:46 -0500

    Add tests for reduce shared and improve rng

--- crates/cubecl-reduce/src/test.rs
+++ crates/cubecl-reduce/src/test.rs
@@ -2,20 +2,37 @@
 
 use cubecl_core as cubecl;
 use cubecl_core::prelude::*;
+use rand::{
+    distributions::{Distribution, Uniform},
+    rngs::StdRng,
+    SeedableRng,
+};
 
 use crate::{
-    reduce_naive, ReduceArgMax, ReduceArgMin, ReduceMean, ReduceNaiveInstruction, ReduceProd,
-    ReduceSum,
+    reduce_naive, reduce_shared, ReduceArgMax, ReduceArgMin, ReduceMean, ReduceNaiveInstruction,
+    ReduceProd, ReduceSharedInstruction, ReduceSum,
 };
 
 // Simple kernel to launch tests.
 #[cube(launch_unchecked)]
-pub fn naive_reduce_dim_kernel<I: Numeric, O: Numeric, R: ReduceNaiveInstruction<I>>(
+pub fn kernel_reduce_naive<I: Numeric, O: Numeric, R: ReduceNaiveInstruction<I>>(
     input: &Tensor<Line<I>>,
     output: &mut Tensor<Line<O>>,
     dim: u32,
 ) {
     reduce_naive::<R, I, O>(input, output, dim)
+}
+
+// Simple kernel to launch tests.
+#[cube(launch_unchecked)]
+pub fn kernel_reduce_shared<I: Numeric, O: Numeric, R: ReduceSharedInstruction<I>>(
+    input: &Tensor<Line<I>>,
+    output: &mut Tensor<Line<O>>,
+    reduce_dim: u32,
+    #[comptime] cube_dim: u32,
+    #[comptime] exact_shape: bool,
+) {
+    reduce_shared::<R, I, O>(input, output, reduce_dim, cube_dim, exact_shape)
 }
 
 // This macro generate all the tests.
@@ -41,6 +58,7 @@ macro_rules! testgen_reduce {
         use cubecl_core::prelude::CubeCount;
 
         $crate::impl_test_reduce!(
+            naive,
             $float,
             [
                 {
@@ -99,6 +117,67 @@ macro_rules! testgen_reduce {
                 }
             ]
         );
+
+        $crate::impl_test_reduce!(
+            shared,
+            $float,
+            [
+                {
+                    id: "reduce_columns_small_matrix_row_major",
+                    shape: [4, 8],
+                    stride: [8, 1],
+                    reduce_dim: 0,
+                    cube_count: CubeCount::Static(8, 1, 1),
+                    cube_dim: CubeDim::new(2, 1, 1),
+                    line_size: 1,
+                },
+                {
+                    id: "reduce_columns_large_matrix_row_major",
+                    shape: [8, 256],
+                    stride: [256, 1],
+                    reduce_dim: 1,
+                    cube_count: CubeCount::Static(8, 1, 1),
+                    cube_dim: CubeDim::new(16, 1, 1),
+                    line_size: 1,
+                },
+                {
+                    id: "reduce_rows_large_matrix_row_major_non_power_two_cube_dim",
+                    shape: [16, 256],
+                    stride: [256, 1],
+                    reduce_dim: 0,
+                    cube_count: CubeCount::Static(256, 1, 1),
+                    cube_dim: CubeDim::new(5, 1, 1),
+                    line_size: 1,
+                },
+                {
+                    id: "rank_three_tensor",
+                    shape: [16, 16, 16],
+                    stride: [1, 256, 16],
+                    reduce_dim: 2,
+                    cube_count: CubeCount::Static(16, 16, 1),
+                    cube_dim: CubeDim::new(4, 1, 1),
+                    line_size: 1,
+                },
+                {
+                    id: "rank_three_tensor_unexact_shape",
+                    shape: [11, 12, 13],
+                    stride: [156, 13, 1],
+                    reduce_dim: 1,
+                    cube_count: CubeCount::Static(11, 1, 13),
+                    cube_dim: CubeDim::new(2, 1, 1),
+                    line_size: 1,
+                },
+                {
+                    id: "reduce_rows_large_matrix_row_major_line_size_four",
+                    shape: [32, 64],
+                    stride: [64, 1],
+                    reduce_dim: 0,
+                    cube_count: CubeCount::Static(64, 1, 1),
+                    cube_dim: CubeDim::new(8, 1, 1),
+                    line_size: 4,
+                }
+            ]
+        );
     };
 }
 
@@ -109,6 +188,7 @@ macro_rules! testgen_reduce {
 #[macro_export]
 macro_rules! impl_test_reduce {
     (
+        $kind:ident,
         $float:ident,
         [
             $(
@@ -126,7 +206,7 @@ macro_rules! impl_test_reduce {
         ::paste::paste! {
             $(
                 #[test]
-                pub fn [< reduce_sum_dim_naive_ $id >]() {
+                pub fn [< reduce_sum_dim_ $kind _ $id >]() {
                     let test = TestCase {
                         shape: $shape.into(),
                         stride: $stride.into(),
@@ -135,11 +215,11 @@ macro_rules! impl_test_reduce {
                         cube_dim: $cube_dim,
                         line_size:$line_size
                     };
-                    test.test_sum_dim_naive::<$float, TestRuntime>(&Default::default());
+                    test.[< test_sum_dim_ $kind >]::<$float, TestRuntime>(&Default::default());
                 }
 
                 #[test]
-                pub fn [< reduce_prod_dim_naive_ $id >]() {
+                pub fn [< reduce_prod_dim_ $kind _ $id >]() {
                     let test = TestCase {
                         shape: $shape.into(),
                         stride: $stride.into(),
@@ -148,11 +228,11 @@ macro_rules! impl_test_reduce {
                         cube_dim: $cube_dim,
                         line_size:$line_size
                     };
-                    test.test_prod_dim_naive::<$float, TestRuntime>(&Default::default());
+                    test.[< test_prod_dim_ $kind >]::<$float, TestRuntime>(&Default::default());
                 }
 
                 #[test]
-                pub fn [< reduce_mean_dim_naive_ $id >]() {
+                pub fn [< reduce_mean_dim_ $kind _ $id >]() {
                     let test = TestCase {
                         shape: $shape.into(),
                         stride: $stride.into(),
@@ -161,11 +241,11 @@ macro_rules! impl_test_reduce {
                         cube_dim: $cube_dim,
                         line_size:$line_size
                     };
-                    test.test_mean_dim_naive::<$float, TestRuntime>(&Default::default());
+                    test.[< test_mean_dim_ $kind >]::<$float, TestRuntime>(&Default::default());
                 }
 
                 #[test]
-                pub fn [< reduce_argmax_dim_naive_ $id >]() {
+                pub fn [< reduce_argmax_dim_ $kind _ $id >]() {
                     let test = TestCase {
                         shape: $shape.into(),
                         stride: $stride.into(),
@@ -174,11 +254,11 @@ macro_rules! impl_test_reduce {
                         cube_dim: $cube_dim,
                         line_size:$line_size
                     };
-                    test.test_argmax_dim_naive::<$float, TestRuntime>(&Default::default());
+                    test.[< test_argmax_dim_ $kind >]::<$float, TestRuntime>(&Default::default());
                 }
 
                 #[test]
-                pub fn [< reduce_argmin_dim_naive_ $id >]() {
+                pub fn [< reduce_argmin_dim_ $kind _ $id >]() {
                     let test = TestCase {
                         shape: $shape.into(),
                         stride: $stride.into(),
@@ -187,7 +267,7 @@ macro_rules! impl_test_reduce {
                         cube_dim: $cube_dim,
                         line_size:$line_size
                     };
-                    test.test_argmin_dim_naive::<$float, TestRuntime>(&Default::default());
+                    test.[< test_argmin_dim_ $kind >]::<$float, TestRuntime>(&Default::default());
                 }
             )*
         }
@@ -212,7 +292,7 @@ impl TestCase {
     {
         let input_values: Vec<F> = self.random_input_values();
         let expected_values = self.cpu_sum_dim(&input_values);
-        self.run_test::<F, F, R, ReduceSum>(device, input_values, expected_values)
+        self.run_test_naive::<F, F, R, ReduceSum>(device, input_values, expected_values)
     }
 
     pub fn test_prod_dim_naive<F, R>(&self, device: &R::Device)
@@ -222,7 +302,7 @@ impl TestCase {
     {
         let input_values: Vec<F> = self.random_input_values();
         let expected_values = self.cpu_prod_dim(&input_values);
-        self.run_test::<F, F, R, ReduceProd>(device, input_values, expected_values)
+        self.run_test_naive::<F, F, R, ReduceProd>(device, input_values, expected_values)
     }
 
     pub fn test_mean_dim_naive<F, R>(&self, device: &R::Device)
@@ -232,7 +312,7 @@ impl TestCase {
     {
         let input_values: Vec<F> = self.random_input_values();
         let expected_values = self.cpu_mean_dim(&input_values);
-        self.run_test::<F, F, R, ReduceMean>(device, input_values, expected_values)
+        self.run_test_naive::<F, F, R, ReduceMean>(device, input_values, expected_values)
     }
 
     pub fn test_argmax_dim_naive<F, R>(&self, device: &R::Device)
@@ -242,7 +322,7 @@ impl TestCase {
     {
         let input_values: Vec<F> = self.random_input_values();
         let expected_values = self.cpu_argmax_dim(&input_values);
-        self.run_test::<F, u32, R, ReduceArgMax>(device, input_values, expected_values)
+        self.run_test_naive::<F, u32, R, ReduceArgMax>(device, input_values, expected_values)
     }
 
     pub fn test_argmin_dim_naive<F, R>(&self, device: &R::Device)
@@ -252,10 +332,10 @@ impl TestCase {
     {
         let input_values: Vec<F> = self.random_input_values();
         let expected_values = self.cpu_argmin_dim(&input_values);
-        self.run_test::<F, u32, R, ReduceArgMin>(device, input_values, expected_values)
+        self.run_test_naive::<F, u32, R, ReduceArgMin>(device, input_values, expected_values)
     }
 
-    pub fn run_test<I, O, R, K>(
+    pub fn run_test_naive<I, O, R, K>(
         &self,
         device: &R::Device,
         input_values: Vec<I>,
@@ -292,7 +372,7 @@ impl TestCase {
                 self.line_size,
             );
 
-            naive_reduce_dim_kernel::launch_unchecked::<I, O, K, R>(
+            kernel_reduce_naive::launch_unchecked::<I, O, K, R>(
                 &client,
                 self.cube_count.clone(),
                 self.cube_dim,
@@ -306,7 +386,115 @@ impl TestCase {
         let bytes = client.read_one(binding);
         let output_values = O::from_bytes(&bytes);
 
-        assert_approx_equal_abs(output_values, &expected_values, 1e-7);
+        assert_approx_equal_abs(output_values, &expected_values, 1e-6);
+    }
+
+    pub fn test_sum_dim_shared<F, R>(&self, device: &R::Device)
+    where
+        F: Float + CubeElement + std::fmt::Display,
+        R: Runtime,
+    {
+        let input_values: Vec<F> = self.random_input_values();
+        let expected_values = self.cpu_sum_dim(&input_values);
+        self.run_test_shared::<F, F, R, ReduceSum>(device, input_values, expected_values)
+    }
+
+    pub fn test_prod_dim_shared<F, R>(&self, device: &R::Device)
+    where
+        F: Float + CubeElement + std::fmt::Display,
+        R: Runtime,
+    {
+        let input_values: Vec<F> = self.random_input_values();
+        let expected_values = self.cpu_prod_dim(&input_values);
+        self.run_test_shared::<F, F, R, ReduceProd>(device, input_values, expected_values)
+    }
+
+    pub fn test_mean_dim_shared<F, R>(&self, device: &R::Device)
+    where
+        F: Float + CubeElement + std::fmt::Display,
+        R: Runtime,
+    {
+        let input_values: Vec<F> = self.random_input_values();
+        let expected_values = self.cpu_mean_dim(&input_values);
+        self.run_test_shared::<F, F, R, ReduceMean>(device, input_values, expected_values)
+    }
+
+    pub fn test_argmax_dim_shared<F, R>(&self, device: &R::Device)
+    where
+        F: Float + CubeElement + std::fmt::Display,
+        R: Runtime,
+    {
+        let input_values: Vec<F> = self.random_input_values();
+        let expected_values = self.cpu_argmax_dim(&input_values);
+        self.run_test_shared::<F, u32, R, ReduceArgMax>(device, input_values, expected_values)
+    }
+
+    pub fn test_argmin_dim_shared<F, R>(&self, device: &R::Device)
+    where
+        F: Float + CubeElement + std::fmt::Display,
+        R: Runtime,
+    {
+        let input_values: Vec<F> = self.random_input_values();
+        let expected_values = self.cpu_argmin_dim(&input_values);
+        self.run_test_shared::<F, u32, R, ReduceArgMin>(device, input_values, expected_values)
+    }
+
+    pub fn run_test_shared<I, O, R, K>(
+        &self,
+        device: &R::Device,
+        input_values: Vec<I>,
+        expected_values: Vec<O>,
+    ) where
+        I: Numeric + CubeElement + std::fmt::Display,
+        O: Numeric + CubeElement + std::fmt::Display,
+        R: Runtime,
+        K: ReduceSharedInstruction<I>,
+    {
+        let client = R::client(device);
+        let input_handle = client.create(I::as_bytes(&input_values));
+
+        // Zero initialize a tensor with the same shape as input
+        // except for the `self.reduce_dim` axis where the shape is 1.
+        let output_handle =
+            client.create(O::as_bytes(&vec![O::from_int(0); expected_values.len()]));
+        let mut output_shape = self.shape.clone();
+        output_shape[self.reduce_dim as usize] = 1;
+        let output_stride = self.output_stride();
+
+        let exact_shape =
+            self.shape[self.reduce_dim as usize] % self.cube_dim.num_elems() as usize == 0;
+
+        unsafe {
+            let input_tensor = TensorArg::from_raw_parts::<I>(
+                &input_handle,
+                &self.stride,
+                &self.shape,
+                self.line_size,
+            );
+            let output_tensor = TensorArg::from_raw_parts::<O>(
+                &output_handle,
+                &output_stride,
+                &output_shape,
+                self.line_size,
+            );
+
+            kernel_reduce_shared::launch_unchecked::<I, O, K, R>(
+                &client,
+                self.cube_count.clone(),
+                self.cube_dim,
+                input_tensor,
+                output_tensor,
+                ScalarArg::new(self.reduce_dim),
+                self.cube_dim.num_elems(),
+                exact_shape,
+            );
+        }
+
+        let binding = output_handle.binding();
+        let bytes = client.read_one(binding);
+        let output_values = O::from_bytes(&bytes);
+
+        assert_approx_equal_abs(output_values, &expected_values, 1.0 / 4.0);
     }
 
     fn cpu_sum_dim<F: Float>(&self, values: &[F]) -> Vec<F> {
@@ -410,18 +598,19 @@ impl TestCase {
 
     fn random_input_values<F: Float>(&self) -> Vec<F> {
         let size = self.shape.iter().product::<usize>() * self.line_size as usize;
+        let rng = StdRng::seed_from_u64(self.pseudo_random_seed());
+        let distribution = Uniform::new_inclusive(-8, 8);
+        let factor = 1.0 / 8.0;
+        distribution
+            .sample_iter(rng)
+            .take(size)
+            .map(|r| F::new(r as f32 * factor))
+            .collect()
+    }
 
-        fn lcg(seed: &mut u64) -> f32 {
-            const A: u64 = 1664525;
-            const C: u64 = 1013904223;
-            const M: f64 = 2u64.pow(32) as f64;
-
-            *seed = (A.wrapping_mul(*seed).wrapping_add(C)) % (1u64 << 32);
-            (*seed as f64 / M * 2.0 - 1.0) as f32
-        }
-
-        let mut seed = 123456789; // Not really important for testing.
-        (0..size).map(|_| F::new(lcg(&mut seed))).collect()
+    // We don't need a fancy crypto-secure seed as this is only for testing.
+    fn pseudo_random_seed(&self) -> u64 {
+        (self.stride.len() * self.shape[0]) as u64 ^ self.cube_dim.num_elems() as u64
     }
 }
 
