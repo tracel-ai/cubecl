9a67e240e3471b28c35e286e5e297371eafeb09c
maxime <t.maxime@pm.me>
2024-12-05 15:10:36 -0500

    remove dead code

--- crates/cubecl-reduce/src/instructions/base.rs
+++ crates/cubecl-reduce/src/instructions/base.rs
@@ -24,37 +24,3 @@ pub trait Reduce<In: Numeric>: Send + Sync + 'static {
         shape_axis_reduce: u32,
     ) -> Line<Out>;
 }
-
-// #[cube]
-// pub trait ReduceShared<In: Numeric> {
-//     type Accumulator: CubeType;
-//     type AccumulatorItem: CubeType;
-
-//     fn create_accumulator(#[comptime] length: u32, #[comptime] line_size: u32)
-//         -> Self::Accumulator;
-
-//     fn init_accumulator(
-//         accumulator: &mut Self::Accumulator,
-//         index: u32,
-//         #[comptime] line_size: u32,
-//     );
-
-//     fn reduce(
-//         accumulator: &mut Self::Accumulator,
-//         destination: u32,
-//         item: Line<In>,
-//         coordinate: Line<u32>,
-//         #[comptime] use_plane: bool,
-//     );
-
-//     fn fuse_accumulator(accumulator: &mut Self::Accumulator, destination: u32, origin: u32);
-
-//     fn get_first(accumulator: Self::Accumulator) -> Self::AccumulatorItem;
-
-//     fn merge_line<Out: Numeric>(accumulator: Self::AccumulatorItem, shape_axis_reduce: u32) -> Out;
-
-//     fn to_output_parallel<Out: Numeric>(
-//         accumulator: Self::AccumulatorItem,
-//         shape_axis_reduce: u32,
-//     ) -> Line<Out>;
-// }
