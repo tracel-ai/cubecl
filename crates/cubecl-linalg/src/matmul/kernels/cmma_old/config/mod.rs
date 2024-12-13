c5d28522c015cb03ea7f9792ca4611bca6bb776e
Louis Fortier-Dubois <louisfd94@gmail.com>
2024-12-13 14:53:46 -0500

    delete cmma old (#372)
    * delete cmma old
    
    * fmt

--- crates/cubecl-linalg/src/matmul/kernels/cmma_old/config/mod.rs
+++
@@ -1,7 +0,0 @@
-mod base;
-mod predefined;
-mod strategy;
-
-pub use base::*;
-pub use predefined::PredefinedCmmaConfig;
-pub(crate) use strategy::*;
--- crates/cubecl-linalg/src/matmul/kernels/cmma_old/epilogue/mod.rs
+++
@@ -1,3 +0,0 @@
-pub(crate) mod base;
-pub(crate) mod large_smem;
-pub(crate) mod reuse_smem;
