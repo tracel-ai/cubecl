19961b7e25692e6a4e9449c8a8a4169143d12c4b
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2024-12-31 17:31:42 +0100

    cast __popcll to long long

--- crates/cubecl-cpp/src/shared/unary.rs
+++ crates/cubecl-cpp/src/shared/unary.rs
@@ -179,7 +179,7 @@ impl<D: Dialect> Unary<D> for CountBits {
     ) -> std::fmt::Result {
         match elem {
             Elem::I32 | Elem::U32 => write!(f, "__popc({input})"),
-            Elem::I64 | Elem::U64 => write!(f, "__popcll({input})"),
+            Elem::I64 | Elem::U64 => write!(f, "{elem}(__popcll({input}))"),
             _ => write!(f, "{}(__popc({}))", elem, zero_extend(input, elem)),
         }
     }
