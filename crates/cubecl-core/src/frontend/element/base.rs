7e02e76704ee5d5ac7bf502d4955999db1c37c86
Maxime Tremblay <t.maxime@pm.me>
2025-01-07 11:31:41 -0500

    Fix magnitude cuda (#398)
    * add missing fmt_left
    
    * add comments

--- crates/cubecl-core/src/frontend/element/base.rs
+++ crates/cubecl-core/src/frontend/element/base.rs
@@ -31,6 +31,8 @@ pub trait CubeType {
 }
 
 /// Trait useful for cube types that are also used with comptime.
+///
+/// This is used to set a variable as mutable. (Need to be fixed or at least renamed.)
 pub trait IntoRuntime: CubeType + Sized {
     /// Make sure a type is actually expanded into its runtime [expand type](CubeType::ExpandType).
     fn runtime(self) -> Self {
