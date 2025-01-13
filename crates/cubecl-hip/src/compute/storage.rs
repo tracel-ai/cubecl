d3b8e6e5c4d572b2874e4ba5f6b224ae3fe7c629
Arthur Brussee <arthur.brussee@gmail.com>
2025-01-13 17:19:35 +0000

    HIP

--- crates/cubecl-hip/src/compute/storage.rs
+++ crates/cubecl-hip/src/compute/storage.rs
@@ -70,28 +70,27 @@ impl ComputeStorage for HipStorage {
 
     type Resource = HipResource;
 
-    fn get(&mut self, handle: &StorageHandle) -> Option<Self::Resource> {
-        self.memory.get(&handle.id).map(|ptr| {
-            let ptr = ptr as u64;
-            let offset = handle.offset();
-            let size = handle.size();
+    fn get(&mut self, handle: &StorageHandle) -> Self::Resource {
+        let ptr = (*self.memory.get(&handle.id).unwrap()) as u64;
 
-            let ptr = ptr + offset;
-            let key = ActiveResource::new(ptr);
+        let offset = handle.offset();
+        let size = handle.size();
 
-            self.activate_slices
-                .insert(key.clone(), ptr as cubecl_hip_sys::hipDeviceptr_t);
+        let ptr = ptr + offset;
+        let key = ActiveResource::new(ptr);
 
-            // The ptr needs to stay alive until we send the task to the server.
-            let ptr = self.activate_slices.get(&key).unwrap();
+        self.activate_slices
+            .insert(key.clone(), ptr as cubecl_hip_sys::hipDeviceptr_t);
 
-            HipResource::new(
-                *ptr,
-                ptr as *const cubecl_hip_sys::hipDeviceptr_t as *mut std::ffi::c_void,
-                offset,
-                size,
-            )
-        })
+        // The ptr needs to stay alive until we send the task to the server.
+        let ptr = self.activate_slices.get(&key).unwrap();
+
+        HipResource::new(
+            *ptr,
+            ptr as *const cubecl_hip_sys::hipDeviceptr_t as *mut std::ffi::c_void,
+            offset,
+            size,
+        )
     }
 
     fn alloc(&mut self, size: u64) -> StorageHandle {
