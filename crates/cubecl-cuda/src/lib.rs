1970a4f395b769e041ee82db013afc3b233cf17a
Jorge Perez Burgos <vaijira@gmail.com>
2025-01-17 03:05:48 +0100

    Add eye support for TensorHandle. (#403)

--- crates/cubecl-cuda/src/lib.rs
+++ crates/cubecl-cuda/src/lib.rs
@@ -19,5 +19,6 @@ mod tests {
     cubecl_linalg::testgen_matmul_accelerated!([f16]);
     cubecl_linalg::testgen_matmul_simple!([f16, bf16, f32]);
     cubecl_linalg::testgen_matmul_tiling2d!([f16, bf16, f32]);
+    cubecl_linalg::testgen_tensor_identity!([f16, bf16, f32, u32]);
     cubecl_reduce::testgen_reduce!([f16, bf16, f32, f64]);
 }
