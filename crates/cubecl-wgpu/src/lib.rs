3ff163bf807e12ec3da8550aec09a9efdc5112df
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-01-21 17:59:44 +0100

    Disable wgsl tests with SPIR-V enabled

--- crates/cubecl-wgpu/src/lib.rs
+++ crates/cubecl-wgpu/src/lib.rs
@@ -20,7 +20,7 @@ pub use runtime::*;
 #[cfg(feature = "spirv")]
 pub use compiler::spirv;
 
-#[cfg(test)]
+#[cfg(all(test, not(feature = "spirv")))]
 #[allow(unexpected_cfgs)]
 mod tests {
     pub type TestRuntime = crate::WgpuRuntime<crate::WgslCompiler>;
