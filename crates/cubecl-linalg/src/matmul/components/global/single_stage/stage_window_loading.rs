4daf7a39bfa979c800a7c24ff9354605bed47386
louisfd <louisfd94@gmail.com>
2025-02-26 16:19:30 -0500

    no paddingg

--- crates/cubecl-linalg/src/matmul/components/global/single_stage/stage_window_loading.rs
+++ crates/cubecl-linalg/src/matmul/components/global/single_stage/stage_window_loading.rs
@@ -145,9 +145,9 @@ impl AsyncLoadingStrategy for WindowDuplicatedLoading {
             );
 
             // If padding needed: TODO comptime conditional
-            for i in window.size..expected_window_size {
-                destination[i] = Line::cast_from(0);
-            }
+            // for i in window.size..expected_window_size {
+            //     destination[i] = Line::cast_from(0);
+            // }
         }
     }
 }
@@ -256,71 +256,6 @@ impl AsyncLoadingStrategy for WindowElectedOnlyLoading {
         }
     }
 }
-
-// #[cube]
-// impl AsyncLoadingStrategy for WindowSplitUnitLoading {
-//     type TilingLayout = StridedTilingLayout;
-
-//     fn load<EG: Numeric, ES: Numeric, G: GlobalConfig, CM: CopyMechanism<ES>>(
-//         read_view: &TensorReader<EG>,
-//         stage_slice: &mut SliceMut<Line<ES>>,
-//         mechanism: CM,
-//         #[comptime] ident: Ident,
-//         #[comptime] config: G,
-//     ) {
-//         let matrix_layout = config.matrix_layout(ident);
-//         let tiling_dimensions = config.tiling_dimensions(ident);
-//         let line_size = config.global_line_size(ident);
-
-//         let (num_slices, expected_window_size) = match matrix_layout {
-//             MatrixLayout::RowMajor => (
-//                 tiling_dimensions.total_row(),
-//                 tiling_dimensions.total_col() / line_size,
-//             ),
-//             MatrixLayout::ColMajor => (
-//                 tiling_dimensions.total_col(),
-//                 tiling_dimensions.total_row() / line_size,
-//             ),
-//         };
-
-//         let plane_dim = config.plane_dim();
-//         let plane_count = config.num_planes();
-//         let unit_count = plane_dim * plane_count;
-//         let unit_index = UNIT_POS;
-
-//         let slices_per_unit = (num_slices + unit_count - 1) / unit_count;
-
-//         #[unroll(slices_per_unit==1)]
-//         for nth_slice_local in 0..slices_per_unit {
-//             let nth_slice = unit_count * nth_slice_local + unit_index;
-
-//             // TODO no if when always fits
-//             if nth_slice < num_slices {
-//                 let window: Window<EG> =
-//                     read_view.load_window_no_tile::<G>(nth_slice, ident, config);
-//                 let mut destination: SliceMut<Line<ES>> =
-//                     StridedTilingLayout::nth_slice::<ES, G::SmmConfig>(
-//                         stage_slice,
-//                         nth_slice,
-//                         ident,
-//                         config.to_smm_config(),
-//                     );
-
-//                 CM::memcpy_async(
-//                     &mechanism,
-//                     &window.slice.try_cast_unchecked(),
-//                     &mut destination,
-//                 );
-
-//                 // If padding needed: TODO comptime conditional
-//                 // Or maybe reset stage to zero after computation?
-//                 for i in window.size..expected_window_size {
-//                     destination[i] = Line::cast_from(0);
-//                 }
-//             }
-//         }
-//     }
-// }
 
 #[cube]
 impl AsyncLoadingStrategy for WindowSplitUnitLoading {
