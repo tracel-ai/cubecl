8bfd1ef1a1d09295144190502e71090335dfb2d8
louisfd <louisfd94@gmail.com>
2025-03-06 11:51:25 -0500

    works

--- crates/cubecl-linalg/src/matmul/components/global/single_stage/loader/sync/strided.rs
+++ crates/cubecl-linalg/src/matmul/components/global/single_stage/loader/sync/strided.rs
@@ -1,8 +1,6 @@
-use std::marker::PhantomData;
-
 use crate::matmul::components::global::tensor_view::TensorReader;
 use crate::matmul::components::global::{GlobalConfig, LoadingValidation};
-use crate::matmul::components::stage::{StridedTilingLayout, TilingOrder};
+use crate::matmul::components::stage::StridedTilingLayout;
 use crate::matmul::components::{Ident, InvalidConfigError};
 use cubecl_core as cubecl;
 use cubecl_core::prelude::*;
@@ -10,26 +8,24 @@ use cubecl_core::prelude::*;
 use super::SyncLoadingStrategy;
 
 #[derive(CubeType, Clone, Copy)]
-/// Loads the content of all tiles in the tensor view using all planes,
+/// Loads the content of all the tensor view using all planes,
 /// iterating with steps determined by the plane's dimension.
-pub struct StridedCoalescedLoading<T: TilingOrder> {
-    tiling_order: PhantomData<T>,
-}
+pub struct StridedCoalescedLoading {}
 
-impl<T: TilingOrder> LoadingValidation for StridedCoalescedLoading<T> {
+impl LoadingValidation for StridedCoalescedLoading {
     fn check<C: GlobalConfig>(config: &C, ident: Ident) -> Result<(), InvalidConfigError> {
-        // let tiling = config.tiling_dimensions(ident);
-        // let line_size = config.global_line_size(ident);
+        let tiling = config.tiling_dimensions(ident);
+        let line_size = config.global_line_size(ident);
 
-        // let num_stage_lines = tiling.total_size() / line_size;
-        // let total_units = config.num_planes() * config.plane_dim();
+        let num_stage_lines = tiling.total_size() / line_size;
+        let total_units = config.num_planes() * config.plane_dim();
 
-        // if num_stage_lines % total_units != 0 {
-        //     return Err(Box::new(
-        //         "Too many data will be loaded, resulting in out of bounds.
-        // Try setting line size and number of planes so that total unit count {:?} divides number of lines in stage.",
-        //     ));
-        // }
+        if num_stage_lines % total_units != 0 {
+            return Err(Box::new(
+                "Too many data will be loaded, resulting in out of bounds.
+        Try setting line size and number of planes so that total unit count {:?} divides number of lines in stage.",
+            ));
+        }
         if config.transpose_load(ident) {
             return Err(Box::new(
                 "Transpose load not yet supported in strided coalesced loading setup",
@@ -41,7 +37,7 @@ impl<T: TilingOrder> LoadingValidation for StridedCoalescedLoading<T> {
 }
 
 #[cube]
-impl<T: TilingOrder> SyncLoadingStrategy for StridedCoalescedLoading<T> {
+impl SyncLoadingStrategy for StridedCoalescedLoading {
     type TilingLayout = StridedTilingLayout;
 
     fn load<EG: Numeric, ES: Numeric, G: GlobalConfig>(
@@ -51,19 +47,25 @@ impl<T: TilingOrder> SyncLoadingStrategy for StridedCoalescedLoading<T> {
         #[comptime] config: G,
     ) {
         let tiling = config.tiling_dimensions(ident);
+        // 4
         let line_size = config.global_line_size(ident);
+        // 256 / 4 = 64
         let num_stage_lines = tiling.total_size() / line_size;
+        // 1*32= 32
         let unit_count = config.num_planes() * config.plane_dim();
+        // 64 / 32 = 2
         let num_loads_per_unit = comptime!(num_stage_lines / unit_count);
 
+        // 0 * 32 + 0..32 = 0..32
         let unit_base_position = UNIT_POS_Y * config.plane_dim() + UNIT_POS_X;
 
         for i in 0..num_loads_per_unit {
             let unit_position = unit_base_position + i * unit_count;
 
-            let line_read = read_view.load_coalesced_in_stage(unit_position, ident, config);
+            let line_read =
+                read_view.load_coalesced_in_stage::<G>(unit_position * line_size, ident, config);
 
-            slice[unit_position / line_size] = Line::cast_from(line_read);
+            slice[unit_position] = Line::cast_from(line_read);
         }
     }
 }
--- crates/cubecl-linalg/src/matmul/components/global/tensor_view.rs
+++ crates/cubecl-linalg/src/matmul/components/global/tensor_view.rs
@@ -231,52 +231,26 @@ impl<EG: Numeric> TensorReader<EG> {
         &self,
         tile_x: u32,
         tile_y: u32,
-        unit_id: u32,
+        position: u32,
         #[comptime] ident: Ident,
         #[comptime] config: G,
     ) -> Line<EG> {
-        let line_size = config.global_line_size(ident);
         let tile_shape_x = config.tiling_dimensions(ident).tile_shape_row();
         let tile_shape_y = config.tiling_dimensions(ident).tile_shape_col();
 
-        let view_tile_x = tile_x * tile_shape_x + self.x_offset;
-        let view_tile_y = tile_y * tile_shape_y + self.y_offset;
+        let view_tile_x = tile_x * tile_shape_x;
+        let view_tile_y = tile_y * tile_shape_y;
 
         let (load_x, load_y) = match config.matrix_layout(ident) {
-            MatrixLayout::RowMajor => (unit_id / tile_shape_y, unit_id % tile_shape_y),
-            MatrixLayout::ColMajor => (unit_id % tile_shape_x, unit_id / tile_shape_x),
+            MatrixLayout::RowMajor => (position / tile_shape_y, position % tile_shape_y),
+            MatrixLayout::ColMajor => (position % tile_shape_x, position / tile_shape_x),
         };
 
-        let view_x = view_tile_x + load_x;
-        let view_y = view_tile_y + load_y;
-
-        let read_pos =
-            (view_x * self.stride_x + view_y * self.stride_y + self.batch_offset) / line_size;
-
-        match comptime!((
-            config.check_row_bounds(ident),
-            config.check_col_bounds(ident)
-        )) {
-            (true, true) => select(
-                view_x < self.shape_x && view_y < self.shape_y,
-                self.tensor.read(read_pos),
-                Line::empty(line_size).fill(EG::from_int(0)),
-            ),
-            (true, false) => select(
-                view_x < self.shape_x,
-                self.tensor.read(read_pos),
-                Line::empty(line_size).fill(EG::from_int(0)),
-            ),
-            (false, true) => select(
-                view_y < self.shape_y,
-                self.tensor.read(read_pos),
-                Line::empty(line_size).fill(EG::from_int(0)),
-            ),
-            (false, false) => self.tensor.read(read_pos),
-        }
+        self.load_coalesced::<G>((load_x + view_tile_x, load_y + view_tile_y), ident, config)
     }
 
-    /// Reads data from the tensor view at the specified tile coordinates (tile_x, tile_y).
+    /// Reads data from the tensor view at the specified index within the whole view,
+    /// without regards to tiles
     ///
     /// Each unit loads one line in a coalesced manner for improved efficiency.
     /// For row-major tensors, subsequent units read lines horizontally within the tile,
@@ -287,26 +261,31 @@ impl<EG: Numeric> TensorReader<EG> {
     /// Out-of-bounds reads will be translated to zeros.
     pub fn load_coalesced_in_stage<G: global::GlobalConfig>(
         &self,
-        tile_x: u32,
-        tile_y: u32,
-        unit_id: u32,
+        position: u32,
+        #[comptime] ident: Ident,
+        #[comptime] config: G,
+    ) -> Line<EG> {
+        let stage_shape_x = config.tiling_dimensions(ident).total_row();
+        let stage_shape_y = config.tiling_dimensions(ident).total_col();
+
+        let load_offsets = match config.matrix_layout(ident) {
+            MatrixLayout::RowMajor => (position / stage_shape_y, position % stage_shape_y),
+            MatrixLayout::ColMajor => (position % stage_shape_x, position / stage_shape_x),
+        };
+
+        self.load_coalesced::<G>(load_offsets, ident, config)
+    }
+
+    fn load_coalesced<G: global::GlobalConfig>(
+        &self,
+        load_offsets: (u32, u32),
         #[comptime] ident: Ident,
         #[comptime] config: G,
     ) -> Line<EG> {
         let line_size = config.global_line_size(ident);
-        let tile_shape_x = config.tiling_dimensions(ident).tile_shape_row();
-        let tile_shape_y = config.tiling_dimensions(ident).tile_shape_col();
 
-        let view_tile_x = tile_x * tile_shape_x + self.x_offset;
-        let view_tile_y = tile_y * tile_shape_y + self.y_offset;
-
-        let (load_x, load_y) = match config.matrix_layout(ident) {
-            MatrixLayout::RowMajor => (unit_id / tile_shape_y, unit_id % tile_shape_y),
-            MatrixLayout::ColMajor => (unit_id % tile_shape_x, unit_id / tile_shape_x),
-        };
-
-        let view_x = view_tile_x + load_x;
-        let view_y = view_tile_y + load_y;
+        let view_x = load_offsets.0 + self.x_offset;
+        let view_y = load_offsets.1 + self.y_offset;
 
         let read_pos =
             (view_x * self.stride_x + view_y * self.stride_y + self.batch_offset) / line_size;
