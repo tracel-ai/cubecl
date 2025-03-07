77a6c4a216216c16503484e2fcb4e49e45fb316c
AntiAnimeGeneral <3327363393@qq.com>
2025-03-07 15:30:50 +0800

    impl slow path scan for width < 16

--- crates/cubecl-std/src/sort/radix/interface.rs
+++ crates/cubecl-std/src/sort/radix/interface.rs
@@ -25,26 +25,12 @@ impl AsRadix for i32 {
 }
 
 #[cube]
-impl AsRadix for f32 {
-    type RadixType = u32;
-    fn as_radix(this: &Self) -> Self::RadixType {
-        let bits = u32::bitcast_from(*this);
-        if (bits & 0x80_00_00_00u32) != 0 {
-            BitwiseNot::bitwise_not(bits)
-        } else {
-            bits | 0x80_00_00_00u32
-        }
-    }
-}
-
-#[cube]
 pub trait RadixData:
     CubeType<ExpandType = ExpandElementTyped<Self>>
     + Sized
     + CubePrimitive
 {
     const BYTECNT: u32;
-    // const MAX: Self;
     fn shift_mask(
         this: &Self,
         shift: u32,
@@ -55,27 +41,11 @@ pub trait RadixData:
 #[cube]
 impl RadixData for u32 {
     const BYTECNT: u32 = 4;
-    // const MAX: Self = u32::MAX;
     fn shift_mask(
         this: &Self,
         shift: u32,
         mask: u32,
     ) -> u32 {
-        // T::max_value();
-        // u32::cast_from(*this >> T::cast_from(shift)) & mask
         this >> shift & mask
     }
 }
-
-// #[cube]
-// impl<T: Int> RadixData for T {
-//     const BYTECNT: u32 = T::BITS / 8;
-//     fn shift_mask(
-//         this: &Self,
-//         shift: u32,
-//         mask: u32,
-//     ) -> u32 {
-//         T::max_value();
-//         u32::cast_from(*this >> T::cast_from(shift)) & mask
-//     }
-// }
