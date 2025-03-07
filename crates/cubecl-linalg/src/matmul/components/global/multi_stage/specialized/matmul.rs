464bf7f4043f4c01c35e246dc97ca26dce8465ed
louisfd <louisfd94@gmail.com>
2025-03-07 11:39:20 -0500

    make double buff and specialized compile with loading strategy

--- crates/cubecl-linalg/src/matmul/components/global/multi_stage/specialized/matmul.rs
+++ crates/cubecl-linalg/src/matmul/components/global/multi_stage/specialized/matmul.rs
@@ -5,9 +5,7 @@ use crate::matmul::components::global::output_loader::Unloader;
 use crate::matmul::components::global::ZeroAccumulatorLoader;
 use crate::matmul::components::global::{GlobalMatmul, SyncInputLoader};
 use crate::matmul::components::stage::single_buffer::{LhsBufferReader, RhsBufferReader};
-use crate::matmul::components::stage::{
-    ColMajorTilingOrder, ContiguousTilingLayout, RowMajorTilingOrder, StageMatmul,
-};
+use crate::matmul::components::stage::StageMatmul;
 use crate::matmul::components::Ident;
 use crate::matmul::components::MatmulPrecision;
 use cubecl_std::tensor::r#virtual::{ReadWrite, VirtualTensor};
