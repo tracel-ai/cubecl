b7ac643086443f69de583d6540a47e4a83a875cf
louisfd <louisfd94@gmail.com>
2025-03-19 09:09:12 -0400

    double buffering barrier algorithm

--- crates/cubecl-linalg/src/matmul/base.rs
+++ crates/cubecl-linalg/src/matmul/base.rs
@@ -14,7 +14,8 @@ use super::{
     },
     kernels::{
         matmul::{
-            self, double_buffering::DoubleBufferingAlgorithm, simple::SimpleAlgorithm,
+            self, double_buffering::DoubleBufferingAlgorithm,
+            double_buffering_barrier::DoubleBufferingBarrierAlgorithm, simple::SimpleAlgorithm,
             simple_barrier::SimpleBarrierAlgorithm, simple_pipelined::SimplePipelinedAlgorithm,
             specialized::SpecializedAlgorithm,
         },
@@ -30,6 +31,7 @@ pub enum Strategy {
     SimpleBarrier(AsyncLoadingStrategy),
     SimplePipelined,
     DoubleBuffering,
+    DoubleBufferingBarrier,
     Specialized,
     #[cfg(any(test, feature = "export_tests"))]
     // Very slow, only use for testing.
@@ -117,6 +119,11 @@ pub fn launch_ref<R: Runtime, EG: MaybeQuantized>(
         }
         Strategy::DoubleBuffering => {
             matmul::launch_ref::<R, EG, DoubleBufferingAlgorithm<Accelerated>>(
+                client, lhs, rhs, out,
+            )
+        }
+        Strategy::DoubleBufferingBarrier => {
+            matmul::launch_ref::<R, EG, DoubleBufferingBarrierAlgorithm<Accelerated>>(
                 client, lhs, rhs, out,
             )
         }
