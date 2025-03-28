b2e7933497d5ddf15599f1e97c9246af0a5213ec
maxtremblay <t.maxime@pm.me>
2025-03-28 14:14:40 -0400

    use typedef for uint and int in cpp

--- crates/cubecl-cpp/src/shared/mma.rs
+++ crates/cubecl-cpp/src/shared/mma.rs
@@ -196,7 +196,7 @@ pub mod wmma_api_base {
                     format!("{}", fragment.elem)
                 }
             }
-            Elem::I8 => "unsigned char".to_string(),
+            Elem::I8 => "int8".to_string(),
             elem => format!("{elem}"),
         };
         match fragment.layout {
@@ -232,13 +232,9 @@ pub mod wmma_api_base {
                 let item = value.item();
                 if item.vectorization > 1 {
                     let elem = item.elem;
-                    let elem_str = match elem {
-                        Elem::I8 => "unsigned char".to_string(),
-                        _ => elem.to_string(),
-                    };
                     writeln!(
                         f,
-                        "{namespace}::load_matrix_sync({frag}, reinterpret_cast<{elem_str} *>({value}), {stride});"
+                        "{namespace}::load_matrix_sync({frag}, reinterpret_cast<{elem} *>({value}), {stride});"
                     )
                 } else {
                     writeln!(
