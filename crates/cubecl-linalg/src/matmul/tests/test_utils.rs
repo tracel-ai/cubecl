398495ff4021d9454eefabe47cc7beb193b917a6
syl20bnr <sylvain.benner@tracel.ai>
2025-04-01 13:36:22 -0400

    Make it compile

--- crates/cubecl-linalg/src/matmul/tests/test_utils.rs
+++ crates/cubecl-linalg/src/matmul/tests/test_utils.rs
@@ -111,9 +111,6 @@ pub(crate) fn assert_equals_approx<R: Runtime, F: Float + CubeElement + Display>
     ));
     let actual = F::from_bytes(&actual);
 
-    // println!("RESULT: {actual:?}");
-    // println!("EXPECTED: {expected:?}");
-
     // normalize to type epsilon
     let epsilon = (epsilon / f32::EPSILON * F::EPSILON.to_f32().unwrap()).max(epsilon);
 
--- crates/cubecl-wgpu/src/backend/wgsl.rs
+++ crates/cubecl-wgpu/src/backend/wgsl.rs
@@ -16,26 +16,54 @@ pub fn bindings(repr: &<WgslCompiler as Compiler>::Representation) -> Vec<(usize
 
 pub async fn request_device(adapter: &wgpu::Adapter) -> (wgpu::Device, wgpu::Queue) {
     let limits = adapter.limits();
-    adapter
-        .request_device(&DeviceDescriptor {
-            label: None,
-            required_features: adapter.features(),
-            required_limits: limits,
-            // The default is MemoryHints::Performance, which tries to do some bigger
-            // block allocations. However, we already batch allocations, so we
-            // can use MemoryHints::MemoryUsage to lower memory usage.
-            memory_hints: wgpu::MemoryHints::MemoryUsage,
-            trace: wgpu::Trace::Off,
-        })
-        .await
-        .map_err(|err| {
-            format!(
-                "Unable to request the device with the adapter {:?}, err {:?}",
-                adapter.get_info(),
-                err
+    #[cfg(not(feature = "msl"))]
+    {
+        adapter
+            .request_device(
+                &DeviceDescriptor {
+                    label: None,
+                    required_features: adapter.features(),
+                    required_limits: limits,
+                    // The default is MemoryHints::Performance, which tries to do some bigger
+                    // block allocations. However, we already batch allocations, so we
+                    // can use MemoryHints::MemoryUsage to lower memory usage.
+                    memory_hints: wgpu::MemoryHints::MemoryUsage,
+                },
+                None,
             )
-        })
-        .unwrap()
+            .await
+            .map_err(|err| {
+                format!(
+                    "Unable to request the device with the adapter {:?}, err {:?}",
+                    adapter.get_info(),
+                    err
+                )
+            })
+            .unwrap()
+    }
+    #[cfg(feature = "msl")]
+    {
+        adapter
+            .request_device(&DeviceDescriptor {
+                label: None,
+                required_features: adapter.features(),
+                required_limits: limits,
+                // The default is MemoryHints::Performance, which tries to do some bigger
+                // block allocations. However, we already batch allocations, so we
+                // can use MemoryHints::MemoryUsage to lower memory usage.
+                memory_hints: wgpu::MemoryHints::MemoryUsage,
+                trace: wgpu::Trace::Off,
+            })
+            .await
+            .map_err(|err| {
+                format!(
+                    "Unable to request the device with the adapter {:?}, err {:?}",
+                    adapter.get_info(),
+                    err
+                )
+            })
+            .unwrap()
+    }
 }
 
 pub fn register_types(props: &mut DeviceProperties<Feature>) {
