04e999a787b3ee1423aef3bbab011d1bd90ca445
maxtremblay <t.maxime@pm.me>
2025-04-02 13:38:54 -0400

    impl list write

--- crates/cubecl-std/src/tests/reinterpret_list.rs
+++ crates/cubecl-std/src/tests/reinterpret_list.rs
@@ -5,23 +5,23 @@ use crate::ReinterpretList;
 use half::f16;
 
 #[cube(launch)]
-fn kernel(input: &Array<Line<i8>>, output: &mut Array<f16>) {
+fn kernel_read(input: &Array<Line<i8>>, output: &mut Array<f16>) {
     let line_size = input.line_size();
     let list = ReinterpretList::<i8, f16, &Array<Line<i8>>>::new(input, line_size);
     output[UNIT_POS] = list.read(UNIT_POS);
 }
 
-pub fn run_test<R: Runtime>(client: ComputeClient<R::Server, R::Channel>, line_size: usize) {
+pub fn run_test_read<R: Runtime>(client: ComputeClient<R::Server, R::Channel>, line_size: usize) {
     let target = [f16::from_f32(1.0), f16::from_f32(-8.5)];
     let casted: [i8; 4] = unsafe { std::mem::transmute(target) };
 
     let input = client.create(i8::as_bytes(&casted));
     let output = client.empty(4);
 
-    kernel::launch::<R>(
+    kernel_read::launch::<R>(
         &client,
         CubeCount::new_single(),
-        CubeDim::new(2, 1, 1),
+        CubeDim::new_1d(2),
         unsafe { ArrayArg::from_raw_parts::<i8>(&input, 4 / line_size, line_size as u8) },
         unsafe { ArrayArg::from_raw_parts::<f16>(&output, 2, 1) },
     );
@@ -32,6 +32,34 @@ pub fn run_test<R: Runtime>(client: ComputeClient<R::Server, R::Channel>, line_s
     assert_eq!(actual, target);
 }
 
+#[cube(launch)]
+fn kernel_write(output: &mut Array<Line<i8>>, input: &Array<f16>) {
+    let line_size = output.line_size();
+    let list = ReinterpretList::<i8, f16, &Array<Line<i8>>>::new(output, line_size);
+    list.write(UNIT_POS, input[UNIT_POS]);
+}
+
+pub fn run_test_write<R: Runtime>(client: ComputeClient<R::Server, R::Channel>, line_size: usize) {
+    let source = [f16::from_f32(1.0), f16::from_f32(-8.5)];
+    let casted: [i8; 4] = unsafe { std::mem::transmute(source) };
+
+    let output = client.empty(4);
+    let input = client.create(f16::as_bytes(&source));
+
+    kernel_read::launch::<R>(
+        &client,
+        CubeCount::new_single(),
+        CubeDim::new_1d(2),
+        unsafe { ArrayArg::from_raw_parts::<i8>(&output, 4 / line_size, line_size as u8) },
+        unsafe { ArrayArg::from_raw_parts::<f16>(&input, 2, 1) },
+    );
+
+    let actual = client.read_one(output.binding());
+    let actual = i8::from_bytes(&actual);
+
+    assert_eq!(actual, casted);
+}
+
 #[macro_export]
 macro_rules! testgen_reinterpret_list {
     () => {
@@ -39,21 +67,39 @@ macro_rules! testgen_reinterpret_list {
             use super::*;
 
             #[test]
-            fn from_i8x1() {
+            fn read_from_i8x1() {
                 let client = TestRuntime::client(&Default::default());
-                cubecl_std::tests::reinterpret_list::run_test::<TestRuntime>(client, 1);
+                cubecl_std::tests::reinterpret_list::run_test_read::<TestRuntime>(client, 1);
             }
 
             #[test]
-            fn from_i8x2() {
+            fn read_from_i8x2() {
                 let client = TestRuntime::client(&Default::default());
-                cubecl_std::tests::reinterpret_list::run_test::<TestRuntime>(client, 2);
+                cubecl_std::tests::reinterpret_list::run_test_read::<TestRuntime>(client, 2);
             }
 
             #[test]
-            fn from_i8x4() {
+            fn read_from_i8x4() {
                 let client = TestRuntime::client(&Default::default());
-                cubecl_std::tests::reinterpret_list::run_test::<TestRuntime>(client, 4);
+                cubecl_std::tests::reinterpret_list::run_test_read::<TestRuntime>(client, 4);
+            }
+
+            #[test]
+            fn write_into_i8x1() {
+                let client = TestRuntime::client(&Default::default());
+                cubecl_std::tests::reinterpret_list::run_test_write::<TestRuntime>(client, 1);
+            }
+
+            #[test]
+            fn write_into_i8x2() {
+                let client = TestRuntime::client(&Default::default());
+                cubecl_std::tests::reinterpret_list::run_test_write::<TestRuntime>(client, 2);
+            }
+
+            #[test]
+            fn write_into_i8x4() {
+                let client = TestRuntime::client(&Default::default());
+                cubecl_std::tests::reinterpret_list::run_test_write::<TestRuntime>(client, 4);
             }
         }
     };
