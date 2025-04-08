15f0250def0d5b2bbb5e3e48df957c519b50578d
louisfd <louisfd94@gmail.com>
2025-04-08 13:24:08 -0400

    async full cyclic

--- crates/cubecl-linalg/src/matmul/components/global/load/strategy/async_full_cyclic.rs
+++ crates/cubecl-linalg/src/matmul/components/global/load/strategy/async_full_cyclic.rs
@@ -4,7 +4,8 @@ use crate::matmul::components::{
     Ident, InputIdent, InvalidConfigError, MatmulPrecision, MatrixLayout,
     global::{
         CopyMechanism, GlobalConfig, LoadingValidation, Quantization,
-        load::AsyncFullLoadingStrategy, tensor_view::TensorReader,
+        load::{AsyncFullLoadingStrategy, default_async_full_load},
+        tensor_view::TensorReader,
     },
     stage::{ContiguousTilingLayout, Stage, TilingOrder},
 };
@@ -45,12 +46,30 @@ impl<T: TilingOrder> AsyncFullLoadingStrategy for AsyncFullCyclicLoading<T> {
 
     fn load_full<MP: MatmulPrecision, CM: CopyMechanism<MP::ES>, G: GlobalConfig>(
         read_view: TensorReader<MP::EI>,
-        mut stage: Stage<MP::ES, Self::TilingLayout>,
+        stage: Stage<MP::ES, Self::TilingLayout>,
+        mechanism: CM,
+        quantization: CubeOption<Quantization<MP>>,
+        #[comptime] input_ident: InputIdent,
+        #[comptime] config: G,
+    ) {
+        default_async_full_load::<Self, MP, G, CM>(
+            read_view,
+            stage,
+            mechanism,
+            quantization,
+            input_ident,
+            config,
+        )
+    }
+
+    fn job<MP: MatmulPrecision, CM: CopyMechanism<MP::ES>, G: GlobalConfig>(
+        read_view: TensorReader<MP::EI>,
+        stage: Stage<MP::ES, Self::TilingLayout>,
         mechanism: CM,
         _quantization: CubeOption<Quantization<MP>>,
         #[comptime] input_ident: InputIdent,
         #[comptime] config: G,
-    ) {
+    ) -> AsyncFullCyclicJob<MP, CM, T> {
         let stage_dim = config.tiling_dimensions(input_ident);
         let total_units = config.plane_dim() * config.num_planes();
         let line_size = config.global_line_size(input_ident);
@@ -67,59 +86,22 @@ impl<T: TilingOrder> AsyncFullLoadingStrategy for AsyncFullCyclicLoading<T> {
         };
 
         let num_slices = comptime!(num_slices_per_tile * stage_dim.tile_count());
-        let num_slices_per_unit = num_slices.div_ceil(total_units);
+        let num_tasks = num_slices.div_ceil(total_units);
 
         let unit_id = UNIT_POS_Y * config.plane_dim() + UNIT_POS_X;
 
-        #[unroll(num_slices_per_unit==1)]
-        for i in 0..num_slices_per_unit {
-            let slice_index = unit_id + total_units * i;
-
-            let nth_tile = slice_index / num_slices_per_tile;
-            let (tile_x, tile_y) = ContiguousTilingLayout::<T>::to_x_y::<G::SmmConfig>(
-                nth_tile,
-                input_ident.as_ident(),
-                config.to_smm_config(),
-            );
-            let nth_slice = slice_index % num_slices_per_tile;
-
-            // TODO make branching comptime conditional
-            if slice_index < num_slices {
-                let window = read_view.load_window_in_tile::<G>(
-                    (tile_x, tile_y),
-                    nth_slice,
-                    input_ident,
-                    config,
-                );
-
-                // Where this unit writes source in the stage
-                let slice_destination_offset =
-                    (nth_tile * num_slices_per_tile + nth_slice) * slice_length_in_lines;
-
-                // Make destination start at offset
-                let mut destination = stage.as_slice_mut().slice_mut(
-                    slice_destination_offset,
-                    slice_destination_offset + slice_length_in_lines,
-                );
-
-                CM::memcpy_async(
-                    &mechanism,
-                    &window.slice.try_cast_unchecked(),
-                    &mut destination,
-                );
-            }
+        AsyncFullCyclicJob::<MP, CM, T> {
+            unit_id,
+            read_view,
+            stage,
+            mechanism,
+            num_tasks,
+            total_units,
+            num_slices,
+            input_ident,
+            num_slices_per_tile,
+            slice_length_in_lines,
         }
-    }
-
-    fn job<MP: MatmulPrecision, CM: CopyMechanism<MP::ES>, G: GlobalConfig>(
-        read_view: TensorReader<MP::EI>,
-        stage: Stage<MP::ES, Self::TilingLayout>,
-        mechanism: CM,
-        _quantization: CubeOption<Quantization<MP>>,
-        #[comptime] input_ident: InputIdent,
-        #[comptime] config: G,
-    ) -> AsyncFullCyclicJob<MP, CM, T> {
-        todo!()
     }
 
     fn barrier_level() -> BarrierLevel {
@@ -129,21 +111,69 @@ impl<T: TilingOrder> AsyncFullLoadingStrategy for AsyncFullCyclicLoading<T> {
 
 #[derive(CubeType, Clone, Copy)]
 pub struct AsyncFullCyclicJob<MP: MatmulPrecision, CM: CopyMechanism<MP::ES>, T: TilingOrder> {
+    unit_id: u32,
+
     read_view: TensorReader<MP::EI>,
     stage: Stage<MP::ES, ContiguousTilingLayout<T>>,
     mechanism: CM,
-    _quantization: CubeOption<Quantization<MP>>,
+
+    #[cube(comptime)]
+    num_tasks: u32,
+    #[cube(comptime)]
+    total_units: u32,
+    #[cube(comptime)]
+    num_slices: u32,
+    #[cube(comptime)]
+    input_ident: InputIdent,
+    #[cube(comptime)]
+    num_slices_per_tile: u32,
+    #[cube(comptime)]
+    slice_length_in_lines: u32,
 }
 
 #[cube]
 impl<MP: MatmulPrecision, CM: CopyMechanism<MP::ES>, T: TilingOrder> LoadingJob<MP>
     for AsyncFullCyclicJob<MP, CM, T>
 {
-    fn len(_this: &Self) -> u32 {
-        todo!()
+    fn len(this: &Self) -> u32 {
+        this.num_tasks.runtime()
     }
 
     fn execute_task<G: GlobalConfig>(this: &mut Self, task_id: u32, #[comptime] config: G) {
-        // TODO
+        let slice_index = this.unit_id + this.total_units * task_id;
+
+        let nth_tile = slice_index / this.num_slices_per_tile;
+        let (tile_x, tile_y) = ContiguousTilingLayout::<T>::to_x_y::<G::SmmConfig>(
+            nth_tile,
+            comptime!(this.input_ident.as_ident()),
+            config.to_smm_config(),
+        );
+        let nth_slice = slice_index % this.num_slices_per_tile;
+
+        // TODO make branching comptime conditional
+        if slice_index < this.num_slices {
+            let window = this.read_view.load_window_in_tile::<G>(
+                (tile_x, tile_y),
+                nth_slice,
+                this.input_ident,
+                config,
+            );
+
+            // Where this unit writes source in the stage
+            let slice_destination_offset =
+                (nth_tile * this.num_slices_per_tile + nth_slice) * this.slice_length_in_lines;
+
+            // Make destination start at offset
+            let mut destination = this.stage.as_slice_mut().slice_mut(
+                slice_destination_offset,
+                slice_destination_offset + this.slice_length_in_lines,
+            );
+
+            CM::memcpy_async(
+                &this.mechanism,
+                &window.slice.try_cast_unchecked(),
+                &mut destination,
+            );
+        }
     }
 }
