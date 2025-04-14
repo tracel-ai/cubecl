e9755f97a7613a9f1acafad71a499dc504786ce2
nathaniel <nathaniel.simard.42@gmail.com>
2025-04-14 11:45:17 -0400

    Cache compilation

--- crates/cubecl-cuda/src/compute/server.rs
+++ crates/cubecl-cuda/src/compute/server.rs
@@ -40,7 +40,7 @@ use std::path::PathBuf;
 use std::{ffi::CStr, os::raw::c_void};
 use std::{ffi::CString, mem::MaybeUninit};
 
-#[cfg(feature = "cache-ptx")]
+#[cfg(feature = "cache-compilation")]
 use cubecl_common::cache::{Cache, CacheOption};
 
 #[derive(Debug)]
@@ -55,7 +55,7 @@ pub(crate) struct CudaContext {
     stream: cudarc::driver::sys::CUstream,
     memory_management: MemoryManagement<CudaStorage>,
     module_names: HashMap<KernelId, CompiledKernel>,
-    #[cfg(feature = "cache-ptx")]
+    #[cfg(feature = "cache-compilation")]
     ptx_cache: Cache<String, PtxCacheEntry>,
     timestamps: KernelTimestamps,
     pub(crate) arch: CudaArchitecture,
@@ -586,7 +586,7 @@ impl CudaContext {
             context,
             memory_management,
             module_names: HashMap::new(),
-            #[cfg(feature = "cache-ptx")]
+            #[cfg(feature = "cache-compilation")]
             ptx_cache: Cache::new("cuda/ptx", CacheOption::default()),
             stream,
             arch,
@@ -616,10 +616,10 @@ impl CudaContext {
         logger: &mut DebugLogger,
         mode: ExecutionMode,
     ) {
-        #[cfg(feature = "cache-ptx")]
+        #[cfg(feature = "cache-compilation")]
         let name = kernel_id.stable_format();
 
-        #[cfg(feature = "cache-ptx")]
+        #[cfg(feature = "cache-compilation")]
         if let Some(entry) = self.ptx_cache.get(&name) {
             log::trace!("Using PTX cache");
             self.load_ptx(
@@ -659,7 +659,7 @@ impl CudaContext {
             options.push("--use_fast_math");
         }
 
-        #[cfg(feature = "cache-ptx")]
+        #[cfg(feature = "cache-compilation")]
         let cluster_dim = compute_kernel.cluster_dim;
 
         let kernel_compiled = logger.debug(kernel_compiled);
@@ -684,7 +684,7 @@ impl CudaContext {
             cudarc::nvrtc::result::get_ptx(program).unwrap()
         };
 
-        #[cfg(feature = "cache-ptx")]
+        #[cfg(feature = "cache-compilation")]
         self.ptx_cache
             .insert(
                 name,
--- crates/cubecl-hip/src/compute/server.rs
+++ crates/cubecl-hip/src/compute/server.rs
@@ -25,6 +25,8 @@ use std::ffi::CString;
 use std::future::Future;
 use std::path::PathBuf;
 
+use cubecl_common::cache::{Cache, CacheOption};
+
 #[derive(Debug)]
 pub struct HipServer {
     ctx: HipContext,
@@ -38,6 +40,16 @@ pub(crate) struct HipContext {
     module_names: HashMap<KernelId, HipCompiledKernel>,
     timestamps: KernelTimestamps,
     compilation_options: CompilationOptions,
+    #[cfg(feature = "cache-compilation")]
+    compilation_cache: Cache<String, CompilationCacheEntry>,
+}
+
+#[cfg(feature = "cache-compilation")]
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone)]
+pub struct CompilationCacheEntry {
+    entrypoint_name: String,
+    cube_dim: (u32, u32, u32),
+    binary: Vec<i8>,
 }
 
 #[derive(Debug)]
@@ -323,6 +335,8 @@ impl HipContext {
             stream,
             timestamps: KernelTimestamps::default(),
             compilation_options,
+            #[cfg(feature = "cache-compilation")]
+            compilation_cache: Cache::new("hip/compilation", CacheOption::default()),
         }
     }
 
@@ -356,11 +370,28 @@ impl HipContext {
         logger: &mut DebugLogger,
         mode: ExecutionMode,
     ) {
+        #[cfg(feature = "cache-compilation")]
+        let name = kernel_id.stable_format();
+        #[cfg(feature = "cache-compilation")]
+        if let Some(entry) = self.compilation_cache.get(&name) {
+            log::trace!("Using compilation cache");
+            self.load_compiled_binary(
+                entry.binary.clone(),
+                kernel_id.clone(),
+                entry.entrypoint_name.clone(),
+                CubeDim {
+                    x: entry.cube_dim.0,
+                    y: entry.cube_dim.1,
+                    z: entry.cube_dim.2,
+                },
+            );
+            return;
+        }
+
         // CubeCL compilation
         // jitc = just-in-time compiled
         let mut jitc_kernel =
             cube_kernel.compile(&mut Default::default(), &self.compilation_options, mode);
-        let func_name = CString::new(jitc_kernel.entrypoint_name.clone()).unwrap();
 
         if logger.is_activated() {
             jitc_kernel.debug_info = Some(DebugInformation::new("cpp", kernel_id.clone()));
@@ -450,6 +481,40 @@ impl HipContext {
                 "Should load compiled code"
             );
         }
+
+        #[cfg(feature = "cache-compilation")]
+        self.compilation_cache
+            .insert(
+                name,
+                CompilationCacheEntry {
+                    entrypoint_name: jitc_kernel.entrypoint_name.clone(),
+                    cube_dim: (
+                        jitc_kernel.cube_dim.x,
+                        jitc_kernel.cube_dim.y,
+                        jitc_kernel.cube_dim.z,
+                    ),
+                    binary: code.clone(),
+                },
+            )
+            .unwrap();
+
+        self.load_compiled_binary(
+            code,
+            kernel_id.clone(),
+            jitc_kernel.entrypoint_name,
+            jitc_kernel.cube_dim,
+        );
+    }
+
+    fn load_compiled_binary(
+        &mut self,
+        code: Vec<i8>,
+        kernel_id: KernelId,
+        entrypoint_name: String,
+        cube_dim: CubeDim,
+    ) {
+        let func_name = CString::new(entrypoint_name.clone()).unwrap();
+
         // Create the HIP module
         let mut module: cubecl_hip_sys::hipModule_t = std::ptr::null_mut();
         unsafe {
@@ -471,7 +536,7 @@ impl HipContext {
             HipCompiledKernel {
                 _module: module,
                 func,
-                cube_dim: jitc_kernel.cube_dim,
+                cube_dim,
             },
         );
     }
