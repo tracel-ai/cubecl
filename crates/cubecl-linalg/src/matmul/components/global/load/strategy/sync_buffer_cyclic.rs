afbc4c9dbe88339e21891781272485322ba9a986
louisfd <louisfd94@gmail.com>
2025-04-29 11:54:34 -0400

    special case for k=1

--- crates/cubecl-linalg/src/matmul/components/global/load/strategy/sync_buffer_cyclic.rs
+++ crates/cubecl-linalg/src/matmul/components/global/load/strategy/sync_buffer_cyclic.rs
@@ -30,7 +30,17 @@ impl<TO: TilingOrder> LoadingValidation for LoadingStrategy<TO> {
 
         let total_units = config.plane_dim() * config.num_planes();
         let jump_length = total_units * line_size;
+
+        // let num_tiles_in_buffer = comptime! {match ident.as_input_ident() {
+
+        //     InputIdent::Lhs => tile_count_row,
+
+        //     InputIdent::Rhs => tile_count_col,
+
+        // }};
+        // but with k=1 one of them is one, it's the same as before
         let num_tiles_in_buffer = tile_count_row * tile_count_col;
+
         let total_num_lines = num_tiles_in_buffer * num_lines_per_tile;
         let num_lines_per_unit = (total_num_lines + total_units - 1) / total_units;
 
@@ -72,7 +82,16 @@ impl<TO: TilingOrder> SyncBufferLoadingStrategy for LoadingStrategy<TO> {
         let total_units = config.plane_dim() * config.num_planes();
         let jump_length = total_units * line_size;
 
+        // let num_tiles_in_buffer = comptime! {match ident.as_input_ident() {
+
+        //     InputIdent::Lhs => tile_count_row,
+
+        //     InputIdent::Rhs => tile_count_col,
+
+        // }};
+        // but with k=1 one of them is one, it's the same as before
         let num_tiles_in_buffer = tile_count_row * tile_count_col;
+
         let total_num_lines = num_tiles_in_buffer * num_lines_per_tile;
         let num_tasks = (total_num_lines + total_units - 1) / total_units;
 
@@ -128,65 +147,113 @@ impl<MP: MatmulPrecision, TO: TilingOrder> LoadingJob<MP, ContiguousTilingLayout
 
         let unit_position = this.unit_position_base + task_id * this.jump_length;
 
-        let tile_index = unit_position / tile_size;
-        let pos_within_tile = unit_position % tile_size;
+        // if k = 1
+        if comptime!(config.tiling_dimensions(Ident::Lhs).tile_count_col() == 1) {
+            let (tile_count_row, tile_count_col) = match comptime!(this.input_ident) {
+                InputIdent::Lhs => (tile_count_row, 2),
+                InputIdent::Rhs => (2, tile_count_col),
+            };
 
-        let (total_tile_count_row, total_tile_count_col) = match comptime!(this.input_ident) {
-            InputIdent::Lhs => (
-                comptime!(tile_count_row),
-                comptime!(tile_count_col * config.num_stages()),
-            ),
-            InputIdent::Rhs => (
-                comptime!(tile_count_row * config.num_stages()),
-                comptime!(tile_count_col),
-            ),
-        };
+            let unit_pos_in_buffer = unit_position / tile_size;
+            let pos_within_tile = unit_position % tile_size;
 
-        let (tile_x_within_buffer, tile_y_within_buffer) = TO::to_row_col::<G::SmmConfig>(
-            tile_index,
-            tile_count_row,
-            tile_count_col,
-            comptime!(this.input_ident.as_ident()),
-            comptime!(config.to_smm_config()),
-        );
+            let (tile_x, tile_y) = match comptime!(this.input_ident) {
+                InputIdent::Lhs => (unit_pos_in_buffer, this.buffer_index.runtime()),
+                InputIdent::Rhs => (this.buffer_index.runtime(), unit_pos_in_buffer),
+            };
 
-        let (tile_x, tile_y) = match comptime!(this.input_ident) {
-            InputIdent::Lhs => (
-                tile_x_within_buffer,
-                this.buffer_index * tile_count_col + tile_y_within_buffer,
-            ),
-            InputIdent::Rhs => (
-                this.buffer_index * tile_count_row + tile_x_within_buffer,
-                tile_y_within_buffer,
-            ),
-        };
+            let nth_tile = TO::to_nth_tile::<G::SmmConfig>(
+                tile_x,
+                tile_y,
+                tile_count_row,
+                tile_count_col,
+                comptime!(this.input_ident.as_ident()),
+                config.to_smm_config(),
+            );
 
-        let line_read = tensor_reader.load_coalesced_in_tile::<G>(
-            tile_x,
-            tile_y,
-            pos_within_tile,
-            this.input_ident,
-            config,
-        );
+            let line_read = tensor_reader.load_coalesced_in_tile::<G>(
+                tile_x,
+                tile_y,
+                pos_within_tile,
+                this.input_ident,
+                config,
+            );
 
-        let nth_tile_in_stage = TO::to_nth_tile::<G::SmmConfig>(
-            tile_x,
-            tile_y,
-            total_tile_count_row,
-            total_tile_count_col,
-            comptime!(this.input_ident.as_ident()),
-            config.to_smm_config(),
-        );
+            let tile_start = nth_tile * this.num_lines_per_tile;
+            let tile_end = tile_start + this.num_lines_per_tile;
+            let mut tile_slice = stage
+                .as_slice_mut(line_size)
+                .slice_mut(tile_start, tile_end);
 
-        let tile_start = nth_tile_in_stage * this.num_lines_per_tile;
-        let tile_end = tile_start + this.num_lines_per_tile;
-        let mut tile_slice = stage
-            .as_slice_mut(line_size)
-            .slice_mut(tile_start, tile_end);
+            tile_slice[pos_within_tile / line_size] = match quantization {
+                CubeOption::Some(quantization) => {
+                    quantization.dequantize(line_read, this.input_ident)
+                }
+                CubeOption::None => Line::cast_from(line_read),
+            }
+        } else {
+            let tile_index = unit_position / tile_size;
+            let pos_within_tile = unit_position % tile_size;
 
-        tile_slice[pos_within_tile / line_size] = match quantization {
-            CubeOption::Some(quantization) => quantization.dequantize(line_read, this.input_ident),
-            CubeOption::None => Line::cast_from(line_read),
+            let (total_tile_count_row, total_tile_count_col) = match comptime!(this.input_ident) {
+                InputIdent::Lhs => (
+                    comptime!(tile_count_row),
+                    comptime!(tile_count_col * config.num_stages()),
+                ),
+                InputIdent::Rhs => (
+                    comptime!(tile_count_row * config.num_stages()),
+                    comptime!(tile_count_col),
+                ),
+            };
+
+            let (tile_x_within_buffer, tile_y_within_buffer) = TO::to_row_col::<G::SmmConfig>(
+                tile_index,
+                tile_count_row,
+                tile_count_col,
+                comptime!(this.input_ident.as_ident()),
+                comptime!(config.to_smm_config()),
+            );
+
+            let (tile_x, tile_y) = match comptime!(this.input_ident) {
+                InputIdent::Lhs => (
+                    tile_x_within_buffer,
+                    this.buffer_index * tile_count_col + tile_y_within_buffer,
+                ),
+                InputIdent::Rhs => (
+                    this.buffer_index * tile_count_row + tile_x_within_buffer,
+                    tile_y_within_buffer,
+                ),
+            };
+
+            let line_read = tensor_reader.load_coalesced_in_tile::<G>(
+                tile_x,
+                tile_y,
+                pos_within_tile,
+                this.input_ident,
+                config,
+            );
+
+            let nth_tile_in_stage = TO::to_nth_tile::<G::SmmConfig>(
+                tile_x,
+                tile_y,
+                total_tile_count_row,
+                total_tile_count_col,
+                comptime!(this.input_ident.as_ident()),
+                config.to_smm_config(),
+            );
+
+            let tile_start = nth_tile_in_stage * this.num_lines_per_tile;
+            let tile_end = tile_start + this.num_lines_per_tile;
+            let mut tile_slice = stage
+                .as_slice_mut(line_size)
+                .slice_mut(tile_start, tile_end);
+
+            tile_slice[pos_within_tile / line_size] = match quantization {
+                CubeOption::Some(quantization) => {
+                    quantization.dequantize(line_read, this.input_ident)
+                }
+                CubeOption::None => Line::cast_from(line_read),
+            }
         }
     }
 
