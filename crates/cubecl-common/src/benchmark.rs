ebe6e0f9fe7492c89b579e58152c27c44ae2c237
nathaniel <nathaniel.simard.42@gmail.com>
2025-05-02 09:42:14 -0400

    Remove print

--- crates/cubecl-common/src/benchmark.rs
+++ crates/cubecl-common/src/benchmark.rs
@@ -347,7 +347,7 @@ where
     let git_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
     let durations = benchmark.run(TimingMethod::Full);
 
-    let result = BenchmarkResult {
+    BenchmarkResult {
         raw: durations.clone(),
         computed: BenchmarkComputations::new(&durations),
         git_hash,
@@ -355,10 +355,7 @@ where
         options: benchmark.options(),
         shapes: benchmark.shapes(),
         timestamp,
-    };
-
-    println!("{result}");
-    result
+    }
 }
 
 #[cfg(test)]
