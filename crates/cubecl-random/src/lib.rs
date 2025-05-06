ceea2c38de1e83bccf5148b1af6deb73bc114c72
Cielbird <guilhem.ane3@gmail.com>
2025-05-06 16:03:13 -0400

    [Feat] Random number generation

---
+++ crates/cubecl-random/src/lib.rs
@@ -0,0 +1,13 @@
+mod base;
+mod bernoulli;
+mod normal;
+mod uniform;
+
+use base::*;
+pub use bernoulli::*;
+pub use normal::*;
+pub use uniform::*;
+
+
+#[cfg(feature = "export_tests")]
+pub mod test;
