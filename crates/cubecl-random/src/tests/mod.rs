f8eb4aa513098620b8182e87426cb751e791403f
Cielbird <guilhem.ane3@gmail.com>
2025-05-07 12:23:10 -0400

    Improved tests for cubecl-random

---
+++ crates/cubecl-random/src/tests/mod.rs
@@ -0,0 +1,21 @@
+pub mod bernoulli;
+pub mod normal;
+pub mod uniform;
+
+#[allow(missing_docs)]
+#[macro_export]
+macro_rules! testgen_random {
+    () => {
+        use cubecl::prelude::*;
+        use cubecl_core as cubecl;
+
+        use cubecl::{client::ComputeClient, prelude::TensorHandleRef};
+        use cubecl_random::*;
+
+        use core::f32;
+
+        cubecl_random::testgen_random_bernoulli!();
+        cubecl_random::testgen_random_normal!();
+        cubecl_random::testgen_random_uniform!();
+    };
+}
