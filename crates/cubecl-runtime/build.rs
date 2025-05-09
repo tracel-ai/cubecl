afbd10438301eb82c866ba7881ee86cac32761c4
Arthur Brussee <arthur.brussee@gmail.com>
2025-05-09 02:18:56 +0100

    Fix wasm by always using cfg(std_io) (#670)

--- crates/cubecl-runtime/build.rs
+++ crates/cubecl-runtime/build.rs
@@ -3,7 +3,9 @@ use cfg_aliases::cfg_aliases;
 fn main() {
     // Setup cfg aliases
     cfg_aliases! {
-        autotune_persistent_cache: { all(feature = "std", any(target_os = "windows", target_os = "linux", target_os = "macos")) },
+        // Some features like autotune caching, compilation caching, and config loading rely on files/environment variables,
+        // which are only applicable on "standard desktop platforms".
+        std_io: { all(feature = "std", any(target_os = "windows", target_os = "linux", target_os = "macos")) },
         exclusive_memory_only: { any(feature = "exclusive-memory-only", target_family = "wasm") },
     }
 }
