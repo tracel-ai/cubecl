6d8d4ac42413040bf0c05120322535fc2b26c1a9
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-05-10 14:19:40 +0200

    Fix test read issue

--- crates/cubecl-linalg/src/tensor/tests/identity.rs
+++ crates/cubecl-linalg/src/tensor/tests/identity.rs
@@ -19,12 +19,14 @@ pub fn test_identity<R: Runtime, C: Numeric + CubeElement + Display>(
     let identity = TensorHandle::<R, C>::empty(&client, [dim, dim].to_vec());
     tensor::identity::launch(&client, &identity);
 
+    // Tensors may be padded for alignment purposes
+    let len = identity.shape.iter().product::<usize>() * size_of::<C>();
     let actual = client.read_one_tensor(identity.handle.clone().binding_with_meta(
         identity.shape,
         identity.strides,
         size_of::<C>(),
     ));
-    let actual = C::from_bytes(&actual);
+    let actual = C::from_bytes(&actual[..len]);
 
     assert_eq!(&expected[..], actual, "identity matrices are not equal.");
 }
