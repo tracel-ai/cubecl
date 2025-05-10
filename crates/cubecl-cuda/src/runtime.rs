e87ecf48c54d8027f584bf5adf41689ceaf39665
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-05-10 16:19:01 +0200

    Fix stride sorting on CUDA

--- crates/cubecl-cuda/src/runtime.rs
+++ crates/cubecl-cuda/src/runtime.rs
@@ -230,6 +230,7 @@ impl Runtime for CudaRuntime {
 
         let mut sorted = strides.to_vec();
         sorted.sort();
+        sorted.reverse();
 
         if sorted != strides {
             return false;
