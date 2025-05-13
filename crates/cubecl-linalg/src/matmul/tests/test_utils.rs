b92891d78dbd12e56a4e708d59ad13ee51dfa50c
louisfd <louisfd94@gmail.com>
2025-05-13 15:53:22 -0400

    fail on NaN

--- crates/cubecl-linalg/src/matmul/tests/test_utils.rs
+++ crates/cubecl-linalg/src/matmul/tests/test_utils.rs
@@ -131,13 +131,13 @@ pub(crate) fn assert_equals_approx<R: Runtime, F: Float + CubeElement + Display>
     // normalize to type epsilon
     let epsilon = (epsilon / f32::EPSILON * F::EPSILON.to_f32().unwrap()).max(epsilon);
 
-    // println!("{:?}", actual);
-    // println!("{:?}", expected);
     for (i, (a, e)) in actual.iter().zip(expected.iter()).enumerate() {
         // account for lower precision at higher values
         let allowed_error = (epsilon * e.to_f32().unwrap()).max(epsilon);
 
-        if f32::abs(a.to_f32().unwrap() - e.to_f32().unwrap()) >= allowed_error {
+        if f32::is_nan(a.to_f32().unwrap())
+            || f32::abs(a.to_f32().unwrap() - e.to_f32().unwrap()) >= allowed_error
+        {
             return Err(format!(
                 "Values differ more than epsilon: index={} actual={}, expected={}, difference={}, epsilon={}",
                 i,
