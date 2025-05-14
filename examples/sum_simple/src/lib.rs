d8996f634d6e603026b6df8223b8743bafb779cc
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-14 13:38:29 -0400

    mend

--- examples/sum_simple/src/lib.rs
+++ examples/sum_simple/src/lib.rs
@@ -14,7 +14,7 @@ fn gelu_array<F: Float>(input_a: &Array<F>, input_b: &Array<F>, output: &mut Arr
 pub fn launch<R: Runtime>(device: &R::Device) {
     let client = R::client(device);
     let input_a: [f32; 128] = std::array::from_fn(|i| i as f32);
-    let input_b: [f32; 128] = std::array::from_fn(|i| (i * i) as f32);
+    let input_b: [f32; 128] = std::array::from_fn(|i| i as f32);
     let output_handle = client.empty(input_a.len() * core::mem::size_of::<f32>());
     let input_a_handle = client.create(f32::as_bytes(&input_a));
     let input_b_handle = client.create(f32::as_bytes(&input_b));
