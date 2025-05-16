ef4afa50e78a0af888f1e359eda4c17ba76b204e
ArthurBrussee <arthur.brussee@gmail.com>
2025-05-16 14:20:51 +0200

    Fix HIP

--- crates/cubecl-hip/src/runtime.rs
+++ crates/cubecl-hip/src/runtime.rs
@@ -8,11 +8,13 @@ use cubecl_cpp::{
     },
 };
 
+use cubecl_common::id::DeviceId;
 use cubecl_core::{
-    AtomicFeature, CubeDim, DeviceId, Feature, MemoryConfiguration, Runtime,
+    AtomicFeature, CubeDim, Feature, MemoryConfiguration, Runtime,
     ir::{Elem, FloatKind},
 };
 use cubecl_hip_sys::HIP_SUCCESS;
+use cubecl_runtime::logging::ServerLogger;
 use cubecl_runtime::{
     ComputeRuntime, DeviceProperties,
     channel::MutexComputeChannel,
@@ -20,6 +22,7 @@ use cubecl_runtime::{
     memory_management::{HardwareProperties, MemoryDeviceProperties, MemoryManagement},
     storage::ComputeStorage,
 };
+use std::sync::Arc;
 
 use crate::{
     HipWmmaCompiler,
@@ -138,12 +141,7 @@ fn create_client<M: DialectWmmaCompiler<HipDialect<M>>>(
     };
     let memory_management =
         MemoryManagement::from_configuration(storage, &mem_properties, options.memory_config);
-    let mut device_props = DeviceProperties::new(
-        &[Feature::Plane],
-        mem_properties,
-        topology,
-        cubecl_runtime::TimeMeasurement::System,
-    );
+    let mut device_props = DeviceProperties::new(&[Feature::Plane], mem_properties, topology);
     register_supported_types(&mut device_props);
     // Not sure if there's a good way to check for support on HIP
     device_props.register_feature(Feature::Type(Elem::AtomicFloat(FloatKind::F64)));
@@ -197,7 +195,7 @@ impl Runtime for HipRuntime {
         (i32::MAX as u32, u16::MAX as u32, u16::MAX as u32)
     }
 
-    fn device_id(device: &Self::Device) -> cubecl_core::DeviceId {
+    fn device_id(device: &Self::Device) -> DeviceId {
         DeviceId::new(0, device.index as u32)
     }
 
