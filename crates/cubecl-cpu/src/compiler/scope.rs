9f6d39f2e60b64bdcb68f3d5412b1b8ce9e17e91
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-20 15:22:59 -0400

    feat: create a kernel function and call it without argument

--- crates/cubecl-cpu/src/compiler/scope.rs
+++
@@ -1,9 +0,0 @@
-use cubecl_core::ir::Scope;
-
-use super::MLIRCompiler;
-
-impl MLIRCompiler {
-    pub(super) fn visit(&mut self, scope: &Scope) {
-        println!("{:?}", scope.locals);
-    }
-}
