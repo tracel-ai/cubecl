5e737baec74a2a3e8400d5a6ecf61995e475ff2c
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-22 15:51:53 -0400

    feat: pass buffer as parameter

---
+++ crates/cubecl-cpu/src/compiler/mlir/prelude.rs
@@ -0,0 +1,6 @@
+#![allow(unused)]
+
+pub(super) use super::item::TransformItem;
+pub(super) use super::types::TransformType;
+
+pub(super) use melior::Context;
---
+++ crates/cubecl-cpu/src/compiler/mlir/types.rs
@@ -0,0 +1,45 @@
+use cubecl_core::Feature;
+use cubecl_core::ir;
+use cubecl_core::ir::Elem;
+use cubecl_core::ir::FloatKind;
+use cubecl_runtime::DeviceProperties;
+use melior::Context;
+use melior::ir::Type;
+
+pub(super) trait TransformType {
+    fn visit_type<'a>(&self, context: &'a Context) -> Type<'a>;
+}
+
+impl TransformType for Elem {
+    fn visit_type<'a>(&self, context: &'a Context) -> Type<'a> {
+        match self {
+            Elem::Float(FloatKind::F32) => Type::float32(context),
+            _ => todo!("This type is not implemented yet."),
+        }
+    }
+}
+
+pub fn register_supported_types(props: &mut DeviceProperties<Feature>) {
+    let supported_types = [
+        // ir::Elem::UInt(ir::UIntKind::U8),
+        // ir::Elem::UInt(ir::UIntKind::U16),
+        // ir::Elem::UInt(ir::UIntKind::U32),
+        // ir::Elem::UInt(ir::UIntKind::U64),
+        // ir::Elem::Int(ir::IntKind::I8),
+        // ir::Elem::Int(ir::IntKind::I16),
+        // ir::Elem::Int(ir::IntKind::I32),
+        // ir::Elem::Int(ir::IntKind::I64),
+        // ir::Elem::AtomicInt(ir::IntKind::I32),
+        // ir::Elem::AtomicInt(ir::IntKind::I64),
+        // ir::Elem::AtomicUInt(ir::UIntKind::U32),
+        // ir::Elem::AtomicUInt(ir::UIntKind::U64),
+        // ir::Elem::Float(ir::FloatKind::F16),
+        ir::Elem::Float(ir::FloatKind::F32),
+        // ir::Elem::Float(ir::FloatKind::F64),
+        // ir::Elem::Bool,
+    ];
+
+    for ty in supported_types {
+        props.register_feature(Feature::Type(ty));
+    }
+}
