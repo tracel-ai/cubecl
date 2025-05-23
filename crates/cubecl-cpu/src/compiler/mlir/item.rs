62feff0d16ff684235e1608d4f34590bc0c37126
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-23 12:29:54 -0400

    feat: move trait implementation to visitor struct

--- crates/cubecl-cpu/src/compiler/mlir/item.rs
+++ crates/cubecl-cpu/src/compiler/mlir/item.rs
@@ -1,23 +1,25 @@
 use cubecl_core::ir::Item;
 use melior::ir::Type;
 
-use super::prelude::*;
+use super::visitor::Visitor;
 
-pub(super) trait VisitItem {
-    fn visit<'a>(&self, context: &'a Context) -> Type<'a>;
-    fn size(&self) -> usize;
-}
-
-impl VisitItem for Item {
-    fn visit<'a>(&self, context: &'a Context) -> Type<'a> {
-        let inner_type = self.elem.visit(context);
-        match self.vectorization {
+impl<'a> Visitor<'a> {
+    pub fn item_to_type(&self, item: Item) -> Type<'a> {
+        let inner_type = self.elem_to_type(item.elem);
+        match item.vectorization {
             Some(size) => Type::vector(&[size.get() as u64], inner_type),
             None => inner_type,
         }
     }
-    // Get the sizes in bytes
-    fn size(&self) -> usize {
+}
+
+pub(super) trait ElemSize {
+    fn size(self) -> usize;
+}
+
+impl ElemSize for Item {
+    /// Get the sizes in bytes
+    fn size(self) -> usize {
         let inner_size = self.elem.size();
         match self.vectorization {
             Some(size) => size.get() as usize * inner_size,
