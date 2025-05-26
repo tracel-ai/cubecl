ac10dabdf450e85d997d2f333229832991d5ab6a
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-26 18:34:51 -0400

    feat: implement the basis, but it is segfaulting rn

--- crates/cubecl-cpu/src/compiler/mlir/scope.rs
+++ crates/cubecl-cpu/src/compiler/mlir/scope.rs
@@ -4,7 +4,7 @@ use super::visitor::Visitor;
 
 impl<'a> Visitor<'a> {
     pub fn visit_scope(&mut self, scope: &Scope) {
-        for instruction in scope.instructions.iter().take(2) {
+        for instruction in scope.instructions.iter().take(4) {
             self.visit_instruction(instruction);
         }
     }
