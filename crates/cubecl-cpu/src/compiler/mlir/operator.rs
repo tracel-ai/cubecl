887dfcd7ca088ac9b5efcb3cd4b15c668e910f5b
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-26 18:34:51 -0400

    feat: implement the basis, but it is segfaulting rn

--- crates/cubecl-cpu/src/compiler/mlir/operator.rs
+++ crates/cubecl-cpu/src/compiler/mlir/operator.rs
@@ -1,6 +1,6 @@
 use cubecl_core::ir::Operator;
 use melior::{
-    dialect::memref,
+    dialect::ods::vector,
     ir::{BlockLike, Value},
 };
 
@@ -11,14 +11,26 @@ impl<'a> Visitor<'a> {
         match operator {
             Operator::Index(index_operator) => {
                 let memref = self.get_variable(index_operator.list);
-                let operation = memref::load(memref, &[], self.location).into();
+                let vector_type = self.item_to_type(index_operator.list.item);
+                let index = self.get_variable(index_operator.index);
+                let operation =
+                    vector::load(self.context, vector_type, memref, &[index], self.location).into();
                 let load_ssa = self
                     .block()
                     .append_operation(operation)
                     .result(0)
                     .unwrap()
                     .into();
-                let operation = memref::store(load_ssa, out, &[], self.location).into();
+                let index = self.zero_index();
+                let operation =
+                    vector::store(self.context, load_ssa, out, &[index], self.location).into();
+                self.block().append_operation(operation);
+            }
+            Operator::IndexAssign(index_assign) => {
+                let memref = self.get_variable_vector(index_assign.value);
+                let index = self.get_variable(index_assign.index);
+                let operation =
+                    vector::store(self.context, memref, out, &[index], self.location).into();
                 self.block().append_operation(operation);
             }
             _ => todo!("{} is not yet implemented", operator),
--- crates/cubecl-cpu/src/compiler/mlir/variable.rs
+++ crates/cubecl-cpu/src/compiler/mlir/variable.rs
@@ -1,6 +1,6 @@
 use cubecl_core::ir::{Variable, VariableKind};
 use melior::{
-    dialect::{arith, memref},
+    dialect::{arith, memref, ods::vector},
     ir::{BlockLike, Type, Value, attribute::IntegerAttribute},
 };
 
@@ -10,7 +10,7 @@ impl<'a> Visitor<'a> {
     pub fn visit_variable(&mut self, variable: Variable) -> Value<'a, 'a> {
         match variable.kind {
             // VariableKind::GlobalInputArray(id) => {}
-            // VariableKind::GlobalOutputArray(id) => {}
+            VariableKind::GlobalOutputArray(id) => self.global_buffers[id as usize],
             VariableKind::LocalConst { id } => {
                 let ptr_type = self.item_to_memref_type(variable.item);
                 let operation =
@@ -28,7 +28,7 @@ impl<'a> Visitor<'a> {
             _ => todo!(),
         }
     }
-    pub fn get_variable(&mut self, variable: Variable) -> Value<'a, 'a> {
+    pub fn get_variable(&self, variable: Variable) -> Value<'a, 'a> {
         match variable.kind {
             VariableKind::GlobalInputArray(id) | VariableKind::GlobalOutputArray(id) => {
                 self.global_buffers[id as usize]
@@ -38,15 +38,33 @@ impl<'a> Visitor<'a> {
                 .get(&id)
                 .expect("Variable should have been declared before")
                 .clone(),
-            VariableKind::Builtin(_builtin) => {
-                let attribute = IntegerAttribute::new(Type::index(self.context), 0).into();
-                self.block()
-                    .append_operation(arith::constant(self.context, attribute, self.location))
-                    .result(0)
-                    .unwrap()
-                    .into()
-            }
+            VariableKind::Builtin(_builtin) => self.zero_index(),
             _ => todo!("{:?} is not yet implemented", variable.kind),
         }
+    }
+    pub fn zero_index(&self) -> Value<'a, 'a> {
+        let attribute = IntegerAttribute::new(Type::index(self.context), 0).into();
+        self.block()
+            .append_operation(arith::constant(self.context, attribute, self.location))
+            .result(0)
+            .unwrap()
+            .into()
+    }
+    pub fn get_variable_vector(&self, variable: Variable) -> Value<'a, 'a> {
+        let value = self.get_variable(variable);
+        let vector_type = self.item_to_type(variable.item);
+        let operation = vector::load(
+            self.context,
+            vector_type,
+            value,
+            &[self.zero_index()],
+            self.location,
+        )
+        .into();
+        self.block()
+            .append_operation(operation)
+            .result(0)
+            .unwrap()
+            .into()
     }
 }
