7754429177a8a54425be074ad20df0c282ae24dc
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-05-27 16:07:11 -0400

    chore: fix conflicts

--- crates/cubecl-cpu/src/compiler/mod.rs
+++ crates/cubecl-cpu/src/compiler/mod.rs
@@ -4,13 +4,8 @@ use cubecl_core::{Compiler, ExecutionMode, ir, prelude::KernelDefinition};
 use cubecl_opt::OptimizerBuilder;
 use mlir::MlirEngine;
 
-<<<<<<< HEAD
 #[derive(Clone, Debug, Default)]
 pub struct MlirCompiler {}
-=======
-#[derive(Debug, Clone, Default)]
-pub struct MLIRCompiler {}
->>>>>>> e46fd3f9 (feat: add display for scope)
 
 #[derive(Default, Debug)]
 pub struct MlirCompilerOptions {}
@@ -26,14 +21,9 @@ impl Compiler for MlirCompiler {
         _compilation_options: &Self::CompilationOptions, // TODO pass this through the visitor, though it doesn't need anything for the moment
         mode: ExecutionMode, // TODO support this by adding array bound checking
     ) -> Self::Representation {
-<<<<<<< HEAD
         let opt = OptimizerBuilder::default().optimize(kernel.body.clone(), kernel.cube_dim, mode);
         kernel.body = opt.root_scope;
         MlirEngine::from_cubecl_ir(kernel)
-=======
-        println!("{}", kernel.body);
-        MLIRKernel
->>>>>>> e46fd3f9 (feat: add display for scope)
     }
 
     fn elem_size(&self, elem: ir::Elem) -> usize {
