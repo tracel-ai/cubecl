87bd125aecdc238198c7bb9ffdd658cda580082e
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-06-03 16:21:24 -0400

    feat: add dot product and absolute support

--- crates/cubecl-cpu/src/compiler/mlir/visitor/item.rs
+++ crates/cubecl-cpu/src/compiler/mlir/visitor/item.rs
@@ -1,9 +1,49 @@
 use cubecl_core::ir::Item;
-use melior::ir::{Type, r#type::MemRefType};
+use melior::{
+    dialect::{arith, ods::vector},
+    ir::{
+        Type, Value,
+        attribute::{FloatAttribute, IntegerAttribute},
+        r#type::MemRefType,
+    },
+};
 
 use super::Visitor;
 
 impl<'a> Visitor<'a> {
+    pub fn create_float_constant_from_item(&self, item: Item, constant: f64) -> Value<'a, 'a> {
+        let constant = FloatAttribute::new(self.context, Type::float32(self.context), constant);
+        let constant = self.append_operation_with_result(arith::constant(
+            self.context,
+            constant.into(),
+            self.location,
+        ));
+        let vector = self.item_to_type(item);
+        self.append_operation_with_result(vector::splat(
+            self.context,
+            vector,
+            constant,
+            self.location,
+        ))
+    }
+
+    pub fn create_int_constant_from_item(&self, item: Item, constant: i64) -> Value<'a, 'a> {
+        let integer = self.elem_to_type(item.elem);
+        let constant = IntegerAttribute::new(integer, constant);
+        let constant = self.append_operation_with_result(arith::constant(
+            self.context,
+            constant.into(),
+            self.location,
+        ));
+        let vector = self.item_to_type(item);
+        self.append_operation_with_result(vector::splat(
+            self.context,
+            vector,
+            constant,
+            self.location,
+        ))
+    }
+
     pub fn item_to_type(&self, item: Item) -> Type<'a> {
         let inner_type = self.elem_to_type(item.elem);
         match item.vectorization {
