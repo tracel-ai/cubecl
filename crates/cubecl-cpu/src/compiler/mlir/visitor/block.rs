1c070a412b546c449556beb2cab553beef65fb4a
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-06-09 18:53:17 -0400

    feat: implement if/else, but it segfault at some point for a test

--- crates/cubecl-cpu/src/compiler/mlir/visitor/block.rs
+++ crates/cubecl-cpu/src/compiler/mlir/visitor/block.rs
@@ -1,27 +1,60 @@
 use std::ops::Deref;
 
 use cubecl_opt::{ControlFlow, NodeIndex, Optimizer};
+use melior::{
+    dialect::cf,
+    ir::{Block, BlockLike, BlockRef, RegionLike},
+};
 
-use super::Visitor;
+use super::prelude::*;
 
 impl<'a> Visitor<'a> {
-    pub fn visit_basic_block(&mut self, block_id: NodeIndex, opt: &Optimizer) {
+    pub fn visit_basic_block(&mut self, block_id: NodeIndex, opt: &Optimizer) -> BlockRef<'a, 'a> {
+        if let Some(block) = self.blocks.get(&block_id) {
+            return *block;
+        }
+
         let basic_block = opt.block(block_id);
 
-        let ops = opt.block(block_id).ops.borrow();
+        let mut arguments = vec![];
 
-        for (_, instruction) in ops.iter() {
+        // for phi_nodes in basic_block.phi_nodes.borrow().iter() {
+        //     let argument_type = phi_nodes.out.item.to_type(self.context);
+        //     arguments.push((argument_type, self.location));
+        // }
+
+        let block = Block::new(&arguments);
+        let this_block = self.current_region.append_block(block);
+        self.current_block = this_block;
+
+        self.blocks.insert(block_id, this_block);
+        for (_, instruction) in basic_block.ops.borrow().iter() {
             self.visit_instruction(instruction);
         }
 
-        drop(ops);
         match basic_block.control_flow.borrow().deref() {
-            // ControlFlow::IfElse {
-            //     cond,
-            //     then,
-            //     or_else,
-            //     merge,
-            // } => {}
+            ControlFlow::IfElse {
+                cond,
+                then,
+                or_else,
+                merge,
+            } => {
+                let condition = self.get_variable(*cond);
+                let true_successor = self.visit_basic_block(*then, opt);
+                let false_successor = self.visit_basic_block(*or_else, opt);
+                this_block.append_operation(cf::cond_br(
+                    self.context,
+                    condition,
+                    true_successor.deref(),
+                    false_successor.deref(),
+                    &[],
+                    &[],
+                    self.location,
+                ));
+                if let Some(merge) = merge {
+                    self.visit_basic_block(*merge, opt);
+                }
+            }
             // ControlFlow::Switch {
             //     value,
             //     default,
@@ -40,10 +73,14 @@ impl<'a> Visitor<'a> {
             //    merge,
             //} => todo!(),
             ControlFlow::Return => {
-                // Implementation needs to jump to the last return because func.return is not really conceived for return inside the function
+                this_block.append_operation(cf::br(&self.last_block, &[], self.location));
             }
-            ControlFlow::None => (),
+            ControlFlow::None => {
+                let successor = self.visit_basic_block(opt.successors(block_id)[0], opt);
+                this_block.append_operation(cf::br(successor.deref(), &[], self.location));
+            }
             _ => todo!("{:?}", basic_block.control_flow),
         };
+        this_block
     }
 }
