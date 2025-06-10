985be9c012d18f675b570bd70ceadfffd69469a0
ArthurBrussee <arthur.brussee@gmail.com>
2025-06-10 12:17:37 -0700

    CI

--- crates/cubecl-hip/src/compute/server.rs
+++ crates/cubecl-hip/src/compute/server.rs
@@ -13,7 +13,6 @@ use cubecl_core::compute::DebugInformation;
 use cubecl_core::prelude::*;
 use cubecl_core::{Feature, server::Bindings};
 use cubecl_hip_sys::{HIP_SUCCESS, get_hip_include_path, hiprtcResult_HIPRTC_SUCCESS};
-use cubecl_runtime::kernel_timestamps::KernelTimestamps;
 use cubecl_runtime::logging::ServerLogger;
 use cubecl_runtime::memory_management::MemoryUsage;
 use cubecl_runtime::memory_management::offset_handles;
