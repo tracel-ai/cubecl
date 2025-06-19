42f1edc93689bd87805112344a686353a93913fb
Lucca Ochsner <ochsner.lucca@gmail.com>
2025-06-19 09:52:42 +0200

    Farm starting point

--- crates/cubecl-farm/src/lib.rs
+++ crates/cubecl-farm/src/lib.rs
@@ -1,16 +1,119 @@
-use std::{collections::HashMap, sync::Arc};
+/*!
+
+Every Farm is composed out of FarmGroup's. In a FarmGroup we find a vector of FarmUnit. When a unit (a single GPU) is linked trough something like a NcclComm it has the state Linked. Otherwise it's seen as Single. With the given implementations we should be able get a good frame to iteraite over our Farm!
+
+For now i will allign the trait FarmCube allong Nccl. The grouping of Nccl here is moved to Rust. This should make the concept more usable for Rust devs. The goal here will be to first just start each device in its own thread to then use a thread per group to manage all concurrent devices. With some helper functions we should be able to send kernels to all devices and so on. Later there will be cpu thread strategies implemented. Also posting to the CPU while having traing loops could be implemented further down the road.
+
+*/
 
 use cubecl_core::{Runtime, client::ComputeClient};
-use cubecl_cuda::{CudaDevice, CudaRuntime, RuntimeOptions};
-use cubecl_runtime::id::DeviceId;
+use cubecl_cuda::{CudaDevice, CudaRuntime};
 use cudarc::driver::*;
-use cudarc::nccl::{self, Comm};
+use cudarc::nccl::{self};
+use std::collections::HashMap;
 
-struct Farm<R: Runtime, C, I> {
-    id: usize,
-    gpu_count: usize,
-    clients: Arc<HashMap<I, ComputeClient<R::Server, R::Channel>>>,
-    comms: Option<Vec<Option<C>>>,
+// TODO:! The Iteration is set up. A entry point with proper guards is needed. Something like the ComputeClient just with extra functions, rest will happen in the background on the long run.
+
+pub struct Farm<R: Runtime, L> {
+    pub id: usize,
+    pub unit_count: usize,
+    pub groups: Vec<FarmGroup<R, L>>,
+}
+
+impl<R: Runtime, L> Farm<R, L> {
+    pub fn all_units(&self) -> impl Iterator<Item = &FarmUnit<R, L>> {
+        self.groups.iter().flat_map(|g| g.units.iter())
+    }
+
+    pub fn linked_units(&self) -> impl Iterator<Item = &FarmUnit<R, L>> {
+        self.all_units().filter(|u| u.is_linked())
+    }
+
+    pub fn single_units(&self) -> impl Iterator<Item = &FarmUnit<R, L>> {
+        self.all_units().filter(|u| !u.is_linked())
+    }
+
+    pub fn group(&self, group_id: usize) -> Option<&FarmGroup<R, L>> {
+        self.groups.iter().find(|g| g.id == group_id)
+    }
+
+    pub fn group_count(&self) -> usize {
+        self.groups.len()
+    }
+
+    pub fn total_units(&self) -> usize {
+        self.groups.iter().map(|g| g.units.len()).sum()
+    }
+}
+
+pub struct FarmGroup<R: Runtime, L> {
+    pub id: usize,
+    pub units: Vec<FarmUnit<R, L>>,
+}
+
+impl<R: Runtime, L> FarmGroup<R, L> {
+    pub fn has_links(&self) -> bool {
+        self.units.iter().any(|u| u.is_linked())
+    }
+
+    pub fn size(&self) -> usize {
+        self.units.len()
+    }
+
+    pub fn is_single(&self) -> bool {
+        self.units.len() == 1
+    }
+
+    pub fn linked_units(&self) -> impl Iterator<Item = &FarmUnit<R, L>> {
+        self.units.iter().filter(|u| u.is_linked())
+    }
+}
+
+pub enum FarmUnit<R: Runtime, L> {
+    Linked {
+        id: usize,
+        device_index: usize,
+        client: ComputeClient<R::Server, R::Channel>,
+        link: L,
+    },
+    Single {
+        device_index: usize,
+        client: ComputeClient<R::Server, R::Channel>,
+    },
+}
+
+impl<R: Runtime, L> FarmUnit<R, L> {
+    pub fn device_index(&self) -> usize {
+        match self {
+            FarmUnit::Linked { device_index, .. } => *device_index,
+            FarmUnit::Single { device_index, .. } => *device_index,
+        }
+    }
+
+    pub fn client(&self) -> &ComputeClient<R::Server, R::Channel> {
+        match self {
+            FarmUnit::Linked { client, .. } => client,
+            FarmUnit::Single { client, .. } => client,
+        }
+    }
+
+    pub fn is_linked(&self) -> bool {
+        matches!(self, FarmUnit::Linked { .. })
+    }
+
+    pub fn link(&self) -> Option<&L> {
+        match self {
+            FarmUnit::Linked { link, .. } => Some(link),
+            FarmUnit::Single { .. } => None,
+        }
+    }
+
+    pub fn rank(&self) -> Option<usize> {
+        match self {
+            FarmUnit::Linked { id, .. } => Some(*id),
+            FarmUnit::Single { .. } => None,
+        }
+    }
 }
 
 pub enum GroupSplit {
@@ -31,185 +134,112 @@ pub trait CubeFarm {
     ) -> Result<Farm<Self::R, Self::Comm, Self::Device>, CudaError>;
 }
 
-impl CubeFarm for CudaRuntime {
+impl FarmCube for CudaRuntime {
     type R = CudaRuntime;
-    type Comm = nccl::Comm;
-    type Device = CudaDevice;
+    type Link = nccl::Comm;
 
-    fn init(
-        id: usize,
-        group: GroupSplit,
-    ) -> Result<Farm<Self::R, Self::Comm, Self::Device>, CudaError> {
+    fn init(group_split: GroupSplit) -> Result<Farm<Self::R, Self::Link>, CudaError> {
         result::init()?;
-        let gpu_count = result::device::get_count()? as usize;
-
-        let mut clients_map = HashMap::new();
-        let mut devices = Vec::new();
-        let mut contexts = Vec::new();
-
-        match (gpu_count, group) {
-            (1, _) => {
-                let device_id = CudaDevice { index: 0, group: 0 };
-                let client = CudaRuntime::client(&device_id);
-                let ctx = CudaContext::new(0)?;
-                contexts.push(ctx);
-                clients_map.insert(device_id.clone(), client);
-                devices.push(device_id);
+        let unit_count = result::device::get_count()? as usize;
+        let assignments = match (unit_count, group_split) {
+            (1, _) => vec![0],
+            (n, GroupSplit::SingleGroup) => vec![0; n],
+            (n, GroupSplit::Proportional(proportions)) => {
+                assign_units_proportional(n, &proportions)
             }
-
-            (n, GroupSplit::SingleGroup) if n > 1 => {
-                for gpu_index in 0..n {
-                    let device_id = CudaDevice {
-                        index: gpu_index,
-                        group: 0,
-                    };
-                    let client = CudaRuntime::client(&device_id);
-                    let ctx = CudaContext::new(gpu_index)?;
-                    contexts.push(ctx);
-                    clients_map.insert(device_id.clone(), client);
-                    devices.push(device_id);
-                }
-            }
-
-            (n, GroupSplit::SingleGpu) if n > 1 => {
-                if id < n {
-                    let device_id = CudaDevice {
-                        index: id,
-                        group: id,
-                    };
-                    let client = CudaRuntime::client(&device_id);
-                    let ctx = CudaContext::new(id)?;
-                    contexts.push(ctx);
-                    clients_map.insert(device_id.clone(), client);
-                    devices.push(device_id);
-                } else {
-                    return Err(CudaError::InvalidDevice);
-                }
-            }
-
-            (n, GroupSplit::Propotional(proportions)) if n > 1 => {
-                let gpu_assignments = gpus_proportional(n, &proportions);
-                for (group_id, gpu_indices) in gpu_assignments.iter().enumerate() {
-                    if group_id == id {
-                        for &gpu_index in gpu_indices {
-                            let device_id = CudaDevice {
-                                index: gpu_index,
-                                group: group_id,
-                            };
-                            let client = CudaRuntime::client(&device_id);
-                            let ctx = CudaContext::new(gpu_index)?;
-                            contexts.push(ctx);
-                            clients_map.insert(device_id.clone(), client);
-                            devices.push(device_id);
-                        }
-                    }
-                }
-            }
-
-            (n, GroupSplit::Explicit(assignments)) if n > 1 => {
-                for (gpu_index, &group_id) in assignments.iter().enumerate() {
-                    if gpu_index < n && group_id == id {
-                        let device_id = CudaDevice {
-                            index: gpu_index,
-                            group: group_id,
-                        };
-                        let client = CudaRuntime::client(&device_id);
-                        let ctx = CudaContext::new(gpu_index)?;
-                        contexts.push(ctx);
-                        clients_map.insert(device_id.clone(), client);
-                        devices.push(device_id);
-                    }
-                }
-            }
-
-            _ => {
-                return Err(CudaError::InvalidConfiguration);
-            }
-        }
-
-        let comms = if devices.len() > 1 {
-            let mut devices_by_group: HashMap<usize, Vec<&CudaDevice>> = HashMap::new();
-            for device in &devices {
-                devices_by_group
-                    .entry(device.group)
-                    .or_insert_with(Vec::new)
-                    .push(device);
-            }
-            let mut all_comms = Vec::new();
-            for (group_id, group_devices) in devices_by_group {
-                if group_devices.len() > 1 {
-                    let nccl_id = nccl::Id::new()?;
-                    let mut group_streams = Vec::new();
-                    let mut group_device_indices = Vec::new();
-                    for device in group_devices {
-                        let ctx = CudaContext::new(device.index)?;
-                        let stream = ctx.default_stream();
-                        group_streams.push(stream);
-                        group_device_indices.push(device.index);
-                    }
-                    for (rank, (stream, device_index)) in group_streams
-                        .into_iter()
-                        .zip(group_device_indices.iter())
-                        .enumerate()
-                    {
-                        let comm = nccl::Comm::from_rank(
-                            stream,
-                            rank,
-                            group_device_indices.len(),
-                            nccl_id.clone(),
-                        )?;
-                        if devices
-                            .iter()
-                            .any(|d| d.index == *device_index && d.group == group_id)
-                        {
-                            all_comms.push(Some(comm));
-                        }
-                    }
-                } else {
-                    break;
-                }
-            }
-
-            if all_comms.is_empty() {
-                None
-            } else {
-                Some(all_comms)
-            }
-        } else {
-            None
+            (_, GroupSplit::Explicit(assignments)) => assignments,
         };
 
+        if assignments.len() > unit_count {
+            return Err(CudaError::InvalidConfiguration);
+        }
+        let mut unit_group_map: HashMap<usize, Vec<usize>> = HashMap::new();
+
+        for (unit_index, &group_id) in assignments.iter().enumerate() {
+            if unit_index < unit_count {
+                unit_group_map
+                    .entry(group_id)
+                    .or_insert_with(Vec::new)
+                    .push(unit_index);
+            }
+        }
+        let mut groups = Vec::new();
+
+        for (group_id, unit_indices) in unit_group_map {
+            let mut units = Vec::new();
+
+            if unit_indices.len() > 1 {
+                let nccl_id = nccl::Id::new()?;
+                let mut streams = Vec::new();
+
+                for &unit_index in &unit_indices {
+                    let ctx = CudaContext::new(unit_index)?;
+                    let stream = ctx.default_stream();
+                    streams.push(stream);
+                }
+
+                for (rank, (&unit_index, stream)) in unit_indices.iter().zip(streams).enumerate() {
+                    let link =
+                        nccl::Comm::from_rank(stream, rank, unit_indices.len(), nccl_id.clone())?;
+                    let unit = CudaDevice { index: unit_index };
+                    let client = CudaRuntime::client(&unit);
+
+                    units.push(FarmUnit::Linked {
+                        id: rank,
+                        device_index: unit_index,
+                        client,
+                        link: link,
+                    });
+                }
+            } else {
+                let unit_index = unit_indices[0];
+                let unit = CudaDevice { index: unit_index };
+                let client = CudaRuntime::client(&unit);
+
+                units.push(FarmUnit::Single {
+                    device_index: unit_index,
+                    client,
+                });
+            }
+
+            groups.push(FarmGroup {
+                id: group_id,
+                units,
+            });
+        }
+        groups.sort_by_key(|g| g.id);
         Ok(Farm {
-            id,
-            gpu_count,
-            clients: Arc::new(clients_map),
-            comms,
+            id: 0,
+            unit_count,
+            groups,
         })
     }
 }
 
-fn gpus_proportional(gpu_count: usize, proportions: &[f64]) -> Vec<Vec<usize>> {
+fn assign_units_proportional(unit_count: usize, proportions: &[f64]) -> Vec<usize> {
     let total: f64 = proportions.iter().sum();
-    let mut groups = vec![vec![]; proportions.len()];
-    let mut assigned = 0;
-    for (i, &prop) in proportions.iter().enumerate() {
-        let count = ((prop / total) * gpu_count as f64).round() as usize;
-        let actual_count = count.min(gpu_count - assigned);
+    let mut assignments = vec![0; unit_count];
+    let mut current_unit = 0;
 
-        for j in 0..actual_count {
-            groups[i].push(assigned + j);
+    for (group_id, &proportion) in proportions.iter().enumerate() {
+        let group_size = ((proportion / total) * unit_count as f64).round() as usize;
+        let actual_size = group_size.min(unit_count - current_unit);
+
+        for _ in 0..actual_size {
+            if current_unit < unit_count {
+                assignments[current_unit] = group_id;
+                current_unit += 1;
+            }
         }
-        assigned += actual_count;
     }
-    let mut group_idx = 0;
-    let group_count = groups.len();
-    while assigned < gpu_count {
-        groups[group_idx % group_count].push(assigned);
-        assigned += 1;
-        group_idx += 1;
-    }
+    let mut group_id = 0;
 
-    groups
+    while current_unit < unit_count {
+        assignments[current_unit] = group_id % proportions.len();
+        current_unit += 1;
+        group_id += 1;
+    }
+    assignments
 }
 
 #[derive(Debug)]
