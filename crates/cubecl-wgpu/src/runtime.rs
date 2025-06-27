faa27d67535892deb1c4ee1352e59bbb6f7ef33e
nathaniel <nathaniel.simard.42@gmail.com>
2025-06-27 16:22:05 -0400

    Revert wrong change

--- crates/cubecl-wgpu/src/runtime.rs
+++ crates/cubecl-wgpu/src/runtime.rs
@@ -226,7 +226,7 @@ pub(crate) fn create_client_on_setup(
         #[cfg(apple_silicon)]
         plane_size_min: 32,
         #[cfg(not(apple_silicon))]
-        plane_size_min: adapter_limits.max_subgroup_size,
+        plane_size_min: adapter_limits.min_subgroup_size,
         plane_size_max: adapter_limits.max_subgroup_size,
         max_bindings: limits.max_storage_buffers_per_shader_stage,
         max_shared_memory_size: limits.max_compute_workgroup_storage_size as usize,
