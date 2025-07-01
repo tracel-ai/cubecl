bbeddf3be25e20b7885f20443a8bc08d4816d664
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-07-01 17:52:44 -0400

    feat: add destructor for the threadpool

--- crates/cubecl-cpu/src/compute/worker.rs
+++ crates/cubecl-cpu/src/compute/worker.rs
@@ -1,31 +1,33 @@
+use dtor::dtor;
 use std::sync::mpsc;
 use std::time::Duration;
 use std::{
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
+    sync::atomic::{AtomicBool, Ordering},
     thread,
 };
 
 use super::compute_task::ComputeTask;
 
+pub static STOP_SIGNAL: AtomicBool = AtomicBool::new(false);
+
+#[dtor]
+fn stop_program() {
+    STOP_SIGNAL.store(true, Ordering::Relaxed);
+}
+
 #[derive(Debug)]
 pub struct Worker {
     // TODO: A circular sync buffer with cache alignment would be a better fit, but for the moment a mpsc channel will do the job.
+    pub thread_id: usize,
     tx: mpsc::Sender<ComputeTask>,
 }
 
 impl Worker {
-    pub fn new(thread_id: usize, stop: Arc<AtomicBool>) -> Self {
+    pub fn new(thread_id: usize) -> Self {
         let (tx, rx) = mpsc::channel();
-        let inner_worker = InnerWorker {
-            thread_id,
-            stop,
-            rx,
-        };
+        let inner_worker = InnerWorker { thread_id, rx };
         thread::spawn(move || inner_worker.work());
-        Self { tx }
+        Self { thread_id, tx }
     }
 
     pub fn send_task(&mut self, compute_task: ComputeTask) {
@@ -35,7 +37,6 @@ impl Worker {
 
 struct InnerWorker {
     thread_id: usize,
-    stop: Arc<AtomicBool>,
     rx: mpsc::Receiver<ComputeTask>,
 }
 
@@ -46,8 +47,9 @@ impl InnerWorker {
             let rx_fifo = self.rx.recv_timeout(Duration::from_millis(100));
             if let Ok(compute_task) = rx_fifo {
                 compute_task.compute();
+                println!("Computing!");
             }
-            if self.stop.load(Ordering::Relaxed) {
+            if STOP_SIGNAL.load(Ordering::Relaxed) {
                 break;
             }
         }
