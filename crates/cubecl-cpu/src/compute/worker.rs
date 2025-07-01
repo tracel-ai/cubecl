856626e0e99a82993a20d9a5745ae03b87445fcb
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-07-01 18:36:53 -0400

    feat: add sync to fix data races

--- crates/cubecl-cpu/src/compute/worker.rs
+++ crates/cubecl-cpu/src/compute/worker.rs
@@ -1,5 +1,5 @@
 use dtor::dtor;
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc};
 use std::time::Duration;
 use std::{
     sync::atomic::{AtomicBool, Ordering},
@@ -19,24 +19,42 @@ fn stop_program() {
 pub struct Worker {
     // TODO: A circular sync buffer with cache alignment would be a better fit, but for the moment a mpsc channel will do the job.
     pub thread_id: usize,
+    waiting: Arc<AtomicBool>,
     tx: mpsc::Sender<ComputeTask>,
 }
 
 impl Worker {
     pub fn new(thread_id: usize) -> Self {
         let (tx, rx) = mpsc::channel();
-        let inner_worker = InnerWorker { thread_id, rx };
+        let waiting = Arc::new(AtomicBool::new(true));
+        let inner_worker = InnerWorker {
+            thread_id,
+            rx,
+            waiting: Arc::clone(&waiting),
+        };
         thread::spawn(move || inner_worker.work());
-        Self { thread_id, tx }
+        Self {
+            thread_id,
+            tx,
+            waiting,
+        }
     }
 
     pub fn send_task(&mut self, compute_task: ComputeTask) {
         self.tx.send(compute_task).unwrap();
     }
+
+    // Spin lock inspired by https://marabos.nl/atomics/building-spinlock.html
+    pub fn sync(&self) {
+        while !self.waiting.load(Ordering::Acquire) {
+            std::hint::spin_loop();
+        }
+    }
 }
 
 struct InnerWorker {
     thread_id: usize,
+    waiting: Arc<AtomicBool>,
     rx: mpsc::Receiver<ComputeTask>,
 }
 
@@ -44,10 +62,14 @@ impl InnerWorker {
     fn work(self) {
         log::trace!("Thread numero {} started", self.thread_id);
         loop {
-            let rx_fifo = self.rx.recv_timeout(Duration::from_millis(100));
-            if let Ok(compute_task) = rx_fifo {
-                compute_task.compute();
-                println!("Computing!");
+            let rx_fifo = self.rx.recv_timeout(Duration::from_millis(1));
+            match rx_fifo {
+                Ok(compute_task) => {
+                    self.waiting.store(false, Ordering::Release);
+                    compute_task.compute()
+                }
+                Err(mpsc::RecvTimeoutError::Timeout) => self.waiting.store(true, Ordering::Release),
+                _ => break,
             }
             if STOP_SIGNAL.load(Ordering::Relaxed) {
                 break;
