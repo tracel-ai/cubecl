a0c8f5a60d5977080fb27560e583d1ed56cddb34
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2025-07-03 09:35:53 -0400

    Refactor hip device (#761)

--- crates/cubecl-hip/src/device.rs
+++ crates/cubecl-hip/src/device.rs
@@ -1,4 +1,10 @@
-#[derive(new, Clone, Debug, PartialEq, Eq, Default, Hash)]
-pub struct HipDevice {
+#[derive(new, Clone, PartialEq, Eq, Default, Hash)]
+pub struct AmdDevice {
     pub index: usize,
+}
+
+impl core::fmt::Debug for AmdDevice {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("AmdDevice({})", self.index))
+    }
 }
