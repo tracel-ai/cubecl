9e5359d377865e7b155a8612443abc3d9b29753b
louisfd <louisfd94@gmail.com>
2025-07-10 14:27:46 -0400

    fix partitionedbatchconfig

--- crates/cubecl-matmul/src/components/batch/partitioned_matmul/config.rs
+++ crates/cubecl-matmul/src/components/batch/partitioned_matmul/config.rs
@@ -49,7 +49,6 @@ impl<G: GlobalConfig> BatchConfig for PartitionedBatchConfig<G> {
 
 impl<G: GlobalConfig> PartitionedBatchConfig<G> {
     /// Create a new config for partitioned batch matmul
-
     pub fn new(global_config: G, hypercube_config: HypercubeConfig) -> Self {
         Self {
             global_config,
@@ -57,11 +56,8 @@ impl<G: GlobalConfig> PartitionedBatchConfig<G> {
         }
     }
 
-    ///
     /// May return an error if:
-    /// - a loader are invalid
-    /// - CubeDim is too big
-    /// - Barriers are not available
+    /// - hypercube config is invalid
     pub fn validate(self, problem: &MatmulProblem) -> Result<Self, MatmulSetupError> {
         self.hypercube_config.validate(problem)?;
         Ok(self)
