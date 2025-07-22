f1b0080edc55c9a0dd02fad333f81a02a200beea
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2025-07-22 16:49:06 -0400

    Feat/static memory pool (#787)

--- crates/cubecl-runtime/src/memory_management/memory_pool/mod.rs
+++ crates/cubecl-runtime/src/memory_management/memory_pool/mod.rs
@@ -5,10 +5,12 @@ mod base;
 mod exclusive_pool;
 pub(crate) mod handle;
 mod sliced_pool;
+mod static_pool;
 
 pub(crate) use base::*;
 pub(crate) use exclusive_pool::*;
 pub(crate) use ring::*;
 pub(crate) use sliced_pool::*;
+pub(crate) use static_pool::*;
 
 pub use handle::*;
