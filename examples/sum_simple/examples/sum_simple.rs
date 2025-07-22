cf16e638b467dee45066b4d2fb907a539b2180a8
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-07-22 15:56:29 -0400

    fix: update cubecl version

--- examples/sum_simple/examples/sum_simple.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    #[cfg(feature = "cuda")]
-    sum_simple::launch::<cubecl::cuda::CudaRuntime>(&Default::default());
-    #[cfg(feature = "wgpu")]
-    sum_simple::launch::<cubecl::wgpu::WgpuRuntime>(&Default::default());
-    #[cfg(feature = "cpu")]
-    sum_simple::launch::<cubecl::cpu::CpuRuntime>(&Default::default());
-}
