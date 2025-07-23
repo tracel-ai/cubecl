f1befd8c6b3d3b5eeaff2b3b15d85fc115cf1d24
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-07-23 17:15:01 -0400

    fix: disable bf16 test because they're not supported by Github runner

--- crates/cubecl-cpu/src/lib.rs
+++ crates/cubecl-cpu/src/lib.rs
@@ -6,13 +6,13 @@ extern crate derive_new;
 mod tests {
     pub type TestRuntime = crate::CpuRuntime;
 
-    pub use half::{bf16, f16};
+    pub use half::f16;
 
-    cubecl_core::testgen_all!(f32: [f16, bf16, f32, f64], i32: [i8, i16, i32, i64], u32: [u8, u16, u32, u64]);
+    cubecl_core::testgen_all!(f32: [f16, f32, f64], i32: [i8, i16, i32, i64], u32: [u8, u16, u32, u64]);
     cubecl_std::testgen!();
-    cubecl_std::testgen_tensor_identity!([f16, bf16, f32, u32]);
+    cubecl_std::testgen_tensor_identity!([f16, f32, u32]);
     cubecl_random::testgen_random!();
-    cubecl_matmul::testgen_matmul_simple!([f16, bf16, f32]);
+    cubecl_matmul::testgen_matmul_simple!([f16, f32]);
 
     // TODO: Failing test that needs to be debugged
     // cubecl_matmul::testgen_matmul_unit!();
