dd59f4a034b5b33a1c13f72952247e8b7ac76d0d
nathaniel <nathaniel.simard.42@gmail.com>
2025-07-24 09:26:17 -0400

    Remove plane

--- crates/cubecl-matmul/src/kernels/layered/selector/plane.rs
+++ crates/cubecl-matmul/src/kernels/layered/selector/plane.rs
@@ -153,10 +153,6 @@ pub fn find_instruction_size(
             .unwrap_or(true)
     };
 
-    if supported(16, 8, 16) {
-        return (16, 8, 16).into();
-    }
-
     if m >= 4 * n && supported(32, 8, 16) {
         (32, 8, 16).into()
     } else if n >= 4 * n && supported(8, 32, 16) {
