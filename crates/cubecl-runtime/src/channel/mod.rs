e07b492d2d14066463420761fa680699a8de6de5
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-08-14 12:14:41 -0400

    feat: replace mutex channel by a mpsc channel

--- crates/cubecl-runtime/src/channel/mod.rs
+++ crates/cubecl-runtime/src/channel/mod.rs
@@ -6,11 +6,6 @@ mod mutex;
 #[cfg(feature = "channel-mutex")]
 pub use mutex::*;
 
-#[cfg(feature = "channel-std-mutex")]
-mod std_mutex;
-#[cfg(feature = "channel-std-mutex")]
-pub use std_mutex::*;
-
 #[cfg(all(feature = "channel-mpsc", not(target_family = "wasm")))]
 mod mpsc;
 #[cfg(all(feature = "channel-mpsc", not(target_family = "wasm")))]
