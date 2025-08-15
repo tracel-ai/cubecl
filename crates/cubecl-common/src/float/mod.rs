fde51073a60f657b44fd84193a9a05d23ff8c4a1
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2025-08-15 10:24:20 -0400

    Feat/fusion quant (#817)
    ---------
    
    Co-authored-by: Guillaume Lagrange <lagrange.guillaume.1@gmail.com>

--- crates/cubecl-common/src/float/mod.rs
+++ crates/cubecl-common/src/float/mod.rs
@@ -1,11 +1,15 @@
+#[cfg(feature = "fp4")]
 mod fp4;
 mod fp6;
+#[cfg(feature = "fp8")]
 mod fp8;
 mod relaxed;
 mod tensor_float;
 
+#[cfg(feature = "fp4")]
 pub use fp4::*;
 pub use fp6::*;
+#[cfg(feature = "fp8")]
 pub use fp8::*;
 pub use relaxed::*;
 pub use tensor_float::*;
