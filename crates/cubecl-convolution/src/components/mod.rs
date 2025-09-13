35d5363e3a29a9394589b4e02c86f59b7ac52196
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-09-13 23:41:18 +0200

    FIx buffer count for acc

--- crates/cubecl-convolution/src/components/mod.rs
+++ crates/cubecl-convolution/src/components/mod.rs
@@ -7,6 +7,10 @@ mod problem;
 mod selection;
 
 pub use config::*;
+use cubecl_matmul::components::tile::{accelerated::AcceleratedMatmul, loader::Strided};
+use cubecl_std::CubeOption;
 pub use error::*;
 pub use problem::*;
 pub use selection::*;
+
+pub type AcceleratedConv = AcceleratedMatmul<CubeOption<Strided>>;
