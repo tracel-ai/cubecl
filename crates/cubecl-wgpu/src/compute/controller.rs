fd461289f1ba0376935460e7da213f09e0199911
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2025-09-19 13:47:32 -0400

    Get data out of staging buffer as soon as available and unmap

--- crates/cubecl-wgpu/src/compute/controller.rs
+++ crates/cubecl-wgpu/src/compute/controller.rs
@@ -4,14 +4,13 @@ use std::ptr::NonNull;
 
 /// Controller for managing wgpu staging buffers managed by a memory pool.
 pub struct WgpuAllocController {
-    staging_buffer: wgpu::Buffer,
     binding: Option<SliceBinding>,
+    buffer_data: Option<Box<[u8]>>,
 }
 
 impl AllocationController for WgpuAllocController {
     fn dealloc(&mut self, _allocation: &cubecl_common::bytes::Allocation) {
-        // We unmap the buffer and release the binding so that the same buffer can be used again.
-        self.staging_buffer.unmap();
+        self.buffer_data.take();
         self.binding = None;
     }
 }
@@ -29,21 +28,25 @@ impl WgpuAllocController {
     ///
     /// The controller and the corresponding `Allocation`.
     pub fn init(binding: SliceBinding, buffer: wgpu::Buffer, size: usize) -> (Self, Allocation) {
-        fn buffer_ptr(buffer: &wgpu::Buffer) -> NonNull<u8> {
+        let data = {
             let data = buffer.slice(..).get_mapped_range();
-            unsafe { NonNull::new_unchecked(data.as_ptr().cast_mut()) }
-        }
+            data[0..size].to_vec().into_boxed_slice()
+        };
+        let ptr = unsafe { NonNull::new_unchecked(data.as_ptr().cast_mut()) };
 
         let allocation = Allocation {
-            ptr: buffer_ptr(&buffer),
+            ptr,
             size,
             align: wgpu::COPY_BUFFER_ALIGNMENT as usize,
         };
 
+        // We unmap the buffer and release the binding so that the same buffer can be used again.
+        buffer.unmap();
+
         (
             Self {
-                staging_buffer: buffer,
                 binding: Some(binding),
+                buffer_data: Some(data),
             },
             allocation,
         )
