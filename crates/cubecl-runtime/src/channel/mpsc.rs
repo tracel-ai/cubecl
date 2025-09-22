ac5a6aec397613b902095122271830cb1475aacd
nathaniel <nathaniel.simard.42@gmail.com>
2025-09-22 16:06:54 -0400

    MPSC

--- crates/cubecl-runtime/src/channel/mpsc.rs
+++ crates/cubecl-runtime/src/channel/mpsc.rs
@@ -1,13 +1,3 @@
-use std::sync::Arc;
-
-use cubecl_common::{
-    ExecutionMode,
-    bytes::Bytes,
-    future::{DynFut, spawn_detached_fut},
-    profile::ProfileDuration,
-    stream_id::StreamId,
-};
-
 use super::ComputeChannel;
 use crate::{
     data_service::DataTransferId,
@@ -18,7 +8,17 @@ use crate::{
         CopyDescriptor, CubeCount, IoError, ProfileError, ProfilingToken,
     },
     storage::{BindingResource, ComputeStorage},
+    stream::scheduler::{SchedulerMultiStream, SchedulerStreamBackend, SchedulerTask},
 };
+use async_channel::Receiver;
+use cubecl_common::{
+    ExecutionMode,
+    bytes::Bytes,
+    future::{DynFut, spawn_detached_fut},
+    profile::ProfileDuration,
+    stream_id::StreamId,
+};
+use std::sync::Arc;
 
 /// Create a channel using a [multi-producer, single-consumer channel to communicate with
 /// the compute server spawn on its own thread.
@@ -91,6 +91,75 @@ impl CopyDescriptorOwned {
     }
 }
 
+impl<S: ComputeServer> core::fmt::Debug for Message<S> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::Create(..) => f.write_str("Create"),
+            Self::Read(..) => f.write_str("Read"),
+            Self::Write(..) => f.write_str("Write"),
+            Self::GetResource(..) => f.write_str("GetResource"),
+            Self::ExecuteKernel(..) => f.write_str("ExecuteKernel"),
+            Self::Flush(..) => f.write_str("Flush"),
+            Self::Sync(..) => f.write_str("Sync"),
+            Self::MemoryUsage(..) => f.write_str("MemoryUsage"),
+            Self::MemoryCleanup(..) => f.write_str("MemoryCleanup"),
+            Self::AllocationMode(..) => f.write_str("AllocationMode"),
+            Self::StartProfile(..) => f.write_str("StartProfile"),
+            Self::StopMeasure(..) => f.write_str("StopMeasure"),
+            Self::DataTransferSend(..) => f.write_str("DataTransferSend"),
+            Self::DataTransferRecv(..) => f.write_str("DataTransferRecv"),
+        }
+    }
+}
+
+impl<S: ComputeServer> SchedulerTask for Message<S> {
+    fn stream_id(&self) -> StreamId {
+        match self {
+            Message::Create(_, stream_id, ..) => *stream_id,
+            Message::Read(_, stream_id, ..) => *stream_id,
+            Message::Write(_, stream_id, ..) => *stream_id,
+            Message::GetResource(_, stream_id, ..) => *stream_id,
+            Message::ExecuteKernel((_, _, _, stream_id), ..) => *stream_id,
+            Message::Flush(stream_id) => *stream_id,
+            Message::Sync(stream_id, ..) => *stream_id,
+            Message::MemoryUsage(stream_id, ..) => *stream_id,
+            Message::MemoryCleanup(stream_id) => *stream_id,
+            Message::AllocationMode(stream_id, ..) => *stream_id,
+            Message::StartProfile(stream_id, ..) => *stream_id,
+            Message::StopMeasure(_, stream_id, ..) => *stream_id,
+            Message::DataTransferSend(stream_id, ..) => *stream_id,
+            Message::DataTransferRecv(stream_id, ..) => *stream_id,
+        }
+    }
+
+    fn bindings<'a>(&'a self) -> Vec<&'a Binding> {
+        match self {
+            Message::Create(..) => Vec::new(),
+            Message::Read(copy_descriptor_owneds, ..) => {
+                copy_descriptor_owneds.iter().map(|c| &c.binding).collect()
+            }
+            Message::Write(items, ..) => items.iter().map(|item| &item.0.binding).collect(),
+            Message::GetResource(binding, ..) => {
+                vec![&binding]
+            }
+            Message::ExecuteKernel(_, bindings, ..) => bindings.buffers.iter().collect(),
+            Message::Flush(..) => vec![],
+            Message::Sync(..) => vec![],
+            Message::MemoryUsage(..) => vec![],
+            Message::MemoryCleanup(..) => vec![],
+            Message::AllocationMode(..) => vec![],
+            Message::StartProfile(..) => vec![],
+            Message::StopMeasure(..) => vec![],
+            Message::DataTransferSend(_, _, copy_descriptor_owned) => {
+                vec![&copy_descriptor_owned.binding]
+            }
+            Message::DataTransferRecv(_, _, copy_descriptor_owned) => {
+                vec![&copy_descriptor_owned.binding]
+            }
+        }
+    }
+}
+
 enum Message<Server>
 where
     Server: ComputeServer,
@@ -140,72 +209,11 @@ where
     Server: ComputeServer + 'static,
 {
     /// Create a new mpsc compute channel.
-    pub fn new(mut server: Server) -> Self {
+    pub fn new(server: Server) -> Self {
         let (sender, receiver) = async_channel::unbounded();
 
-        spawn_detached_fut(async move {
-            while let Ok(message) = receiver.recv().await {
-                match message {
-                    Message::Create(descriptors, stream_id, callback) => {
-                        let descriptors = descriptors.iter().map(|it| it.as_ref()).collect();
-                        let data = server.create(descriptors, stream_id);
-                        callback.send(data).await.unwrap();
-                    }
-                    Message::Read(descriptors, stream, callback) => {
-                        let descriptors = descriptors.iter().map(|it| it.as_ref()).collect();
-                        let data = server.read(descriptors, stream).await;
-                        callback.send(data).await.unwrap();
-                    }
-                    Message::Write(descriptors, stream, callback) => {
-                        let descriptors = descriptors
-                            .iter()
-                            .map(|(desc, data)| (desc.as_ref(), data.as_slice()))
-                            .collect();
-                        let data = server.write(descriptors, stream);
-                        callback.send(data).await.unwrap();
-                    }
-                    Message::GetResource(binding, stream, callback) => {
-                        let data = server.get_resource(binding, stream);
-                        callback.send(data).await.unwrap();
-                    }
-                    Message::ExecuteKernel(kernel, bindings, logger) => unsafe {
-                        server.execute(kernel.0, kernel.1, bindings, kernel.2, logger, kernel.3);
-                    },
-                    Message::Sync(stream, callback) => {
-                        server.sync(stream).await;
-                        callback.send(()).await.unwrap();
-                    }
-                    Message::Flush(stream) => {
-                        server.flush(stream);
-                    }
-                    Message::MemoryUsage(stream, callback) => {
-                        callback.send(server.memory_usage(stream)).await.unwrap();
-                    }
-                    Message::MemoryCleanup(stream) => {
-                        server.memory_cleanup(stream);
-                    }
-                    Message::StartProfile(stream_id, callback) => {
-                        let token = server.start_profile(stream_id);
-                        callback.send(token).await.unwrap();
-                    }
-                    Message::StopMeasure(callback, stream_id, token) => {
-                        callback
-                            .send(server.end_profile(stream_id, token))
-                            .await
-                            .unwrap();
-                    }
-                    Message::AllocationMode(stream, mode) => {
-                        server.allocation_mode(mode, stream);
-                    }
-                    Message::DataTransferSend(stream, id, src) => {
-                        server.register_src(stream, id, src.as_ref());
-                    }
-                    Message::DataTransferRecv(stream, id, dst) => {
-                        server.register_dest(stream, id, dst.as_ref());
-                    }
-                };
-            }
-        });
+        let server = MpscServer { server };
+        spawn_detached_fut(server.run(receiver));
 
         Self {
             state: Arc::new(MpscComputeChannelState { sender }),
@@ -408,5 +416,95 @@ fn handle_response<Response, Err: core::fmt::Debug>(response: Result<Response, E
     match response {
         Ok(val) => val,
         Err(err) => panic!("Can't connect to the server correctly {err:?}"),
+    }
+}
+
+struct MpscServer<S: ComputeServer> {
+    server: S,
+}
+
+impl<S: ComputeServer + 'static> SchedulerStreamBackend for MpscServer<S> {
+    type Task = Message<S>;
+
+    async fn execute(&mut self, tasks: impl Iterator<Item = Self::Task>) {
+        for task in tasks {
+            self.process(task).await;
+        }
+    }
+}
+
+impl<S: ComputeServer + 'static> MpscServer<S> {
+    async fn run(self, receiver: Receiver<Message<S>>) {
+        let mut sm = SchedulerMultiStream::new(self, 8);
+
+        while let Ok(message) = receiver.recv().await {
+            sm.register(message).await;
+        }
+    }
+
+    async fn process(&mut self, message: Message<S>) {
+        match message {
+            Message::Create(descriptors, stream_id, callback) => {
+                let descriptors = descriptors.iter().map(|it| it.as_ref()).collect();
+                let data = self.server.create(descriptors, stream_id);
+                callback.send(data).await.unwrap();
+            }
+            Message::Read(descriptors, stream, callback) => {
+                let descriptors = descriptors.iter().map(|it| it.as_ref()).collect();
+                let data = self.server.read(descriptors, stream).await;
+                callback.send(data).await.unwrap();
+            }
+            Message::Write(descriptors, stream, callback) => {
+                let descriptors = descriptors
+                    .iter()
+                    .map(|(desc, data)| (desc.as_ref(), data.as_slice()))
+                    .collect();
+                let data = self.server.write(descriptors, stream);
+                callback.send(data).await.unwrap();
+            }
+            Message::GetResource(binding, stream, callback) => {
+                let data = self.server.get_resource(binding, stream);
+                callback.send(data).await.unwrap();
+            }
+            Message::ExecuteKernel(kernel, bindings, logger) => unsafe {
+                self.server
+                    .execute(kernel.0, kernel.1, bindings, kernel.2, logger, kernel.3);
+            },
+            Message::Sync(stream, callback) => {
+                self.server.sync(stream).await;
+                callback.send(()).await.unwrap();
+            }
+            Message::Flush(stream) => {
+                self.server.flush(stream);
+            }
+            Message::MemoryUsage(stream, callback) => {
+                callback
+                    .send(self.server.memory_usage(stream))
+                    .await
+                    .unwrap();
+            }
+            Message::MemoryCleanup(stream) => {
+                self.server.memory_cleanup(stream);
+            }
+            Message::StartProfile(stream_id, callback) => {
+                let token = self.server.start_profile(stream_id);
+                callback.send(token).await.unwrap();
+            }
+            Message::StopMeasure(callback, stream_id, token) => {
+                callback
+                    .send(self.server.end_profile(stream_id, token))
+                    .await
+                    .unwrap();
+            }
+            Message::AllocationMode(stream, mode) => {
+                self.server.allocation_mode(mode, stream);
+            }
+            Message::DataTransferSend(stream, id, src) => {
+                self.server.register_src(stream, id, src.as_ref());
+            }
+            Message::DataTransferRecv(stream, id, dst) => {
+                self.server.register_dest(stream, id, dst.as_ref());
+            }
+        };
     }
 }
--- crates/cubecl-runtime/src/stream/scheduler.rs
+++ crates/cubecl-runtime/src/stream/scheduler.rs
@@ -1,8 +1,122 @@
-pub trait SchedulerStreamBackend: 'static {
-    type Task: core::fmt::Debug;
+use crate::{
+    server::Binding,
+    stream::{StreamFactory, StreamPool},
+};
+use core::marker::PhantomData;
+use cubecl_common::stream_id::StreamId;
 
-    fn execute(&mut self, tasks: impl Iterator<Item = Self::Task>);
+pub trait SchedulerStreamBackend {
+    type Task: SchedulerTask;
+
+    async fn execute(&mut self, tasks: impl Iterator<Item = Self::Task>);
+}
+
+pub trait SchedulerTask: core::fmt::Debug {
+    fn stream_id(&self) -> StreamId;
+    fn bindings<'a>(&'a self) -> Vec<&'a Binding>;
 }
 
 #[derive(Debug)]
-pub struct SchedulerMultiStream {}
+pub struct SchedulerMultiStream<B: SchedulerStreamBackend> {
+    pool: StreamPool<SchedulerPoolMarker<B>>,
+    backend: B,
+}
+
+#[derive(Debug)]
+pub struct Stream<B: SchedulerStreamBackend> {
+    tasks: Vec<B::Task>,
+}
+
+impl<B: SchedulerStreamBackend> Stream<B> {
+    fn flush(&mut self) -> Vec<B::Task> {
+        let mut returned = Vec::new();
+        core::mem::swap(&mut returned, &mut self.tasks);
+        returned
+    }
+}
+
+#[derive(Debug)]
+pub struct SchedulerPoolMarker<B: SchedulerStreamBackend> {
+    _p: PhantomData<B>,
+}
+
+impl<B: SchedulerStreamBackend> Default for SchedulerPoolMarker<B> {
+    fn default() -> Self {
+        Self {
+            _p: Default::default(),
+        }
+    }
+}
+
+impl<B: SchedulerStreamBackend> StreamFactory for SchedulerPoolMarker<B> {
+    type Stream = Stream<B>;
+
+    fn create(&mut self) -> Self::Stream {
+        Stream { tasks: Vec::new() }
+    }
+}
+
+impl<B: SchedulerStreamBackend> SchedulerMultiStream<B> {
+    pub fn new(backend: B, max_streams: u8) -> Self {
+        Self {
+            pool: StreamPool::new(SchedulerPoolMarker::default(), max_streams, 0),
+            backend,
+        }
+    }
+
+    pub async fn register(&mut self, task: B::Task) {
+        let stream_id = task.stream_id();
+        self.align_streams(stream_id, task.bindings()).await;
+
+        let current = self.pool.get_mut(&stream_id);
+        current.tasks.push(task);
+    }
+
+    pub(crate) async fn align_streams<'a>(
+        &mut self,
+        stream_id: StreamId,
+        bindings: Vec<&'a Binding>,
+    ) {
+        let mut to_flush = Vec::new();
+
+        for binding in bindings {
+            if binding.stream != stream_id {
+                to_flush.push(binding.stream);
+            }
+        }
+
+        if to_flush.is_empty() {
+            return;
+        }
+
+        self.execute_streams(to_flush).await;
+    }
+
+    pub(crate) async fn execute_streams(&mut self, stream_ids: Vec<StreamId>) {
+        let mut metadata = Vec::new();
+
+        for stream_id in stream_ids.into_iter() {
+            let stream = self.pool.get_mut(&stream_id);
+            let tasks = stream.flush();
+            let num = tasks.len();
+            metadata.push((stream_id, num, tasks.into_iter()));
+        }
+
+        if metadata.is_empty() {
+            return;
+        }
+
+        let total: usize = metadata.iter().map(|i| i.1).sum();
+
+        let num_flushes = metadata.len();
+        let mut tasks = Vec::with_capacity(total);
+
+        for i in 0..num_flushes {
+            if let Some(task) = metadata[i].2.next() {
+                tasks.push(task);
+            }
+        }
+
+        self.backend.execute(tasks.into_iter()).await;
+    }
+}
