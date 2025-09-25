cc24c62f76efb994bdfc4ebb702cce8942ed28ea
nathaniel <nathaniel.simard.42@gmail.com>
2025-09-25 10:14:51 -0400

    Fix wgpu read

--- crates/cubecl-wgpu/src/compute/controller.rs
+++ crates/cubecl-wgpu/src/compute/controller.rs
@@ -5,14 +5,16 @@ use std::ptr::NonNull;
 /// Controller for managing wgpu staging buffers managed by a memory pool.
 pub struct WgpuAllocController {
     staging_buffer: wgpu::Buffer,
-    binding: Option<SliceBinding>,
+    bindings: Option<(SliceBinding, wgpu::BufferViewMut<'static>)>,
 }
 
 impl AllocationController for WgpuAllocController {
     fn dealloc(&mut self, _allocation: &cubecl_common::bytes::Allocation) {
         // We unmap the buffer and release the binding so that the same buffer can be used again.
         self.staging_buffer.unmap();
-        self.binding = None;
+
+        let old = self.bindings.take();
+        core::mem::drop(old);
     }
 }
 
@@ -28,14 +30,25 @@ impl WgpuAllocController {
     /// # Returns
     ///
     /// The controller and the corresponding `Allocation`.
-    pub fn init(binding: SliceBinding, buffer: wgpu::Buffer, size: usize) -> (Self, Allocation) {
-        fn buffer_ptr(buffer: &wgpu::Buffer) -> NonNull<u8> {
-            let data = buffer.slice(..).get_mapped_range();
-            unsafe { NonNull::new_unchecked(data.as_ptr().cast_mut()) }
+    pub fn init(
+        binding: SliceBinding,
+        mut buffer: wgpu::Buffer,
+        size: usize,
+    ) -> (Self, Allocation) {
+        fn buffer_ptr(buffer: &mut wgpu::Buffer) -> (NonNull<u8>, wgpu::BufferViewMut<'static>) {
+            let mut view = buffer.slice(..).get_mapped_range_mut();
+            let data: &mut [u8] = view.as_mut();
+
+            unsafe {
+                (NonNull::new_unchecked(data.as_mut_ptr()), {
+                    core::mem::transmute(view)
+                })
+            }
         }
 
+        let (ptr, view) = buffer_ptr(&mut buffer);
         let allocation = Allocation {
-            ptr: buffer_ptr(&buffer),
+            ptr,
             size,
             align: wgpu::COPY_BUFFER_ALIGNMENT as usize,
         };
@@ -43,7 +56,7 @@ impl WgpuAllocController {
         (
             Self {
                 staging_buffer: buffer,
-                binding: Some(binding),
+                bindings: Some((binding, view)),
             },
             allocation,
         )
