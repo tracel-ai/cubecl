e60d8c4a8f7cb3f1de39abbd53a46389fb3f991d
nathaniel <nathaniel.simard.42@gmail.com>
2025-10-02 12:43:46 -0400

    Fixed

--- crates/cubecl-cpu/src/runtime.rs
+++ crates/cubecl-cpu/src/runtime.rs
@@ -64,7 +64,6 @@ fn create_client(options: RuntimeOptions) -> ComputeClient<Server, Channel> {
     let mem_properties = MemoryDeviceProperties {
         max_page_size: max_shared_memory_size as u64,
         alignment: ALIGNMENT,
-        server_communication: false,
     };
 
     let memory_management =
