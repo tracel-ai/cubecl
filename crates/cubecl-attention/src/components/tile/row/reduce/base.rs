f27bafa8ea30c2079a48e28be36691c9356b8b51
louisfd <louisfd94@gmail.com>
2025-10-06 13:14:03 -0400

    more file change

--- crates/cubecl-attention/src/components/tile/row/reduce/base.rs
+++ crates/cubecl-attention/src/components/tile/row/reduce/base.rs
@@ -1,6 +1,8 @@
 use cubecl_core as cubecl;
 use cubecl_core::prelude::*;
 
+use crate::components::tile::RowMax;
+use crate::components::tile::RowSum;
 use crate::components::tile::RowWise;
 use crate::components::tile::dummy::AttentionMatmulConfig;
 use crate::components::tile::{PlaneLayout, PlaneLayoutExpand};
@@ -41,54 +43,4 @@ pub trait ReduceOp<E: Float> {
     fn reduce_local_store<PL: PlaneLayout<E>>(data: &PL, acc: &mut RowWise<E>);
     fn reduce_step_rowwise(acc: &mut RowWise<E>, elem: &RowWise<E>, mask: bool);
     fn reduce_step_scalar(a: E, b: E) -> E;
-}
-
-#[derive(CubeType)]
-struct RowMax {}
-
-#[derive(CubeType)]
-struct RowSum {}
-
-#[cube]
-impl<E: Float> ReduceOp<E> for RowMax {
-    fn reduce_local<PL: PlaneLayout<E>>(data: &PL) -> RowWise<E> {
-        data.rowwise_max()
-    }
-
-    fn reduce_local_store<PL: PlaneLayout<E>>(data: &PL, acc: &mut RowWise<E>) {
-        acc.max_inplace(&Self::reduce_local::<PL>(data))
-    }
-
-    fn reduce_step_rowwise(acc: &mut RowWise<E>, elem: &RowWise<E>, mask: bool) {
-        let mut masked = RowWise::new_filled(elem.num_rows, E::cast_from(mask) * E::min_value());
-        masked.add_inplace(&elem);
-
-        acc.max_inplace(&masked)
-    }
-
-    fn reduce_step_scalar(a: E, b: E) -> E {
-        Max::max(a, b)
-    }
-}
-
-#[cube]
-impl<E: Float> ReduceOp<E> for RowSum {
-    fn reduce_local<PL: PlaneLayout<E>>(data: &PL) -> RowWise<E> {
-        data.rowwise_sum()
-    }
-
-    fn reduce_local_store<PL: PlaneLayout<E>>(data: &PL, acc: &mut RowWise<E>) {
-        acc.add_inplace(&Self::reduce_local::<PL>(data))
-    }
-
-    fn reduce_step_rowwise(acc: &mut RowWise<E>, elem: &RowWise<E>, mask: bool) {
-        let mut masked = RowWise::new_filled(elem.num_rows, E::cast_from(!mask));
-        masked.mul_inplace(&elem);
-
-        acc.add_inplace(&masked)
-    }
-
-    fn reduce_step_scalar(a: E, b: E) -> E {
-        a + b
-    }
 }
