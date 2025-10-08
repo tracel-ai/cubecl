405d4741c2d3884568bb7553985aae6c614573b2
nathaniel <nathaniel.simard.42@gmail.com>
2025-10-08 16:58:05 -0400

    More tests

--- crates/cubecl-std/src/tests/event.rs
+++ crates/cubecl-std/src/tests/event.rs
@@ -3,9 +3,15 @@ use cubecl::prelude::*;
 use cubecl_core as cubecl;
 
 #[derive(CubeType)]
-pub struct DummyEvent {
+pub struct EventUInt {
     #[cube(comptime)]
     pub value: u32,
+}
+
+#[derive(CubeType)]
+pub struct EventFloat {
+    #[cube(comptime)]
+    pub value: f32,
 }
 
 #[derive(CubeType, Clone)]
@@ -18,14 +24,26 @@ pub struct EventListenerPosOne {
     items: SliceMut<f32>,
 }
 
+#[derive(CubeType, Clone)]
+pub struct EventListenerPosTwo {
+    items: SliceMut<f32>,
+    times: ComptimeCell<Counter>,
+}
+
+#[derive(CubeType, Clone)]
+pub struct Counter {
+    #[cube(comptime)]
+    value: u32,
+}
+
 #[cube]
 impl EventListener for EventListenerPosZero {
-    type Event = DummyEvent;
+    type Event = EventUInt;
 
     fn on_event(&mut self, event: Self::Event, bus: &mut ComptimeEventBus) {
         if comptime!(event.value < 10) {
             comment!("On event pos zero < 10");
-            bus.event::<DummyEvent>(DummyEvent {
+            bus.event::<EventUInt>(EventUInt {
                 value: comptime!(15u32 + event.value),
             });
         } else {
@@ -37,11 +55,35 @@ impl EventListener for EventListenerPosZero {
 
 #[cube]
 impl EventListener for EventListenerPosOne {
-    type Event = DummyEvent;
+    type Event = EventUInt;
 
     fn on_event(&mut self, event: Self::Event, _bus: &mut ComptimeEventBus) {
         comment!("On event pos one");
         self.items[1] = (f32::cast_from(event.value) * 2.0) + self.items[1];
+    }
+}
+
+#[cube]
+impl EventListener for EventListenerPosTwo {
+    type Event = EventFloat;
+
+    fn on_event(&mut self, event: Self::Event, bus: &mut ComptimeEventBus) {
+        comment!("On event pos two");
+        self.items[2] = event.value + self.items[2];
+
+        let times = self.times.read();
+        self.times.store(Counter {
+            value: comptime!(times.value + 1),
+        });
+
+        if comptime!(times.value < 4) {
+            bus.event::<EventFloat>(EventFloat {
+                value: comptime!(event.value * 2.0),
+            });
+            bus.event::<EventUInt>(EventUInt {
+                value: comptime!((event.value * 2.0) as u32),
+            });
+        }
     }
 }
 
@@ -54,19 +96,36 @@ fn test_1(items: SliceMut<f32>) {
     let listener_one = EventListenerPosOne { items };
     bus.listener::<EventListenerPosOne>(listener_one);
 
-    bus.event::<DummyEvent>(DummyEvent { value: 5u32 });
+    bus.event::<EventUInt>(EventUInt { value: 5u32 });
 }
 
 #[cube]
 fn test_2(items: SliceMut<f32>) {
     let mut bus = ComptimeEventBus::new();
     let listener_zero = EventListenerPosZero { items };
-    bus.listener::<EventListenerPosZero>(listener_zero);
-
     let listener_one = EventListenerPosOne { items };
+
+    bus.listener::<EventListenerPosZero>(listener_zero);
     bus.listener::<EventListenerPosOne>(listener_one);
 
-    bus.event::<DummyEvent>(DummyEvent { value: 15u32 });
+    bus.event::<EventUInt>(EventUInt { value: 15u32 });
+}
+
+#[cube]
+fn test_3(items: SliceMut<f32>) {
+    let mut bus = ComptimeEventBus::new();
+    let listener_zero = EventListenerPosZero { items };
+    let listener_one = EventListenerPosOne { items };
+    let listener_two = EventListenerPosTwo {
+        items,
+        times: ComptimeCell::new(Counter { value: 0u32 }),
+    };
+
+    bus.listener::<EventListenerPosZero>(listener_zero);
+    bus.listener::<EventListenerPosOne>(listener_one);
+    bus.listener::<EventListenerPosTwo>(listener_two);
+
+    bus.event::<EventFloat>(EventFloat { value: 15.0f32 });
 }
 
 #[cube(launch_unchecked)]
@@ -77,6 +136,11 @@ fn launch_test_1(output: &mut Array<f32>) {
 #[cube(launch_unchecked)]
 fn launch_test_2(output: &mut Array<f32>) {
     test_2(output.to_slice_mut());
+}
+
+#[cube(launch_unchecked)]
+fn launch_test_3(output: &mut Array<f32>) {
+    test_3(output.to_slice_mut());
 }
 
 pub fn event_test_1<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
@@ -115,6 +179,24 @@ pub fn event_test_2<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
     assert_eq!(actual, &[15.0, 30.0]);
 }
 
+pub fn event_test_3<R: Runtime>(client: ComputeClient<R::Server, R::Channel>) {
+    let output = client.empty(12);
+
+    unsafe {
+        launch_test_3::launch_unchecked::<R>(
+            &client,
+            CubeCount::Static(1, 1, 1),
+            CubeDim { x: 1, y: 1, z: 1 },
+            ArrayArg::from_raw_parts::<f32>(&output, 3, 1),
+        );
+    }
+
+    let bytes = client.read_one(output);
+    let actual = f32::from_bytes(&bytes);
+
+    assert_eq!(actual, &[30.0, 900.0, 465.0]);
+}
+
 #[macro_export]
 macro_rules! testgen_event {
     () => {
@@ -131,6 +213,12 @@ macro_rules! testgen_event {
             fn test_2() {
                 let client = TestRuntime::client(&Default::default());
                 cubecl_std::tests::event::event_test_2::<TestRuntime>(client);
+            }
+
+            #[test]
+            fn test_3() {
+                let client = TestRuntime::client(&Default::default());
+                cubecl_std::tests::event::event_test_3::<TestRuntime>(client);
             }
         }
     };
