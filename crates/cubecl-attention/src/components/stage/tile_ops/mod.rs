c454a1d050cbe0c62c5a60e15a8ed66c58daa606
louisfd <louisfd94@gmail.com>
2025-11-06 14:19:35 -0500

    renaming

--- crates/cubecl-attention/src/components/stage/tile_ops/mod.rs
+++ crates/cubecl-attention/src/components/stage/tile_ops/mod.rs
@@ -1,7 +1,15 @@
-mod reduce;
-mod state;
-mod tiles;
+//! Tile utilities for one tile but abstracted from hardware
 
-pub use reduce::*;
+mod accumulator;
+mod key_value;
+mod mask;
+mod query;
+mod softmax;
+mod state;
+
+pub use accumulator::*;
+pub use key_value::*;
+pub use mask::*;
+pub use query::*;
+pub use softmax::*;
 pub use state::*;
-pub use tiles::*;
--- crates/cubecl-attention/src/components/stage/tile_ops/softmax/base.rs
+++ crates/cubecl-attention/src/components/stage/tile_ops/softmax/base.rs
@@ -1,11 +1,57 @@
 use cubecl_core as cubecl;
 use cubecl_core::prelude::*;
 
+use crate::components::stage::RowMax;
+use crate::components::stage::RowSum;
 use crate::components::tile::FragmentAttentionConfig;
 use crate::components::tile::RowWise;
 use crate::components::tile::RowwiseFormat;
-use crate::components::stage::RowMax;
-use crate::components::stage::RowSum;
+
+use crate::components::stage::{MaskTile, RunningState};
+use crate::components::tile::RowwiseFormatExpand;
+use crate::components::{AttentionPrecision, attention_types::*};
+
+use crate::components::tile::FragmentAttention;
+
+#[cube]
+pub fn tile_softmax<AP: AttentionPrecision, FA: FragmentAttention<AP>, R: Reducer>(
+    rowwise_softmax: &mut <FA as FragmentAttention<AP>>::SoftmaxRow,
+    mask: &MaskTile<AP, FA>,
+    state: &mut RunningState<SM<AP>>,
+    max_placeholder: &mut RowWise<SM<AP>>,
+    sum_placeholder: &mut RowWise<SM<AP>>,
+    #[comptime] dk: u32,
+    #[comptime] config: FA::Config,
+) -> RowWise<SM<AP>> {
+    FA::SoftmaxRow::scale_and_mask::<MaskTile<AP, FA>>(
+        rowwise_softmax,
+        SM::<AP>::new(comptime!(1.0 / (dk as f32).sqrt())),
+        mask,
+    );
+
+    row_max::<SM<AP>, <FA as FragmentAttention<AP>>::SoftmaxRow, R, FA::Config>(
+        max_placeholder,
+        state.m(),
+        rowwise_softmax,
+        config,
+    );
+
+    rowwise_softmax.exp_diff(max_placeholder);
+
+    row_sum::<SM<AP>, <FA as FragmentAttention<AP>>::SoftmaxRow, R, FA::Config>(
+        sum_placeholder,
+        rowwise_softmax,
+        config,
+    );
+
+    let exp_m_diff = state.m().exp_diff(max_placeholder);
+
+    let new_l = exp_m_diff.mul(state.l()).add(sum_placeholder);
+
+    state.update(max_placeholder, &new_l);
+
+    exp_m_diff
+}
 
 #[cube]
 /// Computes the sum of rows on a fragment, using the Reducer's strategy
