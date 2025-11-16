a4156d5c6fe2df334d1eb734249190fbd367ed70
amfaber <andreas.mfaber@gmail.com>
2025-11-16 14:00:08 +0100

    Fix fma so its callable from #cube functions

--- crates/cubecl-core/src/frontend/operation/fma.rs
+++ crates/cubecl-core/src/frontend/operation/fma.rs
@@ -1,34 +1,28 @@
+use cubecl_macros::{cube, intrinsic};
+
 use crate::{
-    ir::{Arithmetic, ExpandElement, FmaOperator, Instruction, Scope},
+    self as cubecl,
+    ir::{Arithmetic, FmaOperator, Instruction},
     prelude::CubePrimitive,
-    unexpanded,
 };
 
 /// Fused multiply-add `A*B+C`.
+#[cube]
 #[allow(unused_variables)]
 pub fn fma<C: CubePrimitive>(a: C, b: C, c: C) -> C {
-    unexpanded!()
+    intrinsic!(|scope| {
+        let output = scope.create_local(a.expand.ty);
+        let out = *output;
+        let a = *a.expand;
+        let b = *b.expand;
+        let c = *c.expand;
+
+        scope.register(Instruction::new(
+            Arithmetic::Fma(FmaOperator { a, b, c }),
+            out,
+        ));
+
+        output.into()
+    })
 }
 
-/// Expand method of [fma].
-#[allow(unused_variables)]
-pub fn fma_expand<C: CubePrimitive>(
-    scope: &mut Scope,
-    a: ExpandElement,
-    b: ExpandElement,
-    c: ExpandElement,
-) -> ExpandElement {
-    let output = scope.create_local(a.ty);
-
-    let out = *output;
-    let a = *a;
-    let b = *b;
-    let c = *c;
-
-    scope.register(Instruction::new(
-        Arithmetic::Fma(FmaOperator { a, b, c }),
-        out,
-    ));
-
-    output
-}
