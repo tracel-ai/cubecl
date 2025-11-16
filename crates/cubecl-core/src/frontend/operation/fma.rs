4e8a0a6e2c38fd47499edb25886b35c01d3299d7
amfaber <andreas.mfaber@gmail.com>
2025-11-16 18:57:06 +0100

    The macro gave issues with the generic

--- crates/cubecl-core/src/frontend/operation/fma.rs
+++ crates/cubecl-core/src/frontend/operation/fma.rs
@@ -1,27 +1,32 @@
-use cubecl_macros::{cube, intrinsic};
-
-use crate::{
-    self as cubecl,
-    ir::{Arithmetic, FmaOperator, Instruction},
-    prelude::CubePrimitive,
-};
+use crate::{prelude::CubePrimitive, unexpanded};
 
 /// Fused multiply-add `A*B+C`.
-#[cube]
 #[allow(unused_variables)]
 pub fn fma<C: CubePrimitive>(a: C, b: C, c: C) -> C {
-    intrinsic!(|scope| {
-        let output = scope.create_local(a.expand.ty);
+    unexpanded!()
+}
+
+/// Expand method of [fma].
+pub mod fma {
+    use cubecl_ir::{Arithmetic, ExpandElement, FmaOperator, Instruction, Scope};
+
+    pub fn expand(
+        scope: &mut Scope,
+        a: ExpandElement,
+        b: ExpandElement,
+        c: ExpandElement,
+    ) -> ExpandElement {
+        let output = scope.create_local(a.ty);
         let out = *output;
-        let a = *a.expand;
-        let b = *b.expand;
-        let c = *c.expand;
+        let a = *a;
+        let b = *b;
+        let c = *c;
 
         scope.register(Instruction::new(
             Arithmetic::Fma(FmaOperator { a, b, c }),
             out,
         ));
 
-        output.into()
-    })
+        output
+    }
 }
