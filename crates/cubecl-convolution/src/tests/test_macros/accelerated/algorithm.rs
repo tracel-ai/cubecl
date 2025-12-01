696462e3fead4484118f45c7df0216a32d5c775c
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2025-12-01 18:40:01 +0100

    Refactor convolution tests

---
+++ crates/cubecl-convolution/src/tests/test_macros/accelerated/algorithm.rs
@@ -0,0 +1,38 @@
+#[macro_export]
+macro_rules! testgen_convolution_accelerated_algorithm {
+    () => {
+        use $crate::kernels::layered::{
+            simple::SimpleConvAlgorithm,
+        };
+        use cubecl_matmul::components::global::read::{
+            async_full_cyclic,
+            sync_full_strided,
+            sync_full_tilewise,
+        };
+        use cubecl_matmul::components::stage::{
+            ColMajorTilingOrder,
+            RowMajorTilingOrder
+        };
+
+        #[cfg(all(feature = "conv_tests_simple", feature="conv_tests_cyclic"))]
+        mod simple_cyclic {
+            use super::*;
+
+            $crate::testgen_convolution_accelerated_precision!(SimpleConvAlgorithm<TMM>);
+        }
+
+        #[cfg(all(feature = "conv_tests_simple", feature="conv_tests_strided"))]
+        mod simple_strided {
+            use super::*;
+
+            $crate::testgen_convolution_accelerated_precision!(SimpleConvAlgorithm<TMM, sync_full_strided::SyncFullStridedLoading, sync_full_strided::SyncFullStridedLoading>);
+        }
+
+        #[cfg(all(feature = "conv_tests_simple", feature="conv_tests_tilewise"))]
+        mod simple_tilewise {
+            use super::*;
+
+            $crate::testgen_convolution_accelerated_precision!(SimpleConvAlgorithm<TMM, sync_full_tilewise::SyncFullTilewiseLoading<RowMajorTilingOrder>, sync_full_tilewise::SyncFullTilewiseLoading<ColMajorTilingOrder>>);
+        }
+    };
+}
