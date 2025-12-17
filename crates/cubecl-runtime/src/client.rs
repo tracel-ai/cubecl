a3f302f63939b881edbd0d7085dc23279f80b0d3
nathaniel <nathaniel.simard.42@gmail.com>
2025-12-17 10:15:07 -0500

    Fix no-std

--- crates/cubecl-runtime/src/client.rs
+++ crates/cubecl-runtime/src/client.rs
@@ -458,7 +458,7 @@ impl<R: Runtime> ComputeClient<R> {
     }
 
     /// Transfer data from one client to another
-    #[tracing::instrument(skip(self, src, dst_server))]
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip(self, src, dst_server)))]
     pub fn to_client(&self, src: Handle, dst_server: &Self) -> Allocation {
         let shape = [src.size() as usize];
         let src_descriptor = src.copy_descriptor(&shape, &[1], 1);
@@ -478,7 +478,10 @@ impl<R: Runtime> ComputeClient<R> {
     /// Transfer data from one client to another
     ///
     /// Make sure the source description can be read in a contiguous manner.
-    #[tracing::instrument(skip(self, src_descriptor, dst_server))]
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(skip(self, src_descriptor, dst_server))
+    )]
     pub fn to_client_tensor(
         &self,
         src_descriptor: CopyDescriptor<'_>,
@@ -512,13 +515,13 @@ impl<R: Runtime> ComputeClient<R> {
     }
 
     #[track_caller]
-    #[tracing::instrument(
+    #[cfg_attr(feature="tracing", tracing::instrument(
         skip(self, kernel, bindings),
         fields(
             kernel.name = %kernel.name(),
             kernel.id = %kernel.id(),
         )
-    )]
+    ))]
     unsafe fn launch_inner(
         &self,
         kernel: <R::Server as ComputeServer>::Kernel,
@@ -761,7 +764,10 @@ impl<R: Runtime> ComputeClient<R> {
     }
 
     /// Transfer data from one client to another
-    #[tracing::instrument(skip(self, src_descriptor, alloc_descriptor, dst_server))]
+    #[cfg_attr(
+        feature = "tracing",
+        tracing::instrument(skip(self, src_descriptor, alloc_descriptor, dst_server))
+    )]
     fn change_client_sync(
         &self,
         src_descriptor: CopyDescriptor<'_>,
