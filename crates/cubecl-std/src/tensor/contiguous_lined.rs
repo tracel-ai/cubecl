403787dd3bcf96ae7f88887b8bc09362bf5ab9ed
nathaniel <nathaniel.simard.42@gmail.com>
2025-12-23 16:33:39 -0500

    Fix the kernel

--- crates/cubecl-std/src/tensor/contiguous_lined.rs
+++ crates/cubecl-std/src/tensor/contiguous_lined.rs
@@ -8,7 +8,7 @@ use cubecl_core::{
 };
 
 #[cube(launch_unchecked)]
-pub fn into_contiguous_lined<N: Numeric>(
+pub fn into_contiguous_perpendicular<N: Numeric>(
     input: &Tensor<Line<N>>,
     output: &mut Tensor<Line<N>>,
     axis_vectorized: u32,
@@ -18,7 +18,6 @@ pub fn into_contiguous_lined<N: Numeric>(
 
     let last_axis = input.rank() - 1;
     let num_batch = output.shape(last_axis) / line_size;
-    debug_print!("Num batch: %i \n", num_batch);
 
     let mut accumulators = Sequence::<Line<N>>::new();
 
@@ -27,8 +26,15 @@ pub fn into_contiguous_lined<N: Numeric>(
         accumulators.push(Line::empty(line_size));
     }
 
-    let channel_input_stride = input.stride(last_axis) / line_size;
-    let channel_output_stride = output.stride(axis_vectorized) / line_size;
+    let channel_input_stride_elem = input.stride(last_axis);
+    let channel_output_stride_elem = output.stride(axis_vectorized);
+
+    let channel_input_stride = channel_input_stride_elem / line_size;
+    let channel_output_stride = channel_output_stride_elem / line_size;
+
+    // debug_print!("channel_input_stride %i\n", channel_input_stride);
+    // debug_print!("channel_output_stride %i\n", channel_output_stride);
+
     let num_runs = output.len() / (num_batch * line_size);
 
     if ABSOLUTE_POS >= num_runs {
@@ -37,9 +43,15 @@ pub fn into_contiguous_lined<N: Numeric>(
 
     let batch_index = ABSOLUTE_POS * num_batch;
     let skip_interval = batch_index / channel_output_stride;
-    let index1 = batch_index % channel_output_stride;
-    let skip_size = line_size * channel_output_stride;
-    let global_index = index1 + (skip_interval * skip_size);
+    let skip_index = batch_index % channel_output_stride;
+    let skip_size = channel_output_stride_elem; // `channel_output_stride * line_size`
+    let global_index = (skip_interval * skip_size) + skip_index;
+
+    // debug_print!("global_index %i - ", global_index);
+    // debug_print!("skip_interval %i - ", skip_interval);
+    // debug_print!("skip_size %i - ", skip_size);
+    // debug_print!("skip_index %i - ", skip_index);
+    // debug_print!("ABSOLUTE_POS %i\n", ABSOLUTE_POS);
 
     for b in 0..num_batch {
         let offset_output = global_index + b;
@@ -121,19 +133,16 @@ pub fn into_contiguous_lined_ref<R: Runtime>(
     );
     let line_size = min(line_size_perpendicular, line_size_parallel);
 
-    println!("Line size: {line_size:?}");
-
-    let num_elems = input.shape.iter().product::<usize>();
-    let working_units = num_elems / (line_size as usize * input.shape[axis]);
-    // let cube_dim = CubeDim::new(client, working_units);
-    let cube_dim = CubeDim::new_single();
+    let num_elems = output.shape.iter().product::<usize>();
+    let working_units = num_elems / (line_size as usize * output.shape[rank - 1]);
+    let cube_dim = CubeDim::new(client, working_units);
     let cube_count = calculate_cube_count_elemwise(client, working_units, cube_dim);
     println!("Axis {axis:?} | {cube_dim:?} | {cube_count:?}");
     println!("{:?} {:?}", input.shape, input.strides);
     println!("{:?} {:?}", output.shape, output.strides);
 
     unsafe {
-        into_contiguous_lined::launch_unchecked::<R>(
+        into_contiguous_perpendicular::launch_unchecked::<R>(
             client,
             cube_count,
             cube_dim,
