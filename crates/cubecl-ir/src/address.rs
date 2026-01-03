177790dbcb75406fb20d16a249abddf559673072
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-01-03 01:41:59 +0100

    Adds docs for `AddressType`

--- crates/cubecl-ir/src/address.rs
+++ crates/cubecl-ir/src/address.rs
@@ -1,13 +1,36 @@
 use crate::{IntKind, Scope, StorageType, UIntKind};
 
+/// The type used for addressing storage types in a kernel.
+/// This is the type `usize` maps to when used in a kernel, with `isize` being mapped to the signed
+/// equivalent.
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Default, PartialOrd, Ord)]
 pub enum AddressType {
+    // Discriminants are explicit to ensure correct ordering
     #[default]
-    U32,
-    U64,
+    U32 = 0,
+    U64 = 1,
 }
 
 impl AddressType {
+    /// Pick an address type based on the number of elements in a buffer.
+    pub fn from_len(num_elems: usize) -> Self {
+        if num_elems > u32::MAX as usize {
+            AddressType::U64
+        } else {
+            AddressType::U32
+        }
+    }
+
+    /// Pick an address type based on the number of elements in a buffer, for a kernel that requires
+    /// signed indices.
+    pub fn from_len_signed(num_elems: usize) -> Self {
+        if num_elems > i32::MAX as usize {
+            AddressType::U64
+        } else {
+            AddressType::U32
+        }
+    }
+
     pub fn register(&self, scope: &mut Scope) {
         scope.register_type::<usize>(self.unsigned_type());
         scope.register_type::<isize>(self.signed_type());
