5b9fb30d49b046232d81a2015bb8860124da1f31
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-01-08 23:04:30 +0100

    Improve atomics API

--- crates/cubecl-core/src/frontend/element/atomic.rs
+++ crates/cubecl-core/src/frontend/element/atomic.rs
@@ -1,10 +1,12 @@
 use cubecl_ir::{AtomicOp, ConstantValue, ExpandElement, StorageType};
+use cubecl_macros::intrinsic;
 
 use super::{ExpandElementIntoMut, ExpandElementTyped, Int, Numeric, into_mut_expand_element};
 use crate::{
+    self as cubecl,
     frontend::{CubePrimitive, CubeType},
     ir::{BinaryOperator, CompareAndSwapOperator, Instruction, Scope, Type, UnaryOperator},
-    unexpanded,
+    prelude::*,
 };
 
 /// An atomic numerical type wrapping a normal numeric primitive. Enables the use of atomic
@@ -15,168 +17,131 @@ pub struct Atomic<Inner: CubePrimitive> {
     pub val: Inner,
 }
 
+type AtomicExpand<Inner> = ExpandElementTyped<Atomic<Inner>>;
+
+#[cube]
 impl<Inner: Numeric> Atomic<Inner> {
     /// Load the value of the atomic.
     #[allow(unused_variables)]
-    pub fn load(pointer: &Self) -> Inner {
-        unexpanded!()
+    pub fn load(&self) -> Inner {
+        intrinsic!(|scope| {
+            let pointer: ExpandElement = self.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Load(UnaryOperator { input: *pointer }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Store the value of the atomic.
     #[allow(unused_variables)]
-    pub fn store(pointer: &Self, value: Inner) {
-        unexpanded!()
+    pub fn store(&self, value: Inner) {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            scope.register(Instruction::new(
+                AtomicOp::Store(UnaryOperator { input: *value }),
+                *ptr,
+            ));
+        })
     }
 
     /// Atomically stores the value into the atomic and returns the old value.
     #[allow(unused_variables)]
-    pub fn swap(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn swap(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Swap(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Atomically add a number to the atomic variable. Returns the old value.
     #[allow(unused_variables)]
-    pub fn add(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn add(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Add(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
+    }
+
+    /// Atomically subtracts a number from the atomic variable. Returns the old value.
+    #[allow(unused_variables)]
+    pub fn sub(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Sub(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Atomically sets the value of the atomic variable to `max(current_value, value)`. Returns
     /// the old value.
     #[allow(unused_variables)]
-    pub fn max(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn max(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Max(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Atomically sets the value of the atomic variable to `min(current_value, value)`. Returns the
     /// old value.
     #[allow(unused_variables)]
-    pub fn min(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
-    }
-
-    /// Atomically subtracts a number from the atomic variable. Returns the old value.
-    #[allow(unused_variables)]
-    pub fn sub(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
-    }
-
-    pub fn __expand_load(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let pointer: ExpandElement = pointer.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Load(UnaryOperator { input: *pointer }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_store(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        scope.register(Instruction::new(
-            AtomicOp::Store(UnaryOperator { input: *value }),
-            *ptr,
-        ));
-    }
-
-    pub fn __expand_swap(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Swap(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_add(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Add(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_sub(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Sub(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_max(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Max(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_min(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Min(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
+    pub fn min(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Min(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 }
 
+#[cube]
 impl<Inner: Int> Atomic<Inner> {
     /// Compare the value at `pointer` to `cmp` and set it to `value` only if they are the same.
     /// Returns the old value of the pointer before the store.
@@ -184,101 +149,76 @@ impl<Inner: Int> Atomic<Inner> {
     /// ### Tip
     /// Compare the returned value to `cmp` to determine whether the store was successful.
     #[allow(unused_variables)]
-    pub fn compare_and_swap(pointer: &Self, cmp: Inner, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn compare_and_swap(&self, cmp: Inner, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let pointer: ExpandElement = self.into();
+            let cmp: ExpandElement = cmp.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::CompareAndSwap(CompareAndSwapOperator {
+                    input: *pointer,
+                    cmp: *cmp,
+                    val: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Executes an atomic bitwise and operation on the atomic variable. Returns the old value.
     #[allow(unused_variables)]
-    pub fn and(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn and(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::And(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Executes an atomic bitwise or operation on the atomic variable. Returns the old value.
     #[allow(unused_variables)]
-    pub fn or(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
+    pub fn or(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Or(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 
     /// Executes an atomic bitwise xor operation on the atomic variable. Returns the old value.
     #[allow(unused_variables)]
-    pub fn xor(pointer: &Self, value: Inner) -> Inner {
-        unexpanded!()
-    }
-
-    pub fn __expand_compare_and_swap(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        cmp: <Inner as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let pointer: ExpandElement = pointer.into();
-        let cmp: ExpandElement = cmp.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::CompareAndSwap(CompareAndSwapOperator {
-                input: *pointer,
-                cmp: *cmp,
-                val: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_and(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::And(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_or(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Or(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
-    }
-
-    pub fn __expand_xor(
-        scope: &mut Scope,
-        pointer: <Self as CubeType>::ExpandType,
-        value: <Inner as CubeType>::ExpandType,
-    ) -> <Inner as CubeType>::ExpandType {
-        let ptr: ExpandElement = pointer.into();
-        let value: ExpandElement = value.into();
-        let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
-        scope.register(Instruction::new(
-            AtomicOp::Xor(BinaryOperator {
-                lhs: *ptr,
-                rhs: *value,
-            }),
-            *new_var,
-        ));
-        new_var.into()
+    pub fn xor(&self, value: Inner) -> Inner {
+        intrinsic!(|scope| {
+            let ptr: ExpandElement = self.into();
+            let value: ExpandElement = value.into();
+            let new_var = scope.create_local(Type::new(Inner::as_type(scope)));
+            scope.register(Instruction::new(
+                AtomicOp::Xor(BinaryOperator {
+                    lhs: *ptr,
+                    rhs: *value,
+                }),
+                *new_var,
+            ));
+            new_var.into()
+        })
     }
 }
 
