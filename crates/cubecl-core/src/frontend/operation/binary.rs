bc840eb9d0206e06f07551c4f92696373dfa9337
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-01-19 16:28:34 +0100

    Fix type on `sub::expand` hack

--- crates/cubecl-core/src/frontend/operation/binary.rs
+++ crates/cubecl-core/src/frontend/operation/binary.rs
@@ -30,7 +30,7 @@ pub mod add {
 }
 
 pub mod sub {
-    use cubecl_ir::ConstantValue;
+    use cubecl_ir::{ConstantValue, Variable};
 
     use super::*;
 
@@ -41,8 +41,15 @@ pub mod sub {
     ) -> ExpandElementTyped<C> {
         // Dirty hack to enable slice destructuring with trailing patterns on `Sequence`
         match (lhs.expand.as_const(), rhs.expand.as_const()) {
-            (Some(ConstantValue::UInt(lhs)), Some(ConstantValue::UInt(rhs))) => {
-                ExpandElement::Plain((lhs - rhs).into()).into()
+            (Some(ConstantValue::UInt(lhs_val)), Some(ConstantValue::UInt(rhs_val))) => {
+                let item_lhs = lhs.expand.ty;
+                let item_rhs = rhs.expand.ty;
+
+                let line_size = find_vectorization(item_lhs, item_rhs);
+
+                let item = item_lhs.line(line_size);
+                let value = (lhs_val - rhs_val).into();
+                ExpandElement::Plain(Variable::constant(value, item)).into()
             }
             _ => binary_expand(scope, lhs.into(), rhs.into(), Arithmetic::Sub).into(),
         }
