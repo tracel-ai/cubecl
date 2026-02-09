a19b2e2bbed797b9ead0399f68a889252ff9b9d5
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-02-09 14:43:24 +0100

    perf: Improve optimized tensor algorithm (#1177)

--- crates/cubecl-runtime/src/memory_management/memory_pool/handle.rs
+++ crates/cubecl-runtime/src/memory_management/memory_pool/handle.rs
@@ -39,11 +39,15 @@ pub fn offset_handles(
     out
 }
 
-/// Maximum multiple of size to consider an optimized allocation
-const MAX_FACTOR: f64 = 1.5;
-
-/// Check if a shape should be allocated as a pitched tensor, based on the last dim.
-/// Should tune the factor.
-pub fn should_optimize(size: usize, buffer_align: usize) -> bool {
-    (size.next_multiple_of(buffer_align) as f64 / size as f64) < MAX_FACTOR
+/// Calculates a best-effort heuristic for the alignment of row-aligned tensors.
+/// Prefers contiguous alignments for unit dimensions, 16-byte minimum alignment for non-unit,
+/// scaling with input size up to `buffer_align`.
+pub fn optimal_align(shape: usize, elem_size: usize, buffer_align: usize) -> usize {
+    if shape == 1 {
+        elem_size
+    } else {
+        (shape * elem_size)
+            .next_power_of_two()
+            .clamp(16, buffer_align)
+    }
 }
