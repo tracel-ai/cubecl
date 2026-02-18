78cef44c1c3e6fc4f9be47079bf175fa27840755
Simeon H.K. Fitch <simeon.fitch@datashapes.com>
2026-02-18 14:37:25 -0500

    Added android to the list of "std_io" platforms.

--- crates/cubecl-runtime/build.rs
+++ crates/cubecl-runtime/build.rs
@@ -5,7 +5,7 @@ fn main() {
     cfg_aliases! {
         // Some features like autotune caching, compilation caching, and config loading rely on files/environment variables,
         // which are only applicable on "standard desktop platforms".
-        std_io: { all(feature = "std", any(target_os = "windows", target_os = "linux", target_os = "macos")) },
+        std_io: { all(feature = "std", any(target_os = "windows", target_os = "linux", target_os = "macos", target_os = "android")) },
         exclusive_memory_only: { any(feature = "exclusive-memory-only", target_family = "wasm") },
         multi_threading: { all(feature = "std", not(target_family = "wasm")) },
     }
