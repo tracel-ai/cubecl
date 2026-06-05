ff4a7d2a3c1c4b11c333086891246f83f35eff1f
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2026-06-04 23:21:01 -0400

    feat: add tests for thread_buffer

--- crates/cubecl-cpu/src/compute/threadpool/thread_buffer.rs
+++ crates/cubecl-cpu/src/compute/threadpool/thread_buffer.rs
@@ -9,6 +9,7 @@ pub trait GetId {
     fn get_id(&self) -> usize;
 }
 
+/// Local thread buffer used for pushing task to a worker for the scheduler and for getting/stealing task for the worker
 pub struct ThreadBuffer<T: GetId> {
     /// Main stream storage
     streams: CircularBuffer<VecDeque<T>>,
@@ -52,6 +53,7 @@ impl<T: GetId> ThreadBuffer<T> {
         self.streams_id.entry(id).insert_entry(self.streams.back());
     }
 
+    /// Add new task to the local thread
     pub fn push(&mut self, elem: T) {
         match self.streams_id.get(&elem.get_id()) {
             Some(&i) => {
@@ -98,6 +100,7 @@ impl<T: GetId> ThreadBuffer<T> {
         None
     }
 
+    /// Pop task for local execution
     pub fn pop(&mut self) -> Option<T> {
         if let Some(elem) = self.pop_local() {
             return Some(elem);
@@ -106,5 +109,150 @@ impl<T: GetId> ThreadBuffer<T> {
             return Some(elem);
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{GetId, ThreadBuffer};
+    use std::sync::Arc;
+
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    struct TestTask {
+        stream_id: usize,
+        value: usize,
+    }
+
+    impl TestTask {
+        fn new(stream_id: usize, value: usize) -> Self {
+            Self { stream_id, value }
+        }
+    }
+
+    impl GetId for TestTask {
+        fn get_id(&self) -> usize {
+            self.stream_id
+        }
+    }
+
+    fn setup_buffers(
+        thread_count: usize,
+        capacity: usize,
+    ) -> Arc<[spin::Mutex<ThreadBuffer<TestTask>>]> {
+        let buffers = (0..thread_count)
+            .map(|thread_id| spin::Mutex::new(ThreadBuffer::new(thread_id, capacity)))
+            .collect::<Vec<_>>();
+        let shared: Arc<[spin::Mutex<ThreadBuffer<TestTask>>]> = buffers.into();
+        for buffer in shared.iter() {
+            buffer.lock().set_threads_buffer(shared.clone());
+        }
+        shared
+    }
+
+    #[test]
+    fn test_pop_empty_returns_none() {
+        let mut buffer = ThreadBuffer::<TestTask>::new(0, 8);
+        assert_eq!(None, buffer.pop());
+        assert_eq!(None, buffer.pop());
+    }
+
+    #[test]
+    fn test_single_stream_fifo_pop_order() {
+        let mut buffer = ThreadBuffer::<TestTask>::new(0, 8);
+
+        buffer.push(TestTask::new(1, 10));
+        buffer.push(TestTask::new(1, 11));
+        buffer.push(TestTask::new(1, 12));
+
+        assert_eq!(Some(TestTask::new(1, 10)), buffer.pop());
+        assert_eq!(Some(TestTask::new(1, 11)), buffer.pop());
+        assert_eq!(Some(TestTask::new(1, 12)), buffer.pop());
+        assert_eq!(None, buffer.pop());
+    }
+
+    #[test]
+    fn test_steal_requires_source_with_more_than_one_stream() {
+        let shared = setup_buffers(2, 8);
+
+        {
+            let mut source = shared[0].lock();
+            source.push(TestTask::new(10, 1));
+        }
+
+        let stolen = shared[1].lock().pop();
+        assert_eq!(None, stolen);
+
+        {
+            let mut source = shared[0].lock();
+            source.push(TestTask::new(20, 2));
+        }
+
+        let stolen = shared[1].lock().pop();
+        assert_eq!(Some(TestTask::new(20, 2)), stolen);
+
+        let source_remaining = shared[0].lock().pop();
+        assert_eq!(Some(TestTask::new(10, 1)), source_remaining);
+    }
+
+    #[test]
+    fn test_steal_from_other_thread_returns_work() {
+        let shared = setup_buffers(2, 8);
+
+        {
+            let mut source = shared[0].lock();
+            source.push(TestTask::new(10, 100));
+            source.push(TestTask::new(20, 200));
+            source.push(TestTask::new(20, 201));
+        }
+
+        let stolen = shared[1].lock().pop();
+        assert_eq!(Some(TestTask::new(20, 200)), stolen);
+
+        let thief_next = shared[1].lock().pop();
+        assert_eq!(Some(TestTask::new(20, 201)), thief_next);
+
+        let source_next = shared[0].lock().pop();
+        assert_eq!(Some(TestTask::new(10, 100)), source_next);
+    }
+
+    #[test]
+    fn test_repush_same_stream_after_progress() {
+        let mut buffer = ThreadBuffer::<TestTask>::new(0, 8);
+
+        buffer.push(TestTask::new(7, 1));
+        buffer.push(TestTask::new(7, 2));
+        assert_eq!(Some(TestTask::new(7, 1)), buffer.pop());
+
+        buffer.push(TestTask::new(7, 3));
+
+        assert_eq!(Some(TestTask::new(7, 2)), buffer.pop());
+        assert_eq!(Some(TestTask::new(7, 3)), buffer.pop());
+        assert_eq!(None, buffer.pop());
+    }
+
+    #[test]
+    fn test_steal_then_push_same_stream_on_thief() {
+        let shared = setup_buffers(2, 8);
+
+        {
+            let mut source = shared[0].lock();
+            source.push(TestTask::new(1, 1));
+            source.push(TestTask::new(2, 10));
+            source.push(TestTask::new(2, 11));
+        }
+
+        let first = shared[1].lock().pop();
+        assert_eq!(Some(TestTask::new(2, 10)), first);
+
+        {
+            let mut thief = shared[1].lock();
+            thief.push(TestTask::new(2, 12));
+        }
+
+        let second = shared[1].lock().pop();
+        let third = shared[1].lock().pop();
+        assert_eq!(Some(TestTask::new(2, 11)), second);
+        assert_eq!(Some(TestTask::new(2, 12)), third);
+        assert_eq!(None, shared[1].lock().pop());
     }
 }
