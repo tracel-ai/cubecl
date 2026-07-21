90d33e5413e2733b63ca3577ef9388c06a85cf81
Charles23R <renaudcharles14@hotmail.com>
2026-07-21 13:47:33 -0400

    format

--- crates/cubecl-wgpu/src/compute/server.rs
+++ crates/cubecl-wgpu/src/compute/server.rs
@@ -4,13 +4,13 @@ use super::storage::{WgpuResource, WgpuStorage};
 use crate::WgpuCompiler;
 use crate::schedule::{BindingsResource, ScheduleTask, ScheduledWgpuBackend};
 use alloc::sync::Arc;
+use cubecl_common::pool::LeasePool;
 use cubecl_common::{
     backtrace::BackTrace,
     bytes::Bytes,
     profile::{ProfileDuration, TimingMethod},
     stream_id::StreamId,
 };
-use cubecl_common::pool::LeasePool;
 use cubecl_core::server::{Binding, StreamErrorMode};
 use cubecl_core::zspace::Shape;
 use cubecl_core::{
