d26d5381c112b7cedce818288e02a04ff53a91e5
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2026-08-04 16:16:57 -0400

    fix: most test of hip now pass

--- crates/cubecl-cpp/src/hip/mod.rs
+++ crates/cubecl-cpp/src/hip/mod.rs
@@ -7,6 +7,7 @@ pub mod mma;
 pub mod plane;
 pub mod signature;
 pub mod ty;
+pub mod unary;
 
 use dialect::*;
 pub use mma::manual::{supported_mma_combinations, supported_scaled_mma_combinations};
