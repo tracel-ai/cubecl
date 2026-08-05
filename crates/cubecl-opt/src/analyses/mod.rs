ae06c5fb8e54b915db56a372b9c571b5ae062956
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2026-08-05 23:42:42 +0200

    Cleanup

--- crates/cubecl-opt/src/analyses/mod.rs
+++ crates/cubecl-opt/src/analyses/mod.rs
@@ -1,4 +1,3 @@
 pub mod liveness;
 pub mod pointer_source;
 pub mod slices;
-pub mod uniformity;
--- crates/cubecl-opt/src/passes/mod.rs
+++ crates/cubecl-opt/src/passes/mod.rs
@@ -1,15 +1,7 @@
-// mod disaggregate_array;
-mod expression_merge;
-// mod index_merge;
-// mod inlined_if_to_select;
-// mod reduce_strength;
 pub mod alloc_shared_memory;
 pub mod annotate_buffer_visibility;
+mod expression_merge;
 pub mod mem2reg;
 pub mod simple_cse;
 
-// pub use disaggregate_array::*;
 pub use expression_merge::*;
-// pub use index_merge::*;
-// pub use inlined_if_to_select::*;
-// pub use reduce_strength::*;
